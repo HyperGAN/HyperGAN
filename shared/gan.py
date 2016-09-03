bae00c98051390d333bc11f05ff02414b5d037ea
Martyn Garcia <martyn@255bits.com>
2016-09-02 21:00:43 -0600

    Revert "[refactor] discriminator creation"
    This reverts commit d78133b8a3e63334b3ea95f6678ad1a70f85dded.

--- shared/gan.py
+++ shared/gan.py
@@ -184,40 +184,40 @@ def discriminator_wide_resnet(config, x):
     activation = config['d_activation']
     batch_size = int(x.get_shape()[0])
     layers = config['d_wide_resnet_depth']
+    result = x
+    #result = build_conv_tower(result, config['conv_d_layers'][:1], config['d_pre_res_filter'], config['batch_size'], config['d_batch_norm'], True, 'd_', config['d_activation'], stride=config['d_pre_res_stride'])
 
-    def deconv_to_size(conv, start_resnet_at_height=64, end_resnet_at_height=16):
-        result = conv
-        height = result.get_shape()[2]
-        i=0
-        while(height > start_resnet_at_height):
-            # downsample + identity
-            result = conv2d(result, layers[0], name='d_expand'+str(i), k_w=3, k_h=3, d_h=2, d_w=2)
-            result = batch_norm(config['batch_size'], name='d_expand_bn'+str(i))(result)
-            result = activation(result)
-            result = conv2d(result, layers[0], name='d_expand'+str(i)+'a', k_w=3, k_h=3, d_h=1, d_w=1)
-            result = batch_norm(config['batch_size'], name='d_expand_bn'+str(i)+'a')(result)
-            result = activation(result)
-            height = result.get_shape()[2]
-            i+=1
-        i=0
-        result = residual_block(result, activation, batch_size, 'widen', 'd_layers_wide')
-        while(height > end_resnet_at_height):
-            result = residual_block(result, activation, batch_size, 'conv', 'd_layers_conv_'+str(i))
-            result = residual_block(result, activation, batch_size, 'identity', 'd_layers_identity_'+str(i))
-            height = result.get_shape()[2]
-            i+= 1
-
-        return result
-
-
-    result = deconv_to_size(x)
-
+    #result = activation(result)
+    result = conv2d(result, layers[0], name='d_expand1', k_w=3, k_h=3, d_h=2, d_w=2)
+    result = batch_norm(config['batch_size'], name='d_expand_bn1')(result)
+    result = activation(result)
+    result = conv2d(result, layers[0], name='d_expand1a', k_w=3, k_h=3, d_h=1, d_w=1)
+    result = batch_norm(config['batch_size'], name='d_expand_bn1a')(result)
+    result = activation(result)
+    result = conv2d(result, layers[0]*2, name='d_expand2', k_w=3, k_h=3, d_h=2, d_w=2)
+    result = batch_norm(config['batch_size'], name='d_expand_bn2')(result)
+    result = activation(result)
+    result = conv2d(result, layers[0]*2, name='d_expand', k_w=3, k_h=3, d_h=1, d_w=1)
+    result = batch_norm(config['batch_size'], name='d_expand_bn')(result)
+    result = activation(result)
+    result = residual_block(result, activation, batch_size, 'widen', 'd_layers_0')
+    result = residual_block(result, activation, batch_size, 'identity', 'd_layers_1')
+    result = residual_block(result, activation, batch_size, 'conv', 'd_layers_2')
+    result = residual_block(result, activation, batch_size, 'identity', 'd_layers_3')
+    result = residual_block(result, activation, batch_size, 'conv', 'd_layers_4')
+    result = residual_block(result, activation, batch_size, 'identity', 'd_layers_5')
+    #result = residual_block(result, activation, batch_size, 'conv', 'd_layers_6')
+    #result = residual_block(result, activation, batch_size, 'identity', 'd_layers_7')
+    #result = residual_block(result, stride=1, 'conv')
+    #result = residual_block(result, stride=1, 'identity')
+    #result = residual_block(result, stride=1,  'conv')
+    #result = residual_block(result, stride=1,  'identity')
     filter_size_w = int(result.get_shape()[1])
     filter_size_h = int(result.get_shape()[2])
-    print("RESULT SIZE IS", result.get_shape())
     filter = [1,filter_size_w,filter_size_h,1]
     stride = [1,filter_size_w,filter_size_h,1]
     result = tf.nn.avg_pool(result, ksize=filter, strides=stride, padding='SAME')
+    print("RESULT SIZE IS", result)
     result = tf.reshape(result, [batch_size, -1])
 
     return result

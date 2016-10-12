df93bec5d9c3c46c8c6104d0d6ed3126981e1821
Mikkel Garcia <mikkel@255bits.com>
2016-10-11 21:47:15 -0600

    [feature] image transform

--- shared/data_loader.py
+++ shared/data_loader.py
@@ -60,6 +60,12 @@ def labelled_image_tensors_from_directory(directory, batch_size, channels=3, for
   #resized_image = reshaped_image
   tf.Tensor.set_shape(resized_image, [height,width,channels])
   print(resized_image)
+  resized_image = tf.image.random_flip_left_right(resized_image)
+  #resized_image = tf.image.random_brightness(resized_image, 0.4)
+  #resized_image = tf.image.random_contrast(resized_image, 0.2, 1.0)
+  #resized_image = tf.image.random_hue(resized_image, 0.1)
+  #resized_image = tf.image.random_saturation(resized_image, 0.5, 1.0)
+
   #resized_image = tf.image.convert_image_dtype(resized_image, tf.float32)
   # Subtract off the mean and divide by the variance of the pixels.
   #float_image = tf.image.per_image_whitening(resized_image)

46f4547c38d4d044818a33adf8b585f7c687e290
Martyn Garcia <martyn@255bits.com>
2016-10-21 16:35:51 -0600

    [fix] batch norm on gs[i]

--- shared/gan.py
+++ shared/gan.py
@@ -78,6 +78,7 @@ def generator(config, inputs, reuse=False):
                     noise = [s[0],resized_wh[0],resized_wh[1],2**(depth+1-i)]
                     result = block_conv(result, activation, batch_size, 'identity', 'g_layers_'+str(i), output_channels=layers, filter=3, noise_shape=noise)
                     first3 = tf.slice(result, [0,0,0,0], [-1,-1,-1,3])
+                    first3 = batch_norm(config['batch_size'], name='g_bn_first3_'+str(i))(first3)
                     first3 = config['g_last_layer'](first3)
                     results.append(first3)
                     size = int(result.get_shape()[1])*int(result.get_shape()[2])*int(result.get_shape()[3])

cf9ff083517501588eef024a8bdb628766be3107
Martyn Garcia <martyn@255bits.com>
2016-10-21 21:10:35 -0600

    [refactor] set_tensor xgs

--- shared/gan.py
+++ shared/gan.py
@@ -320,6 +320,14 @@ def discriminator(config, x, f,z,g,gz):
       minis = get_minibatch_features(config, result, batch_size,config['dtype'])
       result = tf.concat(1, [result]+minis)
 
+    if(config['minibatch']=='openai-smallest-image'):
+      smallest_xg = get_tensor("xgs")[-1]
+      print("SMALL", smallest_xg)
+      minis = block_conv(smallest_xg, config['d_activation'], batch_size, 'identity', 'd_minibatch_', output_channels=16, filter=3)
+      minis = tf.reshape(minis, [config['batch_size']*2, -1])
+      minis = get_minibatch_features(config, minis, batch_size,config['dtype'])
+      result = tf.concat(1, [result]+minis)
+
     if(config['d_linear_layer']):
         result = linear(result, config['d_linear_layers'], scope="d_linear_layer")
         if(config['d_batch_norm']):
@@ -403,13 +411,14 @@ def discriminator_pyramid(config, x, g, xs, gs):
     result = x
     result = conv2d(result, 64, name='d_expand', k_w=3, k_h=3, d_h=2, d_w=2)
 
-    xg = None
+    xgs = []
     for i in range(depth):
       result = batch_norm(config['batch_size'], name='d_expand_bn_'+str(i))(result)
       result = activation(result)
       # APPEND xs[i] and gs[i]
       xg = tf.concat(0, [xs[i+1], gs[i+1]])
       xg += tf.random_normal(xg.get_shape(), mean=0, stddev=config['d_noise'], dtype=config['dtype'])
+      xgs.append(xg)
 
       print("+++++",result, xg, "____")
       result = tf.concat(3, [result, xg])
@@ -417,7 +426,7 @@ def discriminator_pyramid(config, x, g, xs, gs):
       result = conv2d(result, int(result.get_shape()[3])*2, name='d_expand_layer'+str(i), k_w=3, k_h=3, d_h=2, d_w=2)
       print('discriminator result', result)
     print("xg is ", xg)
-    set_tensor("smallest_xg", xg)
+    set_tensor("xgs", xgs)
 
 
     result = batch_norm(config['batch_size'], name='d_expand_bn_end_'+str(i))(result)

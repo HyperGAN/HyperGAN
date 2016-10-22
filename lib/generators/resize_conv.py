a02a887705f11e4f67689694eac39a580e257b8f
Martyn Garcia <martyn@255bits.com>
2016-10-22 17:39:44 -0600

    [fix] add missing generators

---
+++ lib/generators/resize_conv.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from lib.util.hc_tf import *
+
+def generator(config, net):
+    depth=6
+    target_size = int(net.get_shape()[1])*(2**depth)*int(net.get_shape()[2])*(2**depth)*config['channels']
+    nets=[]
+    activation = config['generator.activation']
+    batch_size = config['batch_size']
+
+    for i in range(depth):
+        s = [int(x) for x in net.get_shape()]
+        layers = int(net.get_shape()[3])//1.5
+        if(i == depth-1):
+            layers=config['channels']
+        resized_wh=[s[1]*2, s[2]*2]
+        net = tf.image.resize_images(net, resized_wh[0], resized_wh[1], 1)
+        noise = [s[0],resized_wh[0],resized_wh[1],2**(depth+1-i)]
+        net = block_conv(net, activation, batch_size, 'identity', 'g_layers_'+str(i), output_channels=layers, filter=3, noise_shape=noise)
+        first3 = tf.slice(net, [0,0,0,0], [-1,-1,-1,3])
+        first3 = batch_norm(config['batch_size'], name='g_bn_first3_'+str(i))(first3)
+        first3 = config['g_last_layer'](first3)
+        nets.append(first3)
+        size = int(net.get_shape()[1])*int(net.get_shape()[2])*int(net.get_shape()[3])
+        print("Generator layer:",net, size, target_size)
+
+    return nets
+
+

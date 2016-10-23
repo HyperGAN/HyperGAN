[fix refactor] restore to working defaults, refactor capping gradient

hypergan.py
@@ -65,8 +65,8 @@ hc.set("generator.resize_conv.depth_reduction", 1.75) # Divides our depth by thi
 
 # Trainer configuration
 #trainer = adam_trainer
-#trainer = slowdown_trainer
-trainer = sgd_adam_trainer
+trainer = slowdown_trainer
+#trainer = sgd_adam_trainer
 hc.set("trainer.initializer", trainer.initialize)
 hc.set("trainer.train", trainer.train)
 #Adam trainer
@@ -86,7 +86,7 @@ hc.set("trainer.sgd_adam.generator.lr", 1e-3) # g learning rate
 hc.set("discriminator", pyramid_discriminator.discriminator)
 hc.set("discriminator.activation", [tf.nn.elu, tf.nn.relu, tf.nn.relu6, lrelu]);
 
-hc.set('discriminator.fc_layer', [False])
+hc.set('discriminator.fc_layer', [True])
 hc.set('discriminator.fc_layer.size', 128)
 
 ## Below here are legacy settings that need to be cleaned up - they may still be in use

lib/gan.py
@@ -486,12 +486,6 @@ def create(config, x,y,f):
     else:
         mse_loss = None
 
-    g_vars = [var for var in tf.trainable_variables() if 'g_' in var.name]
-    d_vars = [var for var in tf.trainable_variables() if 'd_' in var.name]
-
-    v_vars = [var for var in tf.trainable_variables() if 'v_' in var.name]
-    g_vars += v_vars
-
     print("adding", g_losses)
     print("and", d_losses)
     g_loss = tf.reduce_mean(tf.add_n(g_losses))
@@ -536,6 +530,11 @@ def create(config, x,y,f):
     if(config['latent_loss']):
         set_tensor('latent_loss', tf.reduce_mean(latent_loss))
 
+    g_vars = [var for var in tf.trainable_variables() if 'g_' in var.name]
+    d_vars = [var for var in tf.trainable_variables() if 'd_' in var.name]
+
+    v_vars = [var for var in tf.trainable_variables() if 'v_' in var.name]
+    g_vars += v_vars
     g_optimizer, d_optimizer = config['trainer.initializer'](config, d_vars, g_vars)
     set_tensor("d_optimizer", d_optimizer)
     set_tensor("g_optimizer", g_optimizer)
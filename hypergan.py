886060df8f262f35c51c4e3c5b2fa7e92109dca8
Mikkel Garcia <mikkel@255bits.com>
2016-10-24 14:55:29 -0600

    [refactor] cli arguments

--- hypergan.py
+++ hypergan.py
@@ -34,30 +34,9 @@ from tensorflow.python.framework import ops
 
 import importlib
 
-import argparse
+import lib.cli as cli
 
-parser = argparse.ArgumentParser(description='Runs the GAN.')
-parser.add_argument('--load_config', type=str)
-parser.add_argument('--epochs', type=int, default=10)
-
-parser.add_argument('--channels', type=int, default=3)
-parser.add_argument('--directory', type=str)
-parser.add_argument('--no_stop', type=bool)
-parser.add_argument('--crop', type=bool, default=True)
-
-parser.add_argument('--width', type=int, default=64)
-parser.add_argument('--height', type=int, default=64)
-parser.add_argument('--seconds', type=int, default=2)
-parser.add_argument('--batch', type=int, default=64)
-parser.add_argument('--format', type=str, default='png')
-parser.add_argument('--bitrate', type=int, default=16*1024)
-parser.add_argument('--test', type=bool, default=False)
-parser.add_argument('--server', type=bool, default=False)
-parser.add_argument('--save_every', type=int, default=0)
-parser.add_argument('--device', type=str, default='/gpu:0')
-parser.add_argument('--build', type=bool, default=False)
-
-args = parser.parse_args()
+args = cli.parse_args()
 
 #The data type to use in our GAN.  Only float32 is supported at the moment
 hc.set('dtype', tf.float32)
---
+++ lib/cli.py
@@ -0,0 +1,27 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Runs the GAN.')
+    parser.add_argument('--load_config', type=str)
+    parser.add_argument('--epochs', type=int, default=10)
+
+    parser.add_argument('--channels', type=int, default=3)
+    parser.add_argument('--directory', type=str)
+    parser.add_argument('--no_stop', type=bool)
+    parser.add_argument('--crop', type=bool, default=True)
+
+    parser.add_argument('--width', type=int, default=64)
+    parser.add_argument('--height', type=int, default=64)
+    parser.add_argument('--seconds', type=int, default=2)
+    parser.add_argument('--batch', type=int, default=64)
+    parser.add_argument('--format', type=str, default='png')
+    parser.add_argument('--bitrate', type=int, default=16*1024)
+    parser.add_argument('--test', type=bool, default=False)
+    parser.add_argument('--server', type=bool, default=False)
+    parser.add_argument('--save_every', type=int, default=0)
+    parser.add_argument('--device', type=str, default='/gpu:0')
+    parser.add_argument('--build', type=bool, default=False)
+
+    return parser.parse_args()
+
+

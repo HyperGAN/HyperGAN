e6692a057cf61c089ec6288a48acabe97d3ec49c
martyn <martyn.garcia@gmail.com>
2016-10-25 01:48:30 -0600

    [fix] remove stride from nostride

--- lib/discriminators/pyramid_nostride_discriminator.py
+++ lib/discriminators/pyramid_nostride_discriminator.py
@@ -8,24 +8,24 @@ def discriminator(config, x, g, xs, gs):
     depth_increase = config['discriminator.pyramid.depth_increase']
     depth = config['discriminator.pyramid.layers']
     result = x
-    result = conv2d(result, 64, name='d_expand', k_w=3, k_h=3, d_h=2, d_w=2)
+    result = conv2d(result, 16, name='d_expand', k_w=3, k_h=3, d_h=1, d_w=1)
 
     xgs = []
     for i in range(depth):
       result = batch_norm(config['batch_size'], name='d_expand_bn_'+str(i))(result)
       result = activation(result)
       # APPEND xs[i] and gs[i]
-      if(i < len(xs)-1):
-        xg = tf.concat(0, [xs[i+1], gs[i+1]])
+      if(i < len(xs) and i > 0):
+        xg = tf.concat(0, [xs[i], gs[i]])
         xg += tf.random_normal(xg.get_shape(), mean=0, stddev=config['discriminator.noise_stddev'], dtype=config['dtype'])
         xgs.append(xg)
   
-        mxg = conv2d(xg, 6*(i+1), name="d_add_xg"+str(i), k_w=3, k_h=3, d_h=1, d_w=1)
+        mxg = conv2d(xg, 6*(i), name="d_add_xg"+str(i), k_w=3, k_h=3, d_h=1, d_w=1)
         mxg = batch_norm(config['batch_size'], name='d_add_xg_bn_'+str(i))(mxg)
         mxg = activation(mxg)
   
-        minisx = tf.reduce_mean(xs[i+1], reduction_indices=0, keep_dims=True)
-        minisg = tf.reduce_mean(gs[i+1], reduction_indices=0, keep_dims=True)
+        minisx = tf.reduce_mean(xs[i], reduction_indices=0, keep_dims=True)
+        minisg = tf.reduce_mean(gs[i], reduction_indices=0, keep_dims=True)
         minisx = tf.tile(minisx, [config['batch_size'], 1,1,1]) 
         minisg = tf.tile(minisg, [config['batch_size'], 1,1,1]) 
         minis = tf.concat(0, [minisx, minisg])

de8dfe0db0a6f1f0708761864c62563af14ef5e7
Martyn Garcia <martyn@255bits.com>
2016-10-29 12:44:39 -0600

    [fix] add missing file

---
+++ lib/regularizers/l2_regularizer.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+def get(config):
+    ws = None
+    with tf.variable_scope("generator"):
+        with tf.variable_scope("g_lin_proj"):
+            tf.get_variable_scope().reuse_variables()
+            ws = tf.get_variable('Matrix',dtype=config['dtype'])
+            tf.get_variable_scope().reuse_variables()
+        lam = config['generator.regularizers.l2.lambda']
+        print("ADDING REG", lam, ws,ws.get_shape())
+        return [lam*tf.nn.l2_loss(ws)]
+
+
+

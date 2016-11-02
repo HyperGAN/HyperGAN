9c2d07f6bed26d3457e0d9a9b527f6746d6cb953
Martyn Garcia <martyn@255bits.com>
2016-11-01 22:33:00 -0600

    [wip] start z at 4,4

--- lib/util/hc_tf.py
+++ lib/util/hc_tf.py
@@ -55,8 +55,8 @@ def find_smallest_prime(x, y):
     for i in range(3,x-1):
         for j in range(1, y-1):
             if(x % (i) == 0 and y % (j) == 0 and x // i == y // j):
-                if(i==j):
-                    return 1,1
+                #if(i==j):
+                #    return 1,1
                 return i,j
     return None,None
 

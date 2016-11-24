fda529612cb27fd1d45e8fe5fd2a7fd2b1e84426
Mikkel Garcia <mikkel@255bits.com>
2016-11-23 18:36:53 -0700

    [feature] rmsprop trainer

--- lib/trainers/rmsprop_trainer.py
+++ lib/trainers/rmsprop_trainer.py
@@ -5,14 +5,11 @@ TINY = 1e-12
 def initialize(config, d_vars, g_vars):
     d_loss = get_tensor('d_loss')
     g_loss = get_tensor('g_loss')
-    g_lr = np.float32(config['trainer.adam.generator.lr'])
-    g_beta1 = np.float32(config['trainer.adam.generator.beta1'])
-    g_beta2 = np.float32(config['trainer.adam.generator.beta2'])
-    g_epsilon = np.float32(config['trainer.adam.generator.epsilon'])
     d_lr = np.float32(config['trainer.rmsprop.discriminator.lr'])
+    g_lr = np.float32(config['trainer.rmsprop.generator.lr'])
 
-    g_optimizer = tf.train.AdamOptimizer(g_lr, beta1=g_beta1, beta2=g_beta2, epsilon=g_epsilon).minimize(g_loss, var_list=g_vars)
-    d_optimizer = tf.train.AdamOptimizer(d_lr).minimize(d_loss, var_list=d_vars)
+    g_optimizer = tf.train.RMSPropOptimizer(g_lr).minimize(g_loss, var_list=g_vars)
+    d_optimizer = tf.train.RMSPropOptimizer(d_lr).minimize(d_loss, var_list=d_vars)
     return g_optimizer, d_optimizer
 
 iteration = 0
@@ -31,6 +28,9 @@ def train(sess, config):
     _, d_cost = sess.run([d_optimizer, d_loss])
     _, g_cost,d_fake,d_real,d_class = sess.run([g_optimizer, g_loss, d_fake_loss, d_real_loss, d_class_loss])
     print("[rms]%2d: g cost %.2f d_fake %.2f d_real %.2f d_class %.2f" % (iteration, g_cost,d_fake, d_real, d_class ))
+    if(g_cost > 0.7):
+      _, g_cost,d_fake,d_real,d_class = sess.run([g_optimizer, g_loss, d_fake_loss, d_real_loss, d_class_loss])
+      print("[rms]%2d: g cost %.2f d_fake %.2f d_real %.2f d_class %.2f" % (iteration, g_cost,d_fake, d_real, d_class ))
 
     global iteration
     iteration+=1

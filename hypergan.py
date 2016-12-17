02913c38f8e4ca5393dfe8ca6872f598b036c789
Martyn Garcia <martyn@255bits.com>
2016-12-17 12:06:16 -0700

    [fix] disable categories

--- hypergan.py
+++ hypergan.py
@@ -144,7 +144,7 @@ hc.set("examples_per_epoch", 30000/4)
 
 #TODO category/bernouilli
 categories = [[2]+[2]+build_categories_config(30)]
-hc.set('categories', categories)
+hc.set('categories', [[]])#categories)
 hc.set('categories_lambda', list(np.linspace(.001, .01, num=100)))
 hc.set('category_loss', [False])
 

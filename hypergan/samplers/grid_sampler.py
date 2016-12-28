1b63f43956d73dea3b31fcc526d99260ce837718
Mikkel Garcia <mikkel@255bits.com>
2016-12-28 15:30:24 -0700

    [fix] batch size 32 frame sampler

--- hypergan/samplers/grid_sampler.py
+++ hypergan/samplers/grid_sampler.py
@@ -7,7 +7,7 @@ def sample(sample_file, sess, config):
     y_t = get_tensor("y")
     z_t = get_tensor("z")
     dropout_t = get_tensor("dropout")
-    categories_t = get_tensor("categories")[0]
+    #categories_t = get_tensor("categories")[0]
 
     x = np.linspace(0,1, 4)
     y = np.linspace(0,1, 6)
@@ -16,7 +16,8 @@ def sample(sample_file, sess, config):
     #z = np.mgrid[-3:3:0.6*3, -3:3:0.38*3].reshape(2,-1).T
     #z = np.mgrid[-6:6:0.6*6, -6:6:0.38*6].reshape(2,-1).T
 
-    z = np.mgrid[-1:1:0.6, -1:1:0.38].reshape(2,-1).T
+    #z = np.random.uniform(-1, 1, [config['batch_size'], 2])
+    z = np.mgrid[-0.999:0.999:0.6, -0.999:0.999:0.26].reshape(2,-1).T
     #z = np.square(1/z) * np.sign(z)
     #z = np.mgrid[0:1000:300, 0:1000:190].reshape(2,-1).T
     #z = np.mgrid[-0:1:0.3, 0:1:0.19].reshape(2,-1).T
@@ -25,12 +26,13 @@ def sample(sample_file, sess, config):
     #z = np.zeros(z_t.get_shape())
     #z.fill(0.2)
 
-    categories = np.zeros(categories_t.get_onshape())
+    #categories = np.zeros(categories_t.get_onshape())
+    
 
-    sample = sess.run(generator, feed_dict={z_t: z, dropout_t: 1.0, categories_t: categories})
-    print(np.shape(sample))
+    sample = sess.run(generator, feed_dict={z_t: z, dropout_t: 1.0})#, categories_t: categories})
+    print(np.shape(sample), np.min(sample), np.max(sample))
     #plot(self.config, sample, sample_file)
-    stacks = [np.hstack(sample[x*6:x*6+6]) for x in range(4)]
+    stacks = [np.hstack(sample[x*8:x*8+8]) for x in range(4)]
     plot(config, np.vstack(stacks), sample_file)
 
 

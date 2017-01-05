7d7ad1d59ac67f12d8816fa18d39d49f60bc70e2
Mikkel Garcia <mikkel@255bits.com>
2017-01-04 17:21:47 -0700

    [experiment] clip gradients more

--- hypergan/util/globals.py
+++ hypergan/util/globals.py
@@ -25,7 +25,7 @@ def capped_optimizer(optimizer, lr, loss, vars):
     if(grad == None):
         print("Warning: No gradient for variable ",var.name)
         return None
-    return (tf.clip_by_value(grad, -1., 1.), var)
+    return (tf.clip_by_value(grad, -0.00001, 0.00001), var)
   capped_gvs = [create_cap(grad,var) for grad, var in gvs]
   capped_gvs = [x for x in capped_gvs if x != None]
   return capped_optimizer.apply_gradients(capped_gvs)

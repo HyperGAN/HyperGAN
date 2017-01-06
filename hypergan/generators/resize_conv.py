81871180c30f64aafdac700c4d2d2993e81577b6
Martyn Garcia <martyn@255bits.com>
2017-01-05 23:47:30 -0700

    [feature] resize_conv tweaks

--- hypergan/generators/resize_conv.py
+++ hypergan/generators/resize_conv.py
@@ -18,33 +18,32 @@ def generator(config, net):
     batch_norm = config['generator.regularizers.layer']
 
     s = [int(x) for x in net.get_shape()]
-    net = block_conv(net, tf.nn.tanh, batch_size, 'identity', 'g_layers_init', output_channels=int(net.get_shape()[3]), filter=3, dropout=get_tensor('dropout'))
+    #net = block_conv(net, tf.nn.tanh, batch_size, 'identity', 'g_layers_init', output_channels=int(net.get_shape()[3]), filter=3, dropout=get_tensor('dropout'))
 
     for i in range(depth):
         s = [int(x) for x in net.get_shape()]
         layers = int(net.get_shape()[3])//depth_reduction
         if(i == depth-1):
-            layers=config['channels']
+            layers=config['channels']*2
         resized_wh=[s[1]*2, s[2]*2]
         if config['generator.layer.noise']:
             noise = [s[0],resized_wh[0],resized_wh[1],2**(depth-i)]
         else:
             noise = None
-        net = tf.image.resize_images(net, [resized_wh[0], resized_wh[1]], 1)
         fltr = 3
         if fltr > net.get_shape()[1]:
             fltr=int(net.get_shape()[1])
         if fltr > net.get_shape()[2]:
             fltr=int(net.get_shape()[2])
-        net = block_conv(net, activation, batch_size, 'identity', 'g_layers_'+str(i), output_channels=layers, filter=fltr, batch_norm=batch_norm, noise_shape=noise)
-        if(i == depth-1):
-            first3 = net
+        if(i == 0):
+            net = block_conv(net, tf.nn.tanh, batch_size, 'identity', 'g_layers_init', output_channels=int(net.get_shape()[3]), filter=3, dropout=get_tensor('dropout'), resize=[resized_wh[0], resized_wh[1]])
         else:
-            first3 = tf.slice(net, [0,0,0,0], [-1,-1,-1,3])
-        if batch_norm and i < depth - 1:
-            first3 = batch_norm(config['batch_size'], name='g_bn_first3_'+str(i))(first3)
+            net = block_conv(net, activation, batch_size, 'identity', 'g_layers_'+str(i), output_channels=layers, filter=fltr, batch_norm=batch_norm, noise_shape=noise, resize=[resized_wh[0], resized_wh[1]])
+
+        first3 = tf.slice(net, [0,0,0,0], [-1,-1,-1,3])
         first3 = config['generator.final_activation'](first3)
-        nets.append(first3)
+        last3 = tf.slice(net, [0,0,0,3], [-1,-1,-1,3])
+        nets.append(first3*tf.nn.sigmoid(last3))
         size = int(net.get_shape()[1])*int(net.get_shape()[2])*int(net.get_shape()[3])
         print("[generator] layer",net, size, target_size,"  with noise ",noise)
 

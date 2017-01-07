edce51a1976684e5a0cc1eef791d2c7aee72fc0d
Martyn Garcia <martyn@255bits.com>
2017-01-07 00:20:40 -0700

    [feature] densenet generator discriminator combo

--- hypergan/discriminators/densenet_discriminator.py
+++ hypergan/discriminators/densenet_discriminator.py
@@ -19,7 +19,7 @@ def discriminator(config, x, g, xs, gs):
       #    net = batch_norm(config['batch_size']*2, name='d_expand_bn_'+str(i))(net)
       #net = activation(net)
       # APPEND xs[i] and gs[i]
-      if(i < len(xs) and i > 0 and int(net.get_shape()[1]) >= 32):
+      if(i < len(xs) and i > 0):
         xg = tf.concat(0, [xs[i], gs[i]])
         xg += tf.random_normal(xg.get_shape(), mean=0, stddev=config['discriminator.noise_stddev']*(i+1), dtype=config['dtype'])
 
@@ -35,28 +35,33 @@ def discriminator(config, x, g, xs, gs):
           filter_size_h = int(net.get_shape()[2])
       filter = [1,filter_size_w,filter_size_h,1]
       stride = [1,filter_size_w,filter_size_h,1]
-      nets = []
       length = 2
       size_dense = 16
-      if i==0:
-          length = 1
+      #if i==0:
+      #    length = 1
       for j in range(length):
           print( j)
           net_dense = net
-          if batch_norm is not None:
-            net_dense = batch_norm(config['batch_size']*2, name='d_expand_bna_'+str(i*10+j))(net_dense)
-          net_dense = activation(net_dense)
-          nets.append(conv2d(net_dense, size_dense, name='d_expand_layear'+str(i*10+j), k_w=3, k_h=3, d_h=1, d_w=1))
-      net = tf.concat(3, [net]+nets)
+          if i > 0:
+              net_dense = activation(net_dense)
+              if batch_norm is not None:
+                net_dense = batch_norm(config['batch_size']*2, name='d_expand_bna_'+str(i*10+j))(net_dense)
+          newnet = conv2d(net_dense, size_dense, name='d_expand_layear'+str(i*10+j), k_w=3, k_h=3, d_h=1, d_w=1)
+          net = tf.concat(3, [net, newnet])
 
       net = tf.nn.avg_pool(net, ksize=filter, strides=stride, padding='SAME')
+
+      net = activation(net)
+      if batch_norm is not None:
+        net = batch_norm(config['batch_size']*2, name='d_expand_b2na_'+str(i*10+j))(net)
+      net = conv2d(net, int(net.get_shape()[3]), name='d_expand_la4year'+str(i*10+j), k_w=1, k_h=1, d_h=1, d_w=1)
 
       print('[discriminator] layer', net)
 
     k=-1
+    net = activation(net)
     if batch_norm is not None:
         net = batch_norm(config['batch_size']*2, name='d_expand_bn_end_'+str(i))(net)
-    net = activation(net)
     net = tf.reshape(net, [batch_size, -1])
  
     return net
--- hypergan/generators/resize_conv.py
+++ hypergan/generators/resize_conv.py
@@ -18,8 +18,9 @@ def generator(config, net):
     batch_norm = config['generator.regularizers.layer']
 
     s = [int(x) for x in net.get_shape()]
-    #net = block_conv(net, tf.nn.tanh, batch_size, 'identity', 'g_layers_init', output_channels=int(net.get_shape()[3]), filter=3, dropout=get_tensor('dropout'))
+    net = block_conv(net, tf.nn.tanh, batch_size, 'identity', 'g_layers_init', output_channels=int(net.get_shape()[3]), filter=3, dropout=get_tensor('dropout'))
 
+    z_proj = None
     for i in range(depth):
         s = [int(x) for x in net.get_shape()]
         layers = int(net.get_shape()[3])//depth_reduction
@@ -35,15 +36,27 @@ def generator(config, net):
             fltr=int(net.get_shape()[1])
         if fltr > net.get_shape()[2]:
             fltr=int(net.get_shape()[2])
-        if(i == 0):
-            net = block_conv(net, tf.nn.tanh, batch_size, 'identity', 'g_layers_init', output_channels=int(net.get_shape()[3]), filter=3, dropout=get_tensor('dropout'), resize=[resized_wh[0], resized_wh[1]])
-        else:
-            net = block_conv(net, activation, batch_size, 'identity', 'g_layers_'+str(i), output_channels=layers, filter=fltr, batch_norm=batch_norm, noise_shape=noise, resize=[resized_wh[0], resized_wh[1]])
+        #fltr size of 1?
+        net = block_conv(net, activation, batch_size, 'identity', 'g_layers_'+str(i), output_channels=layers, filter=3, batch_norm=batch_norm, noise_shape=noise, resize=[resized_wh[0], resized_wh[1]])
+
+        
+        dense_size = 32
+        if i < depth - 1:
+            for j in range(2):
+                net2 = block_conv(net, activation, batch_size, 'identity', 'g_layers_dense_'+str(j)+"_"+str(i), output_channels=dense_size, filter=1, batch_norm=batch_norm, noise_shape=noise)
+                net = tf.concat(3, [net, net2])
+        #first3 = block_conv(net, activation, batch_size, 'identity', 'g_layers2_'+str(i), output_channels=config['channels'], filter=fltr, batch_norm=batch_norm, noise_shape=noise, resize=[resized_wh[0], resized_wh[1]])
+        #last3 = block_conv(net, activation, batch_size, 'identity', 'g_layers3_'+str(i), output_channels=config['channels'], filter=fltr, batch_norm=batch_norm, noise_shape=noise, resize=[resized_wh[0], resized_wh[1]])
 
         first3 = tf.slice(net, [0,0,0,0], [-1,-1,-1,3])
-        first3 = config['generator.final_activation'](first3)
         last3 = tf.slice(net, [0,0,0,3], [-1,-1,-1,3])
-        nets.append(first3*tf.nn.sigmoid(last3))
+        if batch_norm:
+            first3 = batch_norm(config['batch_size'], name='g_bn_first3_'+str(i))(first3)
+            last3 = batch_norm(config['batch_size'], name='g_bn_last3'+str(i))(last3)
+        first3 = config['generator.final_activation'](first3)
+        last3 = tf.nn.sigmoid(last3)
+        first3 = first3*last3
+        nets.append(first3)
         size = int(net.get_shape()[1])*int(net.get_shape()[2])*int(net.get_shape()[3])
         print("[generator] layer",net, size, target_size,"  with noise ",noise)
 

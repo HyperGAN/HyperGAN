3a0d2814a343c4ae7ceb7960214346dd788df5a6
martyn <martyn.garcia@gmail.com>
2017-01-11 00:07:11 -0700

    Update __init__.py
    [fix] better defaults

--- hypergan/__init__.py
+++ hypergan/__init__.py
@@ -60,10 +60,10 @@ args = cli.parse_args()
 hc.set('dtype', tf.float32) #The data type to use in our GAN.  Only float32 is supported at the moment
 
 # Generator configuration
-hc.set("generator.z", 2) # the size of the encoding.  Encoder is set by the 'encoder' property, but could just be a random_uniform
+hc.set("generator.z", 40) # the size of the encoding.  Encoder is set by the 'encoder' property, but could just be a random_uniform
 
-hc.set("generator", [resize_conv.generator])
-hc.set("generator.z_projection_depth", 1024) # Used in the first layer - the linear projection of z
+hc.set("generator", [dense_resize_conv.generator])
+hc.set("generator.z_projection_depth", 512) # Used in the first layer - the linear projection of z
 
 hc.set("generator.activation", [prelu("g_")]); # activation function used inside the generator
 hc.set("generator.activation.end", [tf.nn.tanh]); # Last layer of G.  Should match the range of your input - typically -1 to 1

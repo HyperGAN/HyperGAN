e80eb20c41d822fcaf8f44403feabb84d3575ae8
Martyn Garcia <martyn@255bits.com>
2017-01-22 16:09:39 -0700

    [feature] add noise to x on g

--- examples/colorizer.py
+++ examples/colorizer.py
@@ -46,8 +46,10 @@ def add_bw(gan, net):
     x = tf.image.resize_images(x, shape, 1)
     print("Created bw ", x)
 
-    bw_net = tf.image.rgb_to_grayscale(x)
-    return bw_net
+    x = tf.image.rgb_to_grayscale(x)
+    x += tf.random_normal(x.get_shape(), mean=0, stddev=1e-1, dtype=config['dtype'])
+
+    return x
 
 args = parse_args()
 

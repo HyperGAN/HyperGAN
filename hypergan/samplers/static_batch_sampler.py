d5a8848337de78f863513e077abb73e039e1ab23
Mikkel Garcia <mikkel@255bits.com>
2017-02-02 16:47:10 -0700

    [fix] static batch sampler should lock classes too

--- hypergan/samplers/static_batch_sampler.py
+++ hypergan/samplers/static_batch_sampler.py
@@ -6,24 +6,24 @@ from hypergan.samplers.common import *
 
 #mask_noise = None
 z = None
+y = None
 def sample(sample_file, sess, config):
-    global z
+    global z, y
     generator = get_tensor("g")[0]
     y_t = get_tensor("y")
     z_t = get_tensor("z")
-    #mask_noise_t = get_tensor("mask_noise")
-    #categories_t = get_tensor("categories")[0]
 
     x = np.linspace(0,1, 4)
-    y = np.linspace(0,1, 6)
 
     if z is None:
-        z = np.random.uniform(-1, 1, [config['batch_size'], config['generator.z']])
+        z = sess.run(z_t)
+        y = sess.run(y_t)
+
 
     g=tf.get_default_graph()
     with g.as_default():
         tf.set_random_seed(1)
-        sample = sess.run(generator, feed_dict={z_t: z})
+        sample = sess.run(generator, feed_dict={z_t: z, y_t: y})
         #plot(self.config, sample, sample_file)
         stacks = [np.hstack(sample[x*8:x*8+8]) for x in range(4)]
         plot(config, np.vstack(stacks), sample_file)

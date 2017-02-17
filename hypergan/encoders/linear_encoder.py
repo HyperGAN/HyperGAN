4b65d57cdcb6e8a055f372ff21ca8fa857a12d10
Martyn Garcia <martyn@255bits.com>
2017-02-16 19:23:45 -0700

    [fix] add deprecated projections

--- hypergan/encoders/linear_encoder.py
+++ hypergan/encoders/linear_encoder.py
@@ -47,3 +47,12 @@ def gaussian(config, gan, z):
   rb = tf.sqrt(-2 * tf.log(za+TINY))*tf.sin(2*pi*zb)
 
   return tf.reshape(tf.concat(axis=1, values=[ra, rb]), z.get_shape())
+
+
+def periodic(config, gan, net):
+  return periodic_triangle_waveform(net, config.periods)
+
+def periodic_gaussian(config, gan, net):
+  net = periodic_triangle_waveform(net, config.periods)
+  return gaussian(config, gan, net)
+

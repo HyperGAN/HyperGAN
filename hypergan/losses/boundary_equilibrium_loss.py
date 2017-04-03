438b7565aa001c3b16e040afc6ccd56190bea779
Martyn Garcia <martyn@255bits.com>
2017-04-03 05:03:08 -0600

    [feature] boundary equilibrium loss initial pseudocode

---
+++ hypergan/losses/boundary_equilibrium_loss.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from hypergan.util.ops import *
+from hypergan.util.hc_tf import *
+import hyperchamber as hc
+
+def config(
+        reduce=tf.reduce_mean, 
+        reverse=False,
+        discriminator=None
+    ):
+    selector = hc.Selector()
+    selector.set("reduce", reduce)
+    selector.set('reverse', reverse)
+    selector.set('discriminator', discriminator)
+    selector.set('gamma', 0.001)
+
+    selector.set('create', create)
+
+    return selector.random_config()
+
+def autoencode(gan, x):
+    #TODO g() not defined
+    #TODO encode() not defined
+    return g(encode(x))
+
+def loss(gan, g_or_x):
+    return g_or_x - autoencode(gan, g_or_x)
+
+def create(config, gan):
+    net = config.reduce(net, axis=1)
+    gamma = config.gamma
+    #TODO not verified
+    gan.graph.k=tf.get_variable('k', loss_shape, initializer=tf.constant_initializer(0))
+    d_loss = loss(gan, x)-gan.graph.k*loss(gan, g(z_d))
+    g_loss = loss(gan, g(z_g))
+    gan.graph.k += gan.graph.k + k_lambda * (gamma*loss(gan, x) - loss(gan, g(z_g)))
+
+    #TODO the paper says argmin(d_loss) and argmin(g_loss).  Is `argmin` a hyperparam?
+
+    return [d_loss, g_loss]

ac2e9f8521817f1dbcf658a1f93ba352171f6b94
Martyn Garcia <martyn@255bits.com>
2017-04-03 19:42:47 -0600

    [fix] labels search

--- examples/2d-measure-accuracy.py
+++ examples/2d-measure-accuracy.py
@@ -319,8 +319,18 @@ def train():
         'k_lambda':[0.1, 0.01, 0.001, 1e-4, 1e-5],
         'initial_k':[1,0,0.5,0.1,1e-2,1e-3],
         'reduce': [tf.reduce_mean,hg.losses.wgan_loss.linear_projection,tf.reduce_sum,tf.reduce_logsumexp, tf.argmin]
-
-            }
+        'labels': [
+            [-1, 1, 0],
+            [0, 1, 1],
+            [0, -1, -1],
+            [1, -1, 0],
+            [0, -1, 1],
+            [0, 1, -1],
+            [0, 0.5, -0.5],
+            [0.5, -0.5, 0],
+            [0.5, 0, -0.5]
+        ]
+    }
     #losses.append([hg.losses.wgan_loss.config(**wgan_loss_opts)])
     #losses.append([hg.losses.wgan_loss.config(**wgan_loss_opts)])
     #losses.append([hg.losses.lamb_gan_loss.config(**lamb_loss_opts)])

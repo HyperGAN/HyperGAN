828b62f2b2cedfff947d1c82d186cc6b0927e1fa
Martyn Garcia <martyn@255bits.com>
2017-05-27 00:12:28 -0600

    [feature] larger search space

--- hypergan/search/random_search.py
+++ hypergan/search/random_search.py
@@ -21,49 +21,11 @@ class RandomSearch:
             'encoders':self.encoders()
          }
 
+    def range(self):
+        return list(np.linspace(0, 1, num=1000000))
+
     def trainers(self):
         trainers = []
-
-        rms_opts = {
-            'g_momentum': [0,0.1,0.01,1e-6,1e-5,1e-1,0.9,0.999, 0.5],
-            'd_momentum': [0,0.1,0.01,1e-6,1e-5,1e-1,0.9,0.999, 0.5],
-            'd_decay': [0.8, 0.9, 0.99,0.999,0.995,0.9999,1],
-            'g_decay': [0.8, 0.9, 0.99,0.999,0.995,0.9999,1],
-            'clipped_gradients': [False, 1e-2],
-            'clipped_d_weights': [False, 1e-2],
-            'd_learn_rate': [1e-3,1e-4,5e-4,1e-6,4e-4, 5e-5],
-            'g_learn_rate': [1e-3,1e-4,5e-4,1e-6,4e-4, 5e-5]
-        }
-
-        stable_rms_opts = {
-            "clipped_d_weights": 0.01,
-            "clipped_gradients": False,
-            "d_decay": 0.995, "d_momentum": 1e-05,
-            "d_learn_rate": 0.001,
-            "g_decay": 0.995,
-            "g_momentum": 1e-06,
-            "g_learn_rate": 0.0005,
-        }
-
-        #trainers.append(hg.trainers.rmsprop_trainer.config(**rms_opts))
-
-        adam_opts = {}
-
-        adam_opts = {
-            'd_learn_rate': [1e-3,1e-4,5e-4,1e-2,1e-6],
-            'g_learn_rate': [1e-3,1e-4,5e-4,1e-2,1e-6],
-            'd_beta1': [0.9, 0.99, 0.999, 0.1, 0.01, 0.2, 1e-8],
-            'd_beta2': [0.9, 0.99, 0.999, 0.1, 0.01, 0.2, 1e-8],
-            'g_beta1': [0.9, 0.99, 0.999, 0.1, 0.01, 0.2, 1e-8],
-            'g_beta2': [0.9, 0.99, 0.999, 0.1, 0.01, 0.2, 1e-8],
-            'd_epsilon': [1e-8, 1, 0.1, 0.5],
-            'g_epsilon': [1e-8, 1, 0.1, 0.5],
-            'd_clipped_weights': [False, 0.01],
-            'clipped_gradients': [False, 0.01]
-        }
-
-        #trainers.append(hg.trainers.adam_trainer.config(**adam_opts))
-
         any_opts = {}
 
         tftrainers = [
@@ -71,50 +33,60 @@ class RandomSearch:
                 tf.train.AdagradOptimizer,
                 tf.train.GradientDescentOptimizer,
                 tf.train.AdamOptimizer,
-
+                tf.train.AdagradDAOptimizer, # TODO missing param
+                tf.train.MomentumOptimizer,
+                tf.train.ProximalGradientDescentOptimizer,
+                #tf.train.FtrlOptimizer,
+                tf.train.RMSPropOptimizer,
+                tf.train.ProximalAdagradOptimizer,
         ]
-        # TODO FtrlOptimizer
-        # TODO ProximalAdagradOptimizer
-        # TODO ProximalGradientDescentOptimizer
 
-        any_opts = {
-            'd_learn_rate': [1e-3,1e-4,5e-4,1e-2,1e-6],
-            'g_learn_rate': [1e-3,1e-4,5e-4,1e-2,1e-6],
-            'd_beta1': [0.9, 0.99, 0.999, 0.1, 0.01, 0.2, 1e-8],
-            'd_beta2': [0.9, 0.99, 0.999, 0.1, 0.01, 0.2, 1e-8],
-            'g_beta1': [0.9, 0.99, 0.999, 0.1, 0.01, 0.2, 1e-8],
-            'g_beta2': [0.9, 0.99, 0.999, 0.1, 0.01, 0.2, 1e-8],
-            'd_epsilon': [1e-8, 1, 0.1, 0.5],
-            'g_epsilon': [1e-8, 1, 0.1, 0.5],
-            'g_momentum': [0,0.1,0.01,1e-6,1e-5,1e-1,0.9,0.999, 0.5],
-            'd_momentum': [0,0.1,0.01,1e-6,1e-5,1e-1,0.9,0.999, 0.5],
-            'd_decay': [0.8, 0.9, 0.99,0.999,0.995,0.9999,1],
-            'g_decay': [0.8, 0.9, 0.99,0.999,0.995,0.9999,1],
-            'd_rho': [0.99,0.9,0.95,0.1,0.01,0],
-            'g_rho': [0.99,0.9,0.95,0.1,0.01,0],
-            'd_initial_accumulator_value': [0.99,0.9,0.95,0.1,0.01],
-            'g_initial_accumulator_value': [0.99,0.9,0.95,0.1,0.01],
+        selector = hc.Selector({
+            'd_learn_rate': self.range(),
+            'g_learn_rate': self.range(),
+            'd_beta1': self.range(),
+            'd_beta2': self.range(),
+            'g_beta1': self.range(),
+            'g_beta2': self.range(),
+            'd_epsilon': self.range(),
+            'g_epsilon': self.range(),
+            'g_momentum': self.range(),
+            'd_momentum': self.range(),
+            'd_decay': self.range(),
+            'g_decay': self.range(),
+            'd_rho': self.range(),
+            'g_rho': self.range(),
+            'd_global_step': self.range(),
+            'g_global_step': self.range(),
+            'd_initial_accumulator_value': self.range(),
+            'g_initial_accumulator_value': self.range(),
+            'd_initial_gradient_squared_accumulator_value': self.range(),
+            'g_initial_gradient_squared_accumulator_value': self.range(),
+            'd_initial_gradient_squared_accumulator_value': self.range(),
+            'g_initial_gradient_squared_accumulator_value': self.range(),
             'd_clipped_weights': False,
             'clipped_gradients': False,
             'd_trainer':tftrainers,
-            'g_trainer':tftrainers
-        }
+            'g_trainer':tftrainers,
+            'create': [
+                hg.trainers.proportional_control_trainer.create,
+                hg.trainers.alternating_trainer.create
+            ],
+            'run': [
+                hg.trainers.proportional_control_trainer.run,
+                hg.trainers.alternating_trainer.run
+            ]
+        })
+        config = selector.random_config()
 
-        trainers.append(hg.trainers.proportional_control_trainer.config(**any_opts))
-        #trainers.append(hg.trainers.alternating_trainer.config(**any_opts))
-        return trainers
+
+        return [config]
      
     def losses(self):
-        losses = []
-
-        wgan_loss_opts = {
+        loss_opts = {
             'reverse':[True, False],
             'reduce': [tf.reduce_mean,hg.losses.wgan_loss.linear_projection,tf.reduce_sum,tf.reduce_logsumexp],
-            'gradient_penalty': list(np.arange(1, 100))
-        }
-        lamb_loss_opts = {
-            'reverse':[True, False],
-            'reduce': [tf.reduce_mean,hg.losses.wgan_loss.linear_projection,tf.reduce_sum,tf.reduce_logsumexp],
+            'gradient_penalty': False,
             'labels': [
                 [-1, 1, 0],
                 [0, 1, 1],
@@ -126,106 +98,25 @@ class RandomSearch:
                 [0.5, -0.5, 0],
                 [0.5, 0, -0.5]
             ],
-            'alpha':[0,1e-3,1e-2,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.99,0.999],
-            'beta':[0,1e-3,1e-2,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.99,0.999]
-
-        }
-        lsgan_loss_opts = {
-            'reduce': [tf.reduce_mean,hg.losses.wgan_loss.linear_projection,tf.reduce_sum,tf.reduce_logsumexp],
-            'labels': [
-                [-1, 1, 0],
-                [0, 1, 1],
-                [0, -1, -1],
-                [1, -1, 0],
-                [0, -1, 1],
-                [0, 1, -1],
-                [0, 0.5, -0.5],
-                [0.5, -0.5, 0],
-                [0.5, 0, -0.5]
-            ],
-            'gradient_penalty': [False, 1, 0.1, 0.01, 0.001, 0.0001, 1e-5]
-        }
-        standard_loss_opts= {
-            'reduce': [tf.reduce_mean,hg.losses.wgan_loss.linear_projection,tf.reduce_sum,tf.reduce_logsumexp],
-        'label_smooth': list(np.linspace(0, 1, num=20))
-        }
-        stable_loss_opts = {
-          "alpha": 0.5,
-          "beta": [0.5, 0.8],
-          "discriminator": None,
-          "label_smooth": 0.26111111111111107,
-          "labels": [[
-            0,
-            -1,
-            -1
-          ]],
-          "reduce": "function:tensorflow.python.ops.math_ops.reduce_mean",
-          "reverse": True
-        }
-        began_loss_opts = {
-            'k_lambda':[0.1, 0.01, 0.001, 1e-4, 1e-5],
-
-            'initial_k':[0],
-            'reduce': [tf.reduce_mean,hg.losses.wgan_loss.linear_projection,tf.reduce_sum,tf.reduce_logsumexp, tf.argmin],
-            'labels': [
-                [-1, 1, 0],
-                [0, 1, 1],
-                [0, -1, -1],
-                [1, -1, 0],
-                [0, -1, 1],
-                [0, 1, -1],
-                [0, 0.5, -0.5],
-                [0.5, -0.5, 0],
-                [0.5, 0, -0.5],
-                [-0.5, 0.5, 0.5],
-                [0.5, 0.5, 0],
-                [-0.5, -0.5, 0.5],
-                [-1, 1, 1],
-                [1, -1, -1],
-                [0, 1, 1],
-                [0, -1, -1]
-            ],
-            'gradient_penalty': [False],
-            'use_k': True,
-            'gamma':list(np.linspace(0, 1, num=1000))
+            'alpha':self.range(),
+            'beta':self.range(),
+            'gamma':self.range(),
+            'label_smooth': self.range(),
+            'use_k': [False, True],
+            'initial_k': self.range(),
+            'k_lambda': self.range(),
+            'create': [
+                hg.losses.boundary_equilibrium_loss.create,
+                hg.losses.lamb_gan_loss.create,
+                hg.losses.lsgan_loss.create,
+                hg.losses.standard_gan_loss.create,
+                hg.losses.wgan_loss.create
+            ]
         }
 
-        #losses.append([hg.losses.wgan_loss.config(**wgan_loss_opts)])
-        #losses.append([hg.losses.wgan_loss.config(**wgan_loss_opts)])
-        #losses.append([hg.losses.lamb_gan_loss.config(**lamb_loss_opts)])
-        #losses.append([hg.losses.lamb_gan_loss.config(**stable_loss_opts)])
-        #losses.append([hg.losses.lamb_gan_loss.config(**stable_loss_opts)])
-        #losses.append([hg.losses.lsgan_loss.config(**lsgan_loss_opts)])
-        #losses.append([hg.losses.boundary_equilibrium_loss.config(**began_loss_opts)])
+        config = hc.Selector(loss_opts).random_config()
 
-        stable_loss = [{
-            "create": "function:hypergan.losses.boundary_equilibrium_loss.create",
-            "discriminator": None,
-            "gamma": 0.4,
-            "gradient_penalty": False,
-            "initial_k": 0.01,
-            "k_lambda": 0.002,
-            "labels": [
-                0.5,
-                -0.5,
-                -0.5
-            ],
-            "reduce": "function:tensorflow.python.ops.math_ops.reduce_mean",
-            "reverse": False,
-            "type": "lsgan",
-            "use_k": True
-        }]
-
-
-        losses.append(stable_loss)
-
-
-        #losses.append([hg.losses.wgan_loss.config(**wgan_loss_opts)])
-        #losses.append([hg.losses.lamb_gan_loss.config(**lamb_loss_opts)])
-        #losses.append([hg.losses.standard_gan_loss.config(**standard_loss_opts)])
-        #losses.append([hg.losses.lsgan_loss.config(**lsgan_loss_opts)])
-
-        return losses
+        return [[config]]
 
     def encoders(self):
         encoders = []
@@ -267,7 +158,6 @@ class RandomSearch:
         selector = hc.Selector()
         for key,value in self.options.items():
             selector.set(key, value)
-            print("Set ", key, value)
         
         custom = selector.random_config()
 

821a73a382ea242b3e34bc0e5aeb0189bf105ae0
Martyn Garcia <martyn@255bits.com>
2017-05-29 14:58:19 -0600

    [fix] some tests, create some more failing ones

--- hypergan/losses/aligned_loss.py
+++ hypergan/losses/aligned_loss.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-from hypergan.util.ops import *
-from hypergan.util.hc_tf import *
 import hyperchamber as hc
 from hypergan.generators.resize_conv_generator import minmaxzero
 from hypergan.losses.common import *
--- hypergan/samplers/aligned_began_sampler.py
+++ hypergan/samplers/aligned_began_sampler.py
@@ -1,6 +1,4 @@
 
-from hypergan.util.ops import *
-
 from hypergan.samplers.common import *
 
 def sample_tensor(sess,generator, feed_dict, sample_file):

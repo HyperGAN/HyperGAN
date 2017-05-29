edcf9025ed44e6a8a2ed33e23c48591a33544227
Martyn Garcia <martyn@255bits.com>
2017-05-29 17:20:19 -0600

    [fix] add missing files

---
+++ tests/encoders/match_discriminator_encoder_test.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import hyperchamber as hc
+import numpy as np
+import hypergan as hg
+from hypergan.encoders.match_discriminator_encoder import MatchDiscriminatorEncoder
+from hypergan.ops import TensorflowOps
+
+from unittest.mock import MagicMock
+
+class MockOps:
+    def __init__(self):
+        self.mock = True
+
+class MockTrainer:
+    def __init__(self):
+        self.mock = True
+
+graph = hc.Config({
+    'x': tf.constant(1., shape=[32,32,32])
+})
+gan = hg.GAN(graph = graph, ops = MockOps, config = {})
+
+class MatchDiscriminatorEncoderTest(tf.test.TestCase):
+    def test_create(self):
+        with self.test_session():
+            subject = MatchDiscriminatorEncoder({})
+            
+            z = subject.create(gan)
+            self.assertEqual(z.get_shape()[1], [])
+
+if __name__ == "__main__":
+    tf.test.main()

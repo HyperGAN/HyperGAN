12ef11dbb5c8012a941ffcf64ab04e278b9c443b
Martyn Garcia <martyn@255bits.com>
2017-06-02 21:01:13 -0600

    [test] component weights and biases

--- tests/gan_component_test.py
+++ tests/gan_component_test.py
@@ -11,6 +11,7 @@ from unittest.mock import MagicMock
 
 class MockOps:
     pass
+
 gan = hc.Config({'ops': MockOps})
 component = GANComponent(gan=gan, config={'test':True})
 class GanComponentTest(tf.test.TestCase):
@@ -21,11 +22,11 @@ class GanComponentTest(tf.test.TestCase):
     def test_validate(self):
         with self.test_session():
             self.assertEqual(component.validate(), [])
-    
+
     def test_gan(self):
         with self.test_session():
             self.assertEqual(component.gan, gan)
-    
+
     def test_ops(self):
         with self.test_session():
             self.assertEqual(type(component.ops), MockOps)
@@ -34,5 +35,11 @@ class GanComponentTest(tf.test.TestCase):
         with self.assertRaises(ValidationException):
             GANComponent(config={}, gan=None)
 
+    def test_proxy_methods(self):
+        component = GANComponent(gan=gan, config={'test':True})
+        with self.test_session():
+            self.assertEqual(component.weights, [])
+            self.assertEqual(component.biases, [])
+            self.assertEqual(component.variables(), [])
 if __name__ == "__main__":
     tf.test.main()

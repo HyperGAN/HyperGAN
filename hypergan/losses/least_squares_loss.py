2ad6247e199a2373919e9b4c66cac95351fd8c07
Martyn Garcia <martyn@255bits.com>
2017-06-10 08:22:29 -0600

    [fix] least squares loss and reference configuration

--- hypergan/losses/least_squares_loss.py
+++ hypergan/losses/least_squares_loss.py
@@ -6,9 +6,11 @@ from hypergan.losses.base_loss import BaseLoss
 
 class LeastSquaresLoss(BaseLoss):
 
+    def required(self):
+        return "labels".split()
+
     def _create(self, d_real, d_fake):
-        ops = self.ops
-        ops.describe("LeastSquaresLoss")
+        ops = self.gan.ops
         config = self.config
 
         d_real = config.reduce(d_real, axis = 1)
@@ -16,7 +18,7 @@ class LeastSquaresLoss(BaseLoss):
 
         a,b,c = config.labels
         square = ops.lookup('square')
-        d_loss = square(d_real - b) + square(d_fake - a)
-        g_loss = square(d_fake - c)
+        d_loss = 0.5*square(d_real - b) + 0.5*square(d_fake - a)
+        g_loss = 0.5*square(d_fake - c)
 
         return [d_loss, g_loss]

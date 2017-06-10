1c8d0bdbb2c66d4e68b31caad482fe2cb93c9303
Mikkel Garcia <mikkel@255bits.com>
2017-06-09 21:17:18 -0600

    [feature] support other base configs

--- hypergan/configuration.py
+++ hypergan/configuration.py
@@ -4,7 +4,11 @@ import os
 class Configuration:
     def find(configuration):
         dirname = os.path.dirname(os.path.realpath(__file__))
-        return dirname + "/configurations/"+configuration
+        paths = [dirname + "/configurations/", '~/.hypergan/configs/']
+        for path in paths:
+            file_path = path + configuration
+            if os.path.exists(file_path):
+                return file_path
     def load(configuration):
         return hc.Selector().load(Configuration.find(configuration))
     def default():

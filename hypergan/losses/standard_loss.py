1ea7369de4b3e4caf481b31520b9cbeb31b78d36
Mikkel Garcia <mikkel@255bits.com>
2017-06-09 21:20:22 -0600

    [feature] standard loss working

--- hypergan/losses/standard_loss.py
+++ hypergan/losses/standard_loss.py
@@ -6,7 +6,7 @@ from hypergan.losses.base_loss import BaseLoss
 class StandardLoss(BaseLoss):
 
     def required(self):
-        return "label_smooth alpha beta".split()
+        return "label_smooth reduce".split()
 
     def _create(self, d_real, d_fake):
         ops = self.ops
@@ -16,9 +16,16 @@ class StandardLoss(BaseLoss):
         d_real = config.reduce(d_real, axis = 1)
         d_fake = config.reduce(d_fake, axis = 1)
 
-        zeros = tf.zeros_like(d_fake, dtype=gan.config.dtype)
-        g_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=d_fake, labels=zeros)
-        d_loss = self.sigmoid_kl_with_logits(d_real, 1.-config.label_smooth)
+        zeros = tf.zeros_like(d_fake)
+        ones = tf.ones_like(d_fake)
+        if config.improved_gan:
+            g_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=d_fake, labels=zeros)
+            d_loss = self.sigmoid_kl_with_logits(d_real, 1.-config.label_smooth)
+            #TODO missing a piece? Oops
+        else:
+            g_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=d_fake, labels=zeros)
+            d_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=d_real, labels=zeros) + \
+                     tf.nn.sigmoid_cross_entropy_with_logits(logits=d_fake, labels=ones)
 
         return [d_loss, g_loss]
 

53144b47d88b8ef787de5074fdb1c12863e50a80
Martyn Garcia <martyn@255bits.com>
2017-06-25 15:43:03 -0600

    [fix] some defaults

--- hypergan/gans/alpha_gan.py
+++ hypergan/gans/alpha_gan.py
@@ -54,7 +54,6 @@ class AlphaGAN(BaseGAN):
             d2 = dict(config.discriminator)
             d2['class'] = self.ops.lookup("class:hypergan.discriminators.pyramid_discriminator.PyramidDiscriminator")
             d2["fc_layers"]=config.discriminator.encoder_fc_layers or 0
-            d2['final_activation'] = ops.lookup('tanh')
             encoder = self.create_component(d2)
             encoder.ops.describe("encoder")
             encoder.create(self.inputs.x)
@@ -67,7 +66,8 @@ class AlphaGAN(BaseGAN):
 #            d3["fc_layer_size"]=512
 #            d3["fc_layers"]=0
 #            d3['layer_filter']=None
-            d3["layers"]=config.discriminator.encoder_layers or 3
+            d3["layers"]=config.discriminator.encoder_layers or 0
+            d3["extra_layers"]=config.discriminator.encoder_extra_layers or 2
 
             encoder_discriminator = self.create_component(d3)
             #encoder_discriminator = FullyConnectedDiscriminator(self, {})

332c56a13d74988c5f0f81deb08a68920b53e4ad
Mikkel Garcia <mikkel@255bits.com>
2017-06-27 19:09:22 -0600

    [feature] more explicit config file loading

--- hypergan/configuration.py
+++ hypergan/configuration.py
@@ -4,12 +4,19 @@ import os
 class Configuration:
     def find(configuration):
         dirname = os.path.dirname(os.path.realpath(__file__))
-        paths = [dirname + "/configurations/", os.path.abspath(os.path.expanduser('~/.hypergan/configs/'))+'/']
+        paths = [dirname + "/configurations/", os.path.abspath(os.path.expanduser('~/.hypergan/configs/'))+'/',
+                 os.path.abspath(os.path.relpath("."))+"/" ]
+        Configuration.paths = paths
         for path in paths:
             file_path = path + configuration
+            file_path = os.path.realpath(file_path)
             if os.path.exists(file_path):
                 return file_path
     def load(configuration):
-        return hc.Selector().load(Configuration.find(configuration))
+        config_file = Configuration.find(configuration)
+        if config_file is None:
+            print("[hypergan] Could not find config named:", configuration, "checked paths", Configuration.paths)
+        print("[hypergan] Loading config", config_file)
+        return hc.Selector().load(config_file)
     def default():
         return Configuration.load('default.json')

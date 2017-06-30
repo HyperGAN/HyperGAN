dde3bbd361b4c7014ddb3ce586f6224826c0ecdd
Martyn Garcia <martyn@255bits.com>
2017-06-30 00:16:50 -0600

    [fix] another test

--- tests/samplers/grid_sampler_test.py
+++ tests/samplers/grid_sampler_test.py
@@ -15,6 +15,7 @@ class GridSamplerTest(tf.test.TestCase):
         with self.test_session():
             gan = mock_gan(batch_size=32)
             gan.encoder = UniformEncoder(gan, {'z':2, 'min': -1, 'max': 1, 'projections':['identity']})
+            gan.encoder.create()
             gan.create()
 
             sampler = GridSampler(gan)

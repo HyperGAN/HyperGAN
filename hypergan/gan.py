5555a5ef2ec717f65fd69703d0241399461da3cd
Martyn Garcia <martyn@255bits.com>
2017-07-03 17:28:11 -0600

    [feature] use alphagan when using an alphagan configuration file(generic solution)

--- hypergan/gan.py
+++ hypergan/gan.py
@@ -1,4 +1,17 @@
 from hypergan.gans.standard_gan import StandardGAN
+from hypergan.ops.tensorflow.ops import TensorflowOps
 
-GAN=StandardGAN
+def gan_factory(*args, **kw_args):
+    if 'config' in kw_args:
+        config = kw_args['config']
+        print("kw_args", kw_args['config']['class'])
+    elif len(args) > 0:
+        config = args[0]
+    else:
+        config = None
+    if config and 'class' in config:
+        return TensorflowOps.lookup_function(None, config['class'])(*args, **kw_args)
+    else:
+        return StandardGAN(*args, **kw_args)
 
+GAN=gan_factory

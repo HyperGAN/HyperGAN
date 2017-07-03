6b10161991f8e174f5749d8844a8149a240cb042
Martyn Garcia <martyn@255bits.com>
2017-07-03 16:44:51 -0600

    [fix] sequence gan and chargan

--- examples/sequence.py
+++ examples/sequence.py
@@ -13,30 +13,6 @@ import numpy as np
 
 import math
 
-def text_plot(size, filename, data, x):
-    plt.clf()
-    plt.figure(figsize=(2,2))
-    data = np.squeeze(data)
-    plt.plot(x)
-    plt.plot(data)
-    plt.xlim([0, size])
-    plt.ylim([-2, 2.])
-    plt.ylabel("Amplitude")
-    plt.xlabel("Time")
-    plt.savefig(filename)
-
-def get_vocabulary():
-    lookup_keys = list("~()\"'&+#@/789zyxwvutsrqponmlkjihgfedcba ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456:-,;!?.")
-    lookup_values = np.arange(len(lookup_keys), dtype=np.float32)
-
-    lookup = {}
-
-    for i, key in enumerate(lookup_keys):
-        lookup[key]=lookup_values[i]
-
-    return lookup_keys, lookup
-
-
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a sequence of characters using modes (real valued) output.  Related to chargan', add_help=True)
     parser.add_argument('--batch_size', '-b', type=int, default=32, help='Examples to include in each batch.  If using batch norm, this needs to be preserved when in server mode')
@@ -46,15 +22,17 @@ def parse_args():
     parser.add_argument('--distribution', '-t', type=str, default='circle', help='what distribution to test, options are circle, modes')
     parser.add_argument('--sample_every', type=int, default=50, help='Samples the model every n epochs.')
     parser.add_argument('--save_every', type=int, default=30000, help='Saves the model every n epochs.')
+    parser.add_argument('--one_hot', action='store_true', help='Use character one-hot encodings.')
     return parser.parse_args()
 
 args = parse_args()
+one_hot = args.one_hot
 
 def train():
     config = hg.configuration.Configuration.load(args.config + '.json')
 
     with tf.device(args.device):
-        text_input = TextInput(config, args.batch_size, vocabulary = get_vocabulary)
+        text_input = TextInput(config, args.batch_size, one_hot=one_hot)
         gan = hg.GAN(config, inputs=text_input)
         gan.create()
 
@@ -85,51 +63,25 @@ def train():
         tf.train.start_queue_runners(sess=gan.session)
 
         limit = 10000
-        lookup_keys, lookup = get_vocabulary()
+        vocabulary = text_input.get_vocabulary()
 
-        lookup =  {i[1]:i[0] for i in lookup.items()} # reverse hash
         if args.config:
             limit = 10000000
         for i in range(limit):
             gan.step()
 
-            def sample_output(val):
-                val *= len(lookup_keys)/2.0
-                val += len(lookup_keys)/2.0
-                val = np.round(val)
-
-                val = np.maximum(0, val)
-                val = np.minimum(len(lookup_keys)-1, val)
-
-                ox_val = [lookup[obj] for obj in list(val)]
-                string = "".join(ox_val)
-                return string
-
 
             if i % args.sample_every == 0 and i > 0:
-                g, x_val, reconstruction = gan.session.run([gan.generator.sample, gan.inputs.x, gan.discriminator.reconstruction], {gan.encoder.z: z_0})
+                g, x_val = gan.session.run([gan.generator.sample, gan.inputs.x], {gan.encoder.z: z_0})
                 bs = np.shape(x_val)[0]
-                x_val = x_val.reshape([bs,-1])
-                g = g.reshape([bs,-1])
-                reconstruction = reconstruction.reshape([bs,-1])
-                sample_file="samples/%06d.png" % (samples)
-                #text_plot(64, sample_file, g[0], x_0[0])
                 samples+=1
-                print("X: "+sample_output(x_val[0]))
-                print("RX: "+sample_output(reconstruction[0]))
+                print("X: "+text_input.sample_output(x_val[0]))
                 print("G:")
                 for j, g0 in enumerate(g):
                     if j > 4:
                         break
 
-                    print(sample_output(g0))
-
-            if i % args.save_every == 0 and i > 0 and args.config is not None:
-                pass
-                #print("Saving " + save_file)
-                #with tf.device('/cpu:0'):
-                #    gan.save(save_file)
-
+                    print(text_input.sample_output(g0))
 
         if args.config is None:
             with open("sequence-results-10k.csv", "a") as myfile:

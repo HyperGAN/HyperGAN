0ee33e8f038105b04fb55458be83f18408e03595
Mikkel Garcia <mikkel@255bits.com>
2017-07-04 12:29:41 -0600

    [fix] cleanup hypergan new -l

--- hypergan/configuration.py
+++ hypergan/configuration.py
@@ -16,11 +16,12 @@ class Configuration:
             file_path = os.path.realpath(file_path)
             if os.path.exists(file_path):
                 return file_path
-    def load(configuration):
+    def load(configuration, verbose=True):
         config_file = Configuration.find(configuration)
         if config_file is None:
             print("[hypergan] Could not find config named:", configuration, "checked paths", Configuration.paths)
-        print("[hypergan] Loading config", config_file)
+        if verbose:
+          print("[hypergan] Loading config", config_file)
         return hc.Selector().load(config_file)
     def default():
         return Configuration.load('default.json')

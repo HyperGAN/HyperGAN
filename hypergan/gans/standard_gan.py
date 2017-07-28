d98fe3a24ba550688ccb3e221340c0b187f5f0c8
Mikkel Garcia <mikkel@255bits.com>
2017-07-28 01:05:53 -0600

    [feature] weight_norm deconv

--- hypergan/gans/standard_gan.py
+++ hypergan/gans/standard_gan.py
@@ -70,6 +70,8 @@ class StandardGAN(BaseGAN):
             if self.generator is None and config.generator:
                 self.generator = self.create_component(config.generator)
                 create_if(self.generator)
+            self.uniform_sample = self.generator.sample
+
             if self.discriminator is None and config.discriminator:
                 self.discriminator = self.create_component(config.discriminator)
                 self.discriminator.ops.describe("discriminator")
@@ -81,8 +83,6 @@ class StandardGAN(BaseGAN):
                 self.trainer = self.create_component(config.trainer)
                 create_if(self.trainer)
 
-            if self.generator and hasattr(self.generator, 'sample'):
-                self.uniform_sample = self.generator.sample
 
             self.session.run(tf.global_variables_initializer())
 

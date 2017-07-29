f0294bca275c8e6bcb05df28da663873c3a71984
Mikkel Garcia <mikkel@255bits.com>
2017-07-28 21:47:15 -0600

    [feature] generator scripts for readme

---
+++ hypergan/configurations/generate_samples.py
@@ -0,0 +1,21 @@
+import hypergan as hg
+from subprocess import call
+
+import glob
+
+filenames = glob.glob("*.json")
+
+for f in filenames:
+    config = f.split(".")[0]
+
+    cmd="CUDA_VISIBLE_DEVICES=0 hypergan train /ml/datasets/faces/128x128/all --sample_every 100 --sampler debug --format jpg --size 64x64x3 -b 8 -c "+config+" --resize --save_samples --steps 1000"
+    print(cmd)
+    result = call(cmd, shell=True)
+
+    cmd="mkdir samples/"+config
+    result = call(cmd, shell=True)
+
+    cmd="mv samples/*.png samples/"+config
+    result = call(cmd, shell=True)
+
+

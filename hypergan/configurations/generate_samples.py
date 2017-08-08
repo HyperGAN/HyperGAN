254c6558ca737109d86e9011cbc0d35ce1ed009e
Mikkel Garcia <mikkel@255bits.com>
2017-08-07 22:56:28 -0600

    [feature] update samples

--- hypergan/configurations/generate_samples.py
+++ hypergan/configurations/generate_samples.py
@@ -10,7 +10,7 @@ result = call(cmd, shell=True)
 for f in filenames:
     config = f.split(".")[0]
 
-    cmd="CUDA_VISIBLE_DEVICES=0 hypergan train /ml/datasets/faces/128x128/all --sample_every 8000 --sampler debug --format jpg --size 64x64x3 -b 8 -c ./"+config+" --resize --save_samples --steps 16001"
+    cmd="CUDA_VISIBLE_DEVICES=0 hypergan train /ml/datasets/faces/128x128/all --sample_every 8000 --sampler debug --format jpg --size 64x64x3 -b 8 -c ./"+config+" --resize --save_samples --steps 16001 --save_every 1000000"
     print(cmd)
     result = call(cmd, shell=True)
 

636074bcede784b94684cc2686615a2ab0150cc9
Martyn Garcia <martyn@255bits.com>
2017-08-13 11:38:33 -0600

    [fix] use gradient penalty of 10

--- hypergan/search/random_search.py
+++ hypergan/search/random_search.py
@@ -76,7 +76,7 @@ class RandomSearch:
         loss_opts = {
             'reverse':[True, False],
             'reduce': ['reduce_mean','reduce_sum','reduce_logsumexp'],
-            'gradient_penalty': [True],
+            'gradient_penalty': [10],
             'gradient_penalty_type': 'dragan',
             'labels': [
                 [0, 1, 1]

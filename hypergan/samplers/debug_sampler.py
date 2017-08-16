580a6ebaa43e6776abcde477be443595a432dd15
Martyn Garcia <martyn@255bits.com>
2017-08-15 21:57:15 -0600

    [fix] add segment sampler to debug sampler

--- hypergan/samplers/debug_sampler.py
+++ hypergan/samplers/debug_sampler.py
@@ -3,6 +3,7 @@ from hypergan.samplers.began_sampler import BeganSampler
 from hypergan.samplers.batch_sampler import BatchSampler
 from hypergan.samplers.static_batch_sampler import StaticBatchSampler
 from hypergan.samplers.random_walk_sampler import RandomWalkSampler
+from hypergan.samplers.segment_sampler import SegmentSampler
 import tensorflow as tf
 import numpy as np
 import hypergan as hg
@@ -20,6 +21,9 @@ class DebugSampler(BaseSampler):
           self.samplers += [BeganSampler(gan, samples_per_row)]
 
         print("GANLOSS", gan.loss.__class__.__name__)
+
+        #if hasattr(self.gan.generator, 'g1x'):
+        self.samplers += [SegmentSampler(gan)]
 
 
     def _sample(self):

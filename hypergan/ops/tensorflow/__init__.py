039f79017dc7c245cf4cba9a4b14566b5fb0e118
Mikkel Garcia <mikkel@255bits.com>
2017-12-11 12:39:47 -0700

    [feature] missing gradient method filled in

--- hypergan/ops/tensorflow/__init__.py
+++ hypergan/ops/tensorflow/__init__.py
@@ -7,3 +7,6 @@ from os.path import dirname, basename, isfile
 import glob
 modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
+
+
+from . import missing_gradients
---
+++ hypergan/ops/tensorflow/missing_gradients.py
@@ -0,0 +1,27 @@
+from tensorflow.python.ops import gen_image_ops
+from tensorflow.python.framework import ops
+
+print("SETTING UP REZSIIZ")
+
+@ops.RegisterGradient("ResizeNearestNeighborGrad")
+def _ResizeNearestNeighborGrad(op, grad):
+    """The derivatives for nearest neighbor resizing.
+
+    Args:
+    op: The ResizeNearestNeighbor op.
+    grad: The tensor representing the gradient w.r.t. the output.
+
+    Returns:
+    The gradients w.r.t. the input and the output.
+    """
+    image = op.inputs[0]
+    if image.get_shape()[1:3].is_fully_defined():
+        image_shape = image.get_shape()[1:3]
+    else:
+        image_shape = array_ops.shape(image)[1:3]
+
+    grads = gen_image_ops._resize_nearest_neighbor_grad(
+            grad,
+            image_shape,
+            align_corners=op.get_attr("align_corners"))
+    return [grads, None]

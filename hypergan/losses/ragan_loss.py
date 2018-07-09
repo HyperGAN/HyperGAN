e823774b9cb4532dd0f5e9e06612c8f0824fa8cd
Mikkel Garcia <mikkel@255bits.com>
2018-07-09 00:46:12 -0600

    [feature] rgan least squares formulation

--- hypergan/losses/ragan_loss.py
+++ hypergan/losses/ragan_loss.py
@@ -23,5 +23,10 @@ class RaganLoss(BaseLoss):
         d_loss = -tf.log(tf.nn.sigmoid(pq-pp)+TINY)
         g_loss = -tf.log(tf.nn.sigmoid(pp-pq)+TINY)
 
+        if config.type == "least_squares":
+            a,b,c = config.labels
+            d_loss = 0.5*tf.square(d_real - d_fake - b) + 0.5*tf.square(d_fake - d_real - a)
+            g_loss = 0.5*tf.square(d_fake - d_real - c) + 0.5*tf.square(d_real - d_fake - a)
+
         return [d_loss, g_loss]
 

daf770ea3181860d0e078e1e0e0ad0c6fc247524
Mikkel Garcia <mikkel@255bits.com>
2018-09-06 19:19:39 -0600

    [fix] temporary debug output

--- hypergan/samplers/debug_sampler.py
+++ hypergan/samplers/debug_sampler.py
@@ -36,21 +36,25 @@ class IdentitySampler(BaseSampler):
 class DebugSampler(BaseSampler):
     def __init__(self, gan, samples_per_row=8):
         BaseSampler.__init__(self, gan, samples_per_row)
+        x_t = gan.inputs.x
+        global x
+        x = gan.session.run(x_t)
         self.samplers = [
-          IdentitySampler(gan, gan.inputs.x, samples_per_row),
+          #IdentitySampler(gan, gan.inputs.x, samples_per_row),
           #IdentitySampler(gan, gan.inputs.xb, samples_per_row),
-          IdentitySampler(gan, gan.autoencoded_x, samples_per_row),
-          StaticBatchSampler(gan, samples_per_row),
-          BatchSampler(gan, samples_per_row),
-          RandomWalkSampler(gan, samples_per_row)
+          #IdentitySampler(gan, gan.autoencoded_x, samples_per_row),
+          #StaticBatchSampler(gan, samples_per_row),
+          #BatchSampler(gan, samples_per_row),
+          #RandomWalkSampler(gan, samples_per_row)
         ]
 
-        self.samplers += [IdentitySampler(gan, gan.inputs.x, samples_per_row)]
+        #self.samplers += [IdentitySampler(gan, tf.image.resize_images(gan.inputs.x, [128,128], method=1), samples_per_row)]
         if hasattr(gan.generator, 'pe_layers'):
             self.samplers += [IdentitySampler(gan, gx, samples_per_row) for gx in gan.generator.pe_layers]
             pe_layers = self.gan.skip_connections.get_array("progressive_enhancement")
             print("SAMPLERS", pe_layers)
-        self.samplers += [IdentitySampler(gan, gan.inputs.x, samples_per_row)]
+        self.samplers += [IdentitySampler(gan, tf.image.resize_images(gan.inputs.x, [128,128], method=1), samples_per_row)]
+        self.samplers += IdentitySampler(gan, tf.image.resize_images(gan.autoencoded_x, [128,128], method=1), samples_per_row),
 #          IdentitySampler(gan, gan.autoencoded_x, samples_per_row),
         if gan.config.loss['class'] == BoundaryEquilibriumLoss:
           self.samplers += [BeganSampler(gan, samples_per_row)]
@@ -60,7 +64,23 @@ class DebugSampler(BaseSampler):
             self.samplers += [SegmentSampler(gan)]
 
         if hasattr(gan, 'seq'):
-            self.samplers += [IdentitySampler(gan, gx, samples_per_row) for gx in gan.seq]
+            self.samplers += [IdentitySampler(gan, tf.image.resize_images(gx, [128,128], method=1), samples_per_row) for gx in gan.seq]
+
+        default = tf.zeros_like(gan.generator.layer('gend8x8'))
+        def add_samples(layer):
+            layer = gan.generator.layer(layer)
+            if layer is None:
+                layer = default
+
+            self.samplers.append(IdentitySampler(gan, tf.image.resize_images(layer, [128,128], method=1), 1))
+
+        add_samples('gend8x8')
+        add_samples('gend16x16')
+        add_samples('gend32x32')
+        add_samples('gend64x64')
+        add_samples('gend128x128')
+
+
 
 
     def _sample(self):

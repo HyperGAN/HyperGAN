5ab28431ad04e8abfe3f1f386359ac35ede7d5a0
Mikkel Garcia <mikkel@255bits.com>
2018-10-24 18:09:01 -0600

    [fix] dense update

--- hypergan/ops/tensorflow/rmsmirror.py
+++ hypergan/ops/tensorflow/rmsmirror.py
@@ -17,8 +17,16 @@ import tensorflow as tf
 class RMSMirrorOptimizer(RMSPropOptimizer):
 
 
-  def __init__(self, learning_rate=0.001, decay=0.9, momentum=0.0, epsilon=1e-8, use_locking=False, centered=False, name="RMSMirror"):
+  def __init__(self, learning_rate=0.001, decay=0.9, momentum=0.0, p=0.1, epsilon=1e-8, use_locking=False, centered=False, name="RMSMirror"):
     super().__init__(learning_rate, decay=decay, momentum=momentum, epsilon=epsilon, use_locking=use_locking, centered=centered, name=name)
+    self._p = p
+
+  def _prepare(self):
+    super()._prepare()
+    if self._p == "rand":
+        self._p_t = tf.random_uniform([1], minval=0.0, maxval=1.0)
+    else:
+        self._p_t = ops.convert_to_tensor(self._p, name="p")
 
   def _create_slots(self, var_list):
     super()._create_slots(var_list)
@@ -28,6 +36,7 @@ class RMSMirrorOptimizer(RMSPropOptimizer):
       
   def _apply_dense(self, grad, var):
     lr_t = math_ops.cast(self._learning_rate_tensor, var.dtype.base_dtype)
+    p_t = math_ops.cast(self._p_t, var.dtype.base_dtype)
     decay_t = math_ops.cast(self._decay_tensor, var.dtype.base_dtype)
     epsilon_t = math_ops.cast(self._epsilon_tensor, var.dtype.base_dtype)
     momentum_t = math_ops.cast(self._momentum_tensor, var.dtype.base_dtype)
@@ -41,7 +50,9 @@ class RMSMirrorOptimizer(RMSPropOptimizer):
     g_t_1 = self.get_slot(var, "g")
     g_t = g_t_1.assign( g_t )
 
-    var_update = state_ops.assign_sub(var, 2. * lr_t * g_t - lr_t * g_t_1) #Adam would be lr_t * g_t
+    #movement = 2. * lr_t * g_t - lr_t * g_t_1
+    movement = lr_t * g_t - p_t * lr_t * (g_t_1 - g_t)
+    var_update = state_ops.assign_sub(var, movement)
     return control_flow_ops.group(*[var_update, rms_t, g_t])
 
   def _apply_sparse(self, grad, var):

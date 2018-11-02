21474047ed4090aa8c1f4f9f51b5f62cc4ea4c70
Martyn Garcia <martyn@255bits.com>
2018-11-01 19:28:11 -0600

    [fix] remove missing file

--- hypergan/cli.py
+++ hypergan/cli.py
@@ -20,7 +20,6 @@ import shutil
 import sys
 
 from hypergan.samplers.static_batch_sampler import StaticBatchSampler
-from hypergan.samplers.infoplane_sampler import InfoPlaneSampler
 from hypergan.samplers.batch_sampler import BatchSampler
 from hypergan.samplers.batch_walk_sampler import BatchWalkSampler
 from hypergan.samplers.grid_sampler import GridSampler

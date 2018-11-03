3a647e64ac601910e34b4d76aabd3ec8280d4ed4
Martyn Garcia <martyn@255bits.com>
2018-11-03 17:02:28 -0600

    [optimize] parallelize

--- hypergan/optimizers/consensus_optimizer.py
+++ hypergan/optimizers/consensus_optimizer.py
@@ -63,12 +63,11 @@ class ConsensusOptimizer(optimizer.Optimizer):
     )
     Jgrads = tf.gradients(consensus_reg, d_vars) + [tf.zeros_like(g) for g in g_vars]
     print("LR_T", self._lr_t)
-    op7 = tf.group([tf.assign_sub(v, self._lr_t*(grad+(jg * self._beta))) if jg is not None else tf.assign_sub(v,self._lr_t*grad+tf.zeros_like(v)) for v,grad, jg in zip(var_list, all_grads, Jgrads)])
-    with tf.get_default_graph().control_dependencies([op7]):
 
-        op6 = tf.group([self.optimizer._apply_dense(g,v) for g,v in grads_and_vars])
-        with tf.get_default_graph().control_dependencies([op6]):
-            return tf.no_op()
+    op6 = [tf.assign_sub(v, (jg * self._beta)) if jg is not None else tf.no_op() for v,grad, jg in zip(var_list, all_grads, Jgrads)]
+    op6 += [self.optimizer._apply_dense(g,v) for g,v in grads_and_vars]
+    with tf.get_default_graph().control_dependencies(op6):
+        return tf.no_op()
 
   
   def _apply_sparse(self, grad, var):

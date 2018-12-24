c451cd9ebf000b026ef28a3046cfda8260f15a02
Mikkel Garcia <mikkel@255bits.com>
2018-12-24 14:14:32 -0700

    [feature] config options + l1 loss on w

--- hypergan/optimizers/social_optimizer.py
+++ hypergan/optimizers/social_optimizer.py
@@ -44,34 +44,51 @@ class SocialOptimizer(optimizer.Optimizer):
             g_vars += [var]
         else:
             raise("Couldn't find var in g_vars or d_vars")
-    w = [tf.Variable(0.5), tf.Variable(0.5)]
+    w = [tf.Variable(self.config.start_at or 0.0), tf.Variable(self.config.start_at or 0.0)]
 
     Vidv = [self.gan.trainer.d_loss, self.gan.trainer.g_loss]
     #Vsoc = [1/2. * self.gan.trainer.d_loss + 1/2.* self.gan.trainer.g_loss, -1/2. * self.gan.trainer.d_loss - 1/2.* self.gan.trainer.g_loss]
     Vsoc = [1/2. * self.gan.trainer.d_loss + 1/2.* self.gan.trainer.g_loss, 1/2. * self.gan.trainer.d_loss + 1/2.* self.gan.trainer.g_loss]
 
-    wlr = self.config.w_learn_rate or 0.1
+    wlr = self.config.w_learn_rate or 0.01
     wt1 = [w[0] + wlr * (Vidv[0] - Vsoc[0]), w[1] + wlr * (Vidv[1] - Vsoc[1])]
-
     def clamped(net):
-        return tf.maximum(0., tf.minimum(net, 1.))
+        return tf.maximum(self.config.min or 0., tf.minimum(net, self.config.max or 1.))
 
     self._prepare()
 
     wt1 = [clamped(wt1[0]),clamped(wt1[1])]
-    self.gan.add_metric('w0', w[0])
-    self.gan.add_metric('w1', w[1])
+    self.gan.add_metric('wt0', wt1[0])
+    self.gan.add_metric('wt1', wt1[1])
     op1 = tf.group(*[tf.assign(w, v) for w,v in zip(w, wt1)]) # store variables
 
     with tf.get_default_graph().control_dependencies([op1]):
         Vi = [(1. - w[0]) * Vidv[0] + w[0] * Vsoc[0],
               (1. - w[1]) * Vidv[1] + w[1] * Vsoc[1]]
+        if self.config.reverse_w:
+            Vi = [(w[0]) * Vidv[0] + (1.0-w[0]) * Vsoc[0],
+                  (w[1]) * Vidv[1] + (1.0-w[1]) * Vsoc[1]]
+        self.gan.add_metric('w0', w[0])
+        self.gan.add_metric('w1', w[1])
 
         new_grads = tf.gradients(Vi[0], d_vars) + tf.gradients(Vi[1], g_vars)
+        self.gan.trainer.d_loss = Vi[0]
+        self.gan.trainer.g_loss = Vi[1]
         new_grads_and_vars = list(zip(new_grads, var_list)).copy()
         op3 = self.optimizer.apply_gradients(new_grads_and_vars.copy(), global_step=global_step, name=name)
         with tf.get_default_graph().control_dependencies([op3]):
-            return tf.no_op()
+            if(self.config.w_l1):
+                # return to selfish state
+                wt1 = [wt1[0] + self.config.w_l1 * ((self.config.l1_default or 0.0)-wt1[0]),
+                       wt1[1] + self.config.w_l1 * ((self.config.l1_default or 0.0)-wt1[1])]
+                op4 = tf.group(*[tf.assign(w, v) for w,v in zip(w, wt1)]) # store variables
+                with tf.get_default_graph().control_dependencies([op4]):
+                    self.gan.add_metric('l1w0', w[0])
+                    self.gan.add_metric('l1w1', w[1])
+                    return tf.no_op()
+
+            else:
+                return tf.no_op()
 
   def _apply_sparse(self, grad, var):
     raise NotImplementedError("Sparse gradient updates are not supported.")

22aa743e206ad8e70365c9ba9de37803409633f7
Mikkel Garcia <mikkel@255bits.com>
2018-12-26 10:50:29 -0700

    [feature] memory_size config option

--- hypergan/train_hooks/imle_train_hook.py
+++ hypergan/train_hooks/imle_train_hook.py
@@ -17,31 +17,38 @@ from hypergan.train_hooks.base_train_hook import BaseTrainHook
 
 class IMLETrainHook(BaseTrainHook):
   """https://arxiv.org/pdf/1809.09087.pdf"""
-  def __init__(self, gan=None, config=None, trainer=None, name="ClosestExampleTrainHook", search_size=4):
+  def __init__(self, gan=None, config=None, trainer=None, name="ClosestExampleTrainHook", search_size=4, memory_size=1):
     super().__init__(config=config, gan=gan, trainer=trainer, name=name)
-    gan_inputs = self.gan.inputs.x
-    self.s_max = [ tf.Variable( tf.zeros_like(gan_inputs)) for i in range(search_size)]
-    self.x_matched = tf.Variable(tf.zeros_like(gan.generator.sample))
+    self.x_matched = [ tf.Variable(tf.zeros_like(gan.generator.sample)) for j in range(memory_size)]
     self.latent_max = [ tf.Variable( tf.zeros_like(gan.latent.sample)) for i in range(search_size)]
-    self.latent = tf.Variable(tf.zeros_like(gan.latent.sample))
-    self.assign_x = tf.assign(self.x_matched, gan.inputs.x)
+    self.latent = [ tf.Variable(tf.zeros_like(gan.latent.sample)) for j in range(memory_size)]
+    self.assign_x = [tf.assign(self.x_matched[j], gan.inputs.x) for j in range(memory_size)]
     self.d_lambda = config['lambda'] or 1
 
     self.search_size = search_size
+    self.memory_size = memory_size
 
-    self.current = tf.Variable(tf.zeros_like(gan_inputs))
-    self.assign_latent_to_max = [self.latent.assign(self.latent_max[i]) for i in range(search_size)]
-    self.assign_s_max = [self.s_max[i].assign(gan.generator.sample) for i in range(search_size)]
-    self.assign_s_max_to_current = [self.current.assign(self.s_max[i]) for i in range(search_size)]
+    self.assign_latent_to_max = [[self.latent[j].assign(self.latent_max[i]) for i in range(search_size)] for j in range(memory_size)]
     self.assign_latent = [self.latent_max[i].assign(gan.latent.sample) for i in range(search_size)]
-    self.gi = self.gan.create_component(self.gan.config.generator, name='generator', input=self.latent, reuse=True)
-    diff = tf.abs(self.gi.sample-self.x_matched)
-    n = tf.norm(diff, ord=2)
-    self.l2_loss_on_saved = self.d_lambda * tf.reduce_sum(diff/n)
 
-    diff_on_g = tf.abs(gan.generator.sample-self.x_matched)
-    n_on_g = tf.norm(diff, ord=2)
-    self.l2_loss_on_g = self.d_lambda * tf.reduce_sum(diff_on_g/n_on_g)
+    l2_losses = tf.zeros([1])
+    self.gi = []
+    for j in range(memory_size):
+        self.gi.append(self.gan.create_component(self.gan.config.generator, name='generator', input=self.latent[j], reuse=True))
+        diff = tf.abs(self.gi[-1].sample-self.x_matched[j])
+        n = tf.norm(diff, ord=2)
+        l2_losses += tf.reduce_sum(diff/n)
+        
+    self.l2_loss_on_saved = tf.reduce_sum(self.d_lambda * l2_losses)
+
+    l2_losses = []
+    for j in range(memory_size):
+        diff = tf.abs(gan.generator.sample-self.x_matched[j])
+        n = tf.norm(diff, ord=2)
+        l2_losses.append(tf.reduce_sum(diff/n))
+        
+    self.l2_loss_on_g = l2_losses
+
     self.gan.add_metric('perceptual', self.l2_loss_on_saved)
     self.gan.add_metric('latent', tf.reduce_sum(self.latent))
     self.gan.add_metric('xm', tf.reduce_sum(self.x_matched))
@@ -56,15 +63,15 @@ class IMLETrainHook(BaseTrainHook):
     if step % (self.config.step_count or 1000) != 0:
       return
     print("[IMLE] recalculating likelihood")
-    scores = []
-    self.gan.session.run(self.assign_x)
-    for i in range(self.search_size):
-        s,_,_ = self.gan.session.run([self.l2_loss_on_g, self.assign_s_max[i], self.assign_latent[i]])
-        scores.append(s)
-    sort = zip(scores, self.assign_s_max_to_current, self.assign_latent_to_max)
-    sort2 = sorted(sort, key=itemgetter(0))
-    print("  Max ", scores[0])
-    print("  Min ", scores[-1])
-    self.gan.session.run(sort2[-1][-1])
-    self.gan.session.run(sort2[-1][-2])
+    for j in range(self.memory_size):
+        scores = []
+        self.gan.session.run(self.assign_x[j])
+        for i in range(self.search_size):
+            s,_ = self.gan.session.run([self.l2_loss_on_g[j], self.assign_latent[i]])
+            scores.append(s)
+        sort = zip(scores, self.assign_latent_to_max[j])
+        sort2 = sorted(sort, key=itemgetter(0))
+        print("  Max ", sort2[-1][0])
+        print("  Min ", sort2[0][0])
+        self.gan.session.run(sort2[0][-1])
 

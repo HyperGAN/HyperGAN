e2b8390ac8b7e16e0dad5ecdd8f827eb164f924e
Martyn Garcia <martyn@255bits.com>
2019-01-06 17:30:24 -0700

    [fix] tests

--- tests/distributions/category_distribution_test.py
+++ tests/distributions/category_distribution_test.py
@@ -2,32 +2,32 @@ import tensorflow as tf
 import hyperchamber as hc
 import numpy as np
 import hypergan as hg
-from hypergan.encoders.uniform_encoder import UniformEncoder
+from hypergan.distributions.uniform_distribution import UniformDistribution
 from hypergan.gan_component import ValidationException
 from hypergan.ops import TensorflowOps
 
 from unittest.mock import MagicMock
 from tests.mocks import MockDiscriminator, mock_gan
 
-from hypergan.encoders.category_encoder import CategoryEncoder
+from hypergan.distributions.category_distribution import CategoryDistribution
 
 config = {
     "categories": [1,2,3],
 }
-class CategoriesEncoderTest(tf.test.TestCase):
+class CategoriesDistributionTest(tf.test.TestCase):
     def test_categories(self):
         with self.test_session():
             gan = mock_gan()
-            encoder = CategoryEncoder(gan, config)
-            gan.encoder = encoder
-            gan.encoder.create()
+            distribution = CategoryDistribution(gan, config)
+            gan.distribution = distribution
+            gan.distribution.create()
             gan.create()
-            self.assertEqual(gan.ops.shape(encoder.sample)[1]//2, len(encoder.categories))
+            self.assertEqual(gan.ops.shape(distribution.sample)[1]//2, len(distribution.categories))
 
     def test_validate(self):
         with self.assertRaises(ValidationException):
             gan = mock_gan()
-            CategoryEncoder(gan, {})
+            CategoryDistribution(gan, {})
 
 
 if __name__ == "__main__":
--- tests/distributions/uniform_distribution_test.py
+++ tests/distributions/uniform_distribution_test.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import hyperchamber as hc
 import numpy as np
 import hypergan as hg
-from hypergan.encoders.uniform_encoder import UniformEncoder
+from hypergan.distributions.uniform_distribution import UniformDistribution
 from hypergan.gan_component import ValidationException
 from hypergan.ops import TensorflowOps
 
@@ -10,25 +10,25 @@ from unittest.mock import MagicMock
 from tests.mocks import MockDiscriminator, mock_gan
 
 gan = mock_gan()
-encoder = UniformEncoder(gan, {
+distribution = UniformDistribution(gan, {
     'test':True,
     "z": 2,
     "min": 0,
     "max": 1
 })
-class UniformEncoderTest(tf.test.TestCase):
+class UniformDistributionTest(tf.test.TestCase):
     def test_config(self):
         with self.test_session():
-            self.assertEqual(encoder.config.test, True)
+            self.assertEqual(distribution.config.test, True)
 
     def test_projection(self):
         config = {
-                "projections": [hg.encoders.uniform_encoder.identity],
+                "projections": [hg.distributions.uniform_distribution.identity],
                 "z": 2,
                 "min": 0,
                 "max": 1
                 }
-        subject = UniformEncoder(gan, config)
+        subject = UniformDistribution(gan, config)
         with self.test_session():
             projections = subject.create()
             self.assertEqual(subject.ops.shape(projections)[1], 2)
@@ -40,26 +40,11 @@ class UniformEncoderTest(tf.test.TestCase):
                 "min": 0,
                 "max": 1
                 }
-        subject = UniformEncoder(gan, config)
+        subject = UniformDistribution(gan, config)
         with self.test_session():
             projections = subject.create()
             self.assertEqual(int(projections.get_shape()[1]), len(config['projections'])*config['z'])
             
-    def test_validate(self):
-        with self.assertRaises(ValidationException):
-            config = {
-                "projections": [hg.encoders.uniform_encoder.identity],
-                "z": 3,
-                "min": 0,
-                "max": 1
-            }
- 
-            UniformEncoder(gan, config)
-
-    def test_validate_z_must_be_multiple_of_two(self):
-        with self.assertRaises(ValidationException):
-            UniformEncoder(gan, {})
-
     def test_projection_gaussian(self):
         config = {
                 "projections": ['identity', 'gaussian'],
@@ -67,7 +52,7 @@ class UniformEncoderTest(tf.test.TestCase):
                 "min": 0,
                 "max": 1
                 }
-        subject = UniformEncoder(gan, config)
+        subject = UniformDistribution(gan, config)
         with self.test_session():
             projections = subject.create()
             self.assertEqual(int(projections.get_shape()[1]), len(config['projections'])*config['z'])

ac8154eb30b835ee99e131f08eed95dbefb53d74
Martyn Garcia <martyn@255bits.com>
2019-01-29 23:00:28 -0700

    [fix] more tests

--- tests/inputs/image_loader_test.py
+++ tests/inputs/image_loader_test.py
@@ -29,39 +29,36 @@ class ImageLoaderTest(tf.test.TestCase):
     def test_load_fixture(self):
         with self.test_session():
             loader = ImageLoader(32) #TODO crop=true?
-            x, y = loader.create(fixture_path(), width=2, height=2, format='png')
-            self.assertEqual(int(y.get_shape()[1]), 2)
-            self.assertEqual(int(x.get_shape()[1]), 2)
-            self.assertEqual(int(x.get_shape()[2]), 2)
+            loader.create(fixture_path(), width=2, height=2, format='png')
+            self.assertEqual(int(loader.x.get_shape()[1]), 2)
+            self.assertEqual(int(loader.x.get_shape()[2]), 2)
 
 
     def test_load_fixture_resize(self):
         with self.test_session():
             loader = ImageLoader(32) #TODO crop=true?
-            x, y = loader.create(fixture_path(), width=8, height=8, resize=True, format='png')
-            self.assertEqual(int(y.get_shape()[1]), 2)
-            self.assertEqual(int(x.get_shape()[1]), 8)
-            self.assertEqual(int(x.get_shape()[2]), 8)
+            loader.create(fixture_path(), width=8, height=8, resize=True, format='png')
+            self.assertEqual(int(loader.x.get_shape()[1]), 8)
+            self.assertEqual(int(loader.x.get_shape()[2]), 8)
 
 
     def test_load_fixture_single(self):
         with self.test_session():
             loader = ImageLoader(32) #TODO crop=true? why is this working
-            x, y = loader.create(fixture_path('images'), width=4, height=4, format='png')
-            self.assertEqual(int(y.get_shape()[1]), 1)
-            self.assertEqual(int(x.get_shape()[1]), 4)
-            self.assertEqual(int(x.get_shape()[2]), 4)
+            loader.create(fixture_path('images'), width=4, height=4, format='png')
+            self.assertEqual(int(loader.x.get_shape()[1]), 4)
+            self.assertEqual(int(loader.x.get_shape()[2]), 4)
 
     def test_load_fixture_single(self):
         with self.test_session():
             loader = ImageLoader(32) #TODO crop=true?
-            x, y = loader.create(fixture_path(), width=4, height=4, format='png')
+            loader.create(fixture_path(), width=4, height=4, format='png')
             self.assertEqual(loader.file_count, 2)
 
     def test_load_fixture_single_count(self):
         with self.test_session():
             loader = ImageLoader(32) #TODO crop=true?
-            x, y = loader.create(fixture_path('white'), width=4, height=4, format='png')
+            loader.create(fixture_path('white'), width=4, height=4, format='png')
             self.assertEqual(loader.file_count, 1)
 
 if __name__ == "__main__":

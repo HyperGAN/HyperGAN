8bf401abd4a1aea4d2f4400cfd5483d79030853f
Mikkel Garcia <mikkel@255bits.com>
2019-06-12 16:40:44 -0600

    [fix] depth trainer call train hooks

--- hypergan/trainers/experimental/depth_trainer.py
+++ hypergan/trainers/experimental/depth_trainer.py
@@ -9,20 +9,15 @@ TINY = 1e-12
 
 class DepthTrainer(BaseTrainer):
     """ Runs an optimizer multiple times and combines the output into a mixture. """
-    def create(self):
+    def _create(self):
         self.hist = [0 for i in range(2)]
         config = self.config
-        self.global_step = tf.train.get_global_step()
         self.mix_threshold_reached = False
-        decay_function = config.decay_function
         variables = self.gan.d_vars() + self.gan.g_vars()
         self.ema = [ tf.Variable(_v) for _v in variables ]
         self.store_v = [ _v.assign(_v2) for _v,_v2 in zip(self.ema, variables) ]
-        self.combine = [ _v.assign(config.decay *_ema + (1.-config.decay)*_new) for _v, _ema, _new in zip(variables, self.ema, variables)]
-        self._delegate = self.gan.create_component(config.trainer, d_vars=self.d_vars, g_vars=self.g_vars, loss=self.loss)
-        self._delegate.create()
-        self.slot_vars_g = self._delegate.slot_vars_g
-        self.slot_vars_d = self._delegate.slot_vars_g
+        self.combine = [ _v.assign((config.decay or 0.1) *_ema + (1.-(config.decay or 0.1))*_new) for _v, _ema, _new in zip(variables, self.ema, variables)]
+        self._delegate = self.gan.create_component(config.trainer, d_vars=self.d_vars, g_vars=self.g_vars)
 
     def required(self):
         return "".split()
@@ -31,10 +26,11 @@ class DepthTrainer(BaseTrainer):
         gan = self.gan
         sess = gan.session
         config = self.config
-        loss = self.loss 
 
+        self.before_step(self.current_step, feed_dict)
         gan.session.run(self.store_v)
         for i in range(config.depth or 2):
             self._delegate.step(feed_dict)
         gan.session.run(self.combine)
+        self.after_step(self.current_step, feed_dict)
 

6d58d7a01b720cae9cbee94534d6d6bbb04219ee
Mikkel Garcia <mikkel@255bits.com>
2019-08-02 02:16:12 -0600

    [fix] better colorizer random sampling

--- hypergan/samplers/random_walk_sampler.py
+++ hypergan/samplers/random_walk_sampler.py
@@ -18,22 +18,22 @@ class RandomWalkSampler(BaseSampler):
         inputs_t = gan.inputs.x
 
         if self.z is None:
-            gan.session.run(gan.inputs.set['x'])
+            gan.session.run(gan.set_x)
             self.z = gan.latent.sample.eval()
             self.target = gan.latent.sample.eval()
             self.input = gan.session.run(gan.inputs.x)
-            self.input = np.tile(self.input[0], [gan.batch_size(), 1, 1, 1])
 
-        if self.step > self.steps:
+        if self.step % self.steps == 0:
             self.z = self.target
             self.target = gan.latent.sample.eval()
-            self.step = 0
 
-        percent = float(self.step)/self.steps
+        percent = float(self.step % self.steps)/self.steps
         z_interp = self.z*(1.0-percent) + self.target*percent
         self.step+=1
+        if self.step % 360 == 0:
+            gan.session.run(gan.set_x)
+            self.input = gan.session.run(gan.inputs.x)
         output = gan.session.run(gan.generator.sample, feed_dict={z_t: z_interp, inputs_t: self.input})
-        output[0] = self.input[0]
         g=tf.get_default_graph()
         with g.as_default():
             tf.set_random_seed(1)

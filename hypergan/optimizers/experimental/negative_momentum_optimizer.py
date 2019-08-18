26c2c5162606505e5546b7e010a4531aa0464c60
Mikkel Garcia <mikkel@255bits.com>
2019-08-18 15:58:07 +0000

    [fix] negative momentum variable

--- hypergan/optimizers/experimental/negative_momentum_optimizer.py
+++ hypergan/optimizers/experimental/negative_momentum_optimizer.py
@@ -47,11 +47,11 @@ class NegativeMomentumOptimizer(optimizer.Optimizer):
     grad_list = d_grads + g_grads
     var_list = d_vars + g_vars
 
-    with ops.init_scope():
-            nms = [self._get_or_make_slot(v, tf.zeros_like(v), "nm", self._name) for v in var_list]
     self._prepare()
-
-    nms = [self.get_slot(v, "nm") for v in var_list]
+    
+    nms = []
+    for v in var_list:
+        nms.append(tf.get_variable(v.name.split(":")[0]+"_nm", initializer=tf.zeros_initializer, shape=v.shape, aggregation=tf.VariableAggregation.MEAN ))
     momentum = []
     for grad, nm, w in zip(grad_list, nms, var_list):
         momentum += [-self._decay * nm]

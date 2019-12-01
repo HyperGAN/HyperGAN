f2bf1d74add84b8f24797c2bc0861f0f1ea448f1
Mikkel Garcia <mikkel@255bits.com>
2019-12-01 10:52:34 -0700

    [feature] https://arxiv.org/abs/1910.09779

--- hypergan/losses/wasserstein_loss.py
+++ hypergan/losses/wasserstein_loss.py
@@ -12,4 +12,14 @@ class WassersteinLoss(BaseLoss):
         d_loss = -d_real + d_fake
         g_loss = -d_fake
 
+        if config.kl:
+            # https://arxiv.org/abs/1910.09779
+            loss_real = tf.reduce_mean(tf.nn.relu(1.-d_real))
+            d_fake_norm = tf.reduce_mean(tf.math.exp(d_fake))+1e-8
+            d_fake_ratio = (tf.math.exp(d_fake)+1e-8) / d_fake_norm
+            loss_fake = d_fake * d_fake_ratio
+            loss_fake = tf.reduce_mean(tf.nn.relu(1.+loss_fake))
+            d_loss = loss_real + loss_fake
+            g_loss = -loss_fake
+
         return [d_loss, g_loss]

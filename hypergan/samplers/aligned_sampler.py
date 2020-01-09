[fix] handle negative signs in params [fix] samplers with configurablegan

hypergan/samplers/aligned_sampler.py
@@ -1,4 +1,5 @@
 from hypergan.samplers.base_sampler import BaseSampler
+from hypergan.gan_component import ValidationException, GANComponent
 
 import tensorflow as tf
 import numpy as np
@@ -9,30 +10,39 @@ class AlignedSampler(BaseSampler):
         self.xa_v = None
         self.xb_v = None
         self.created = False
-
-    def compatible_with(gan):
-        if hasattr(gan.inputs, 'xa') and \
-            hasattr(gan.inputs, 'xb') and \
-            hasattr(gan, 'cyca'):
-            return True
-        return False
+        self.x_cache = None
+        self.latent_cache = None
 
     def sample(self, path, sample_to_file):
         gan = self.gan
 
         sess = gan.session
         config = gan.config
-
-        xs, *x_hats = sess.run([gan.inputs.xs[0]]+[_x.sample for _x in gan.x_hats])
+        if hasattr(gan, 'x_hats'):
+            xs, *x_hats = sess.run([gan.inputs.xs[0]]+[_x.sample for _x in gan.x_hats])
+        elif hasattr(gan, "generators_cache"):
+            feed_dict = {}
+            if self.x_cache is None:
+                self.x_cache = sess.run(gan.inputs.xs)
+                self.latent_cache = sess.run(gan.latent.sample)
+            
+            for i, x in enumerate(gan.inputs.xs):
+                feed_dict[x] = self.x_cache[i]
+            xs = sess.run(gan.inputs.xs, feed_dict)
+            feed_dict[gan.latent.sample]=self.latent_cache
+            x_hats = sess.run([g.sample for g in gan.generators_cache.values()], feed_dict)
+        else:
+            raise ValidationException("Unknown alignment gan type")
 
         stacks = []
         bs = gan.batch_size() // 2
         width = min(gan.batch_size(), 8)
-        for i in range(1):
-            stacks.append([xs[i*width+j] for j in range(width)])
-        for x_h in x_hats:
-            for i in range(1):
-                stacks.append([x_h[i*width+j] for j in range(width)])
+        for i in range(len(gan.inputs.xs)):
+            print(np.shape(xs))
+            stacks.append([xs[i][j] for j in range(width)])
+        print(np.shape(x_hats), "XH")
+        for i in range(len(x_hats)):
+            stacks.append([x_hats[i][j] for j in range(width)])
 
         images = np.vstack([np.hstack(s) for s in stacks])
 

hypergan/samplers/batch_walk_sampler.py
@@ -70,9 +70,15 @@ class BatchWalkSampler(BaseSampler):
             z = self.steps[i][self.step]
             z = np.expand_dims(z,axis=0)
             x = self.xs[i]
-            g = gan.session.run(gan.generator.sample, feed_dict={z_t: z, x_t: x})
-            gs.append(x)
-            gs.append(g)
+            if hasattr(gan, "generators_cache"):
+                feed_dict={z_t: z, x_t: x}
+                x_hats = gan.session.run([g.sample for g in gan.generators_cache.values()], feed_dict)
+                gs += [x] + x_hats
+
+            else:
+                g = gan.session.run(gan.generator.sample, feed_dict={z_t: z, x_t: x})
+                gs.append(x)
+                gs.append(g)
         g = np.hstack(gs)
         xshape = gan.ops.shape(gan.inputs.x)
         g = np.reshape(gs, [self.rows*2, self.columns, xshape[1], xshape[2], xshape[3]])
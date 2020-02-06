9c54a47ec9e587b3bb1aaec22c112dce81301a56
Mikkel Garcia <mikkel@255bits.com>
2020-02-06 16:12:43 -0700

    [wip] simultaneous trainer

--- hypergan/trainers/simultaneous_trainer.py
+++ hypergan/trainers/simultaneous_trainer.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
 import numpy as np
+import torch
 import hyperchamber as hc
 import inspect
 
@@ -10,7 +10,15 @@ TINY = 1e-12
 class SimultaneousTrainer(BaseTrainer):
     """ Steps G and D simultaneously """
     def _create(self):
-        pass
+        self.optimizer = torch.optim.Adam(self.gan.generator.parameters(), lr=1e-3, betas=(.9,.999))
+        #remaining
+        #* set optimizer from config
+        #* use discriminator
+        #* use loss
+        #* make sure metrics work
+        #* generator architecture
+        #* gan.parameters() sholud return all component params
+
 
     def required(self):
         return "".split()
@@ -21,12 +29,20 @@ class SimultaneousTrainer(BaseTrainer):
         loss = gan.loss
         metrics = gan.metrics()
 
-        d_loss, g_loss = loss.sample
+        self.optimizer.zero_grad()
+
+        g_loss = torch.abs(self.gan.generator(self.gan.latent.sample()))
+
+        #d_loss, g_loss = loss.sample
+
+        (-g_loss.mean()).backward()
 
         self.before_step(self.current_step, feed_dict)
 
         if self.current_step % 10 == 0:
             self.print_metrics(self.current_step)
+
+        self.optimizer.step()
 
     def print_metrics(self, step):
         metrics = self.gan.metrics()

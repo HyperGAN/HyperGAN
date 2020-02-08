a1b072971c3878be5e3fa35057cae578a30f9d61
Mikkel Garcia <mikkel@255bits.com>
2020-02-08 12:18:18 -0700

    [fix] stabilizing example

--- hypergan/train_hooks/stabilizing_training_train_hook.py
+++ hypergan/train_hooks/stabilizing_training_train_hook.py
@@ -23,23 +23,30 @@ class StabilizingTrainingTrainHook(BaseTrainHook):
       self.gamma = self.gan.configurable_param(self.config.gamma or 1.0)
 
   def forward(self):
-      d_real, d_fake = self.gan.forward_discriminator()
-      d_loss, g_loss = self.gan.forward_loss()
-      d1 = self.sig(d_fake)
-      d2 = self.sig(d_real)
-      d_params = self.gan.d_parameters()
-      g_params = self.gan.g_parameters()
-      d1_grads = torch_grad(outputs=d_loss.mean(), inputs=d_params, retain_graph=True)
-      d2_grads = torch_grad(outputs=g_loss.mean(), inputs=g_params, retain_graph=True)
+      x = self.gan.inputs.sample
+      g = self.gan.generator_sample
+      d1_params = Variable(x, requires_grad=True).cuda()#self.gan.d_parameters()
+      d2_params = Variable(g, requires_grad=True).cuda()#self.gan.g_parameters()
+      d1_logits = self.gan.discriminator(d1_params)
+      d2_logits = self.gan.discriminator(d2_params)
+      d1 = self.sig(d1_logits)
+      d2 = self.sig(d2_logits)
+      d1_params = list(self.gan.discriminator.parameters()) + [d1_logits]
+      d2_params = list(self.gan.discriminator.parameters()) + [d2_logits]
+      d1_grads = torch_grad(outputs=d1_logits.mean(), inputs=d1_params, retain_graph=True, create_graph=True)
+      d2_grads = torch_grad(outputs=d2_logits.mean(), inputs=d2_params, retain_graph=True, create_graph=True)
       d1_norm = [torch.norm(_d1_grads.view(-1).cuda(),p=2,dim=0) for _d1_grads in d1_grads]
       d2_norm = [torch.norm(_d2_grads.view(-1).cuda(),p=2,dim=0) for _d2_grads in d2_grads]
 
       reg_d1 = [(((1.0-d1)**2).cuda() * (_d1_norm**2).cuda()) for _d1_norm in d1_norm]
       reg_d2 = [((d2**2).cuda() * (_d2_norm**2).cuda()) for _d2_norm in d2_norm]
+      #reg_d1 = [((d1**2).cuda() * (_d1_norm**2).cuda()) for _d1_norm in d1_norm]
+      #reg_d2 = [(((1.0-d2)**2).cuda() * (_d2_norm**2).cuda()) for _d2_norm in d2_norm]
       reg_d1 = sum(reg_d1)
       reg_d2 = sum(reg_d2)
+      #self.d_loss = self.gamma * reg_d1.mean()
       self.d_loss = self.gamma * (reg_d1+reg_d2).mean()
-      #self.g_loss = self.gan.configurable_param(self.config.gamma or 1.0) * (reg_d2).mean()
+      #self.g_loss = self.gamma * reg_d2.mean()
       #self.gan.add_metric('stable_js', self.ops.squash(self.d_loss))
 
       return [self.d_loss, self.g_loss]

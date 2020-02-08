256667692e65ad98232e1c9ca8065aa3388cb8c9
Martyn Garcia <martyn@255bits.com>
2020-02-08 15:22:51 -0700

    [feature] Reshape module

---
+++ hypergan/modules/reshape.py
@@ -0,0 +1,8 @@
+import torch.nn as nn
+
+class Reshape(nn.Module):
+    def __init__(self, *dims):
+        self.dims = dims
+        super(Reshape, self).__init__()
+    def forward(self, x):
+        return x.view(x.size()[0], *self.dims)

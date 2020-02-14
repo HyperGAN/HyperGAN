bf1b18ea0691e010be3d09e349f2becc16ade150
Mikkel Garcia <mikkel@255bits.com>
2020-02-14 15:28:34 -0700

    [feature] learn rate dropout train hook

--- hypergan/train_hooks/learning_rate_dropout_train_hook.py
+++ hypergan/train_hooks/learning_rate_dropout_train_hook.py
@@ -1,41 +1,48 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+from hypergan.train_hooks.base_train_hook import BaseTrainHook
+from operator import itemgetter
+from tensorflow.python.framework import ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import state_ops
-from tensorflow.python.framework import ops
 from tensorflow.python.training import optimizer
+from torch.autograd import Variable
+from torch.autograd import grad as torch_grad
 import hyperchamber as hc
-import numpy as np
 import inspect
-from operator import itemgetter
-from hypergan.train_hooks.base_train_hook import BaseTrainHook
+import numpy as np
+import torch
+import torch.nn as nn
 
 class LearningRateDropoutTrainHook(BaseTrainHook):
   """ https://arxiv.org/abs/1912.00144 """
-  def __init__(self, gan=None, config=None, trainer=None, name="LearnRateDropoutTrainHook"):
-      super().__init__(config=config, gan=gan, trainer=trainer, name=name)
+  def __init__(self, gan=None, config=None, trainer=None):
+      super().__init__(config=config, gan=gan, trainer=trainer)
+      self.ones = self.gan.configurable_param(self.config.ones or 1.0)
+      self.zeros = self.gan.configurable_param(self.config.zeros or 0.0)
+      self.dropout = self.gan.configurable_param(self.config.dropout or 0.9)
+
+  def forward(self):
+      return [None, None]
 
   def gradients(self, d_grads, g_grads):
-      dropout = self.gan.configurable_param(self.config.dropout or 0.1)
-      ones = self.gan.configurable_param(self.config.ones or 1.0)
-      zeros = self.gan.configurable_param(self.config.zeros or 0.0)
-      d_ones = [tf.fill(self.ops.shape(_g), ones) for _g in d_grads]
-      g_ones = [tf.fill(self.ops.shape(_g), ones) for _g in g_grads]
-      d_zeros = [tf.fill(self.ops.shape(_g), zeros) for _g in d_grads]
-      g_zeros = [tf.fill(self.ops.shape(_g), zeros) for _g in g_grads]
-      da = [tf.where((tf.random_uniform(self.ops.shape(_d_grads))- (1.0-dropout)) < 0, _o, _z) for _d_grads, _o, _z in zip(d_grads, d_ones, d_zeros)]
-      ga = [tf.where((tf.random_uniform(self.ops.shape(_g_grads))- (1.0-dropout)) < 0, _o, _z) for _g_grads, _o, _z in zip(g_grads, g_ones, g_zeros)]
+      d_ones = [torch.ones_like(_g) for _g in d_grads]
+      g_ones = [torch.ones_like(_g) for _g in g_grads]
+      d_zeros = [torch.zeros_like(_g) for _g in d_grads]
+      g_zeros = [torch.zeros_like(_g) for _g in g_grads]
+      da = [torch.where((torch.rand_like(_d_grads)- (1.0-self.dropout)) < 0, _o, _z) for _d_grads, _o, _z in zip(d_grads, d_ones, d_zeros)]
+      ga = [torch.where((torch.rand_like(_g_grads)- (1.0-self.dropout)) < 0, _o, _z) for _g_grads, _o, _z in zip(g_grads, g_ones, g_zeros)]
+
       if self.config.skip_d is None:
           d_grads = [_a * _grad for _a, _grad in zip(da, d_grads)]
       if self.config.skip_g is None:
           g_grads = [_a * _grad for _a, _grad in zip(ga, g_grads)]
-      def count_params(variables):
-        return np.sum([np.prod(self.ops.shape(t)) for t in variables ])
-      self.gan.add_metric('dropout-one', ones)
-      self.gan.add_metric('dropout-perc', dropout)
-      self.gan.add_metric('dropout', sum([self.ops.squash((1.0-_a/ones), tf.reduce_sum) for _a in da]) / count_params(da))
+      #def count_params(variables):
+      #  return np.sum([np.prod(self.ops.shape(t)) for t in variables ])
+      #self.gan.add_metric('dropout-one', ones)
+      #self.gan.add_metric('dropout-perc', dropout)
+      #self.gan.add_metric('dropout', sum([self.ops.squash((1.0-_a/ones), tf.reduce_sum) for _a in da]) / count_params(da))
 
       return [d_grads, g_grads]

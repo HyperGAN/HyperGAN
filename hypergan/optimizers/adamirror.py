1726e8e6778b07279362a06a1a92b117cabf7213
Martyn Garcia <martyn@255bits.com>
2020-02-17 09:02:10 -0700

    [fix] optimistic adam

--- hypergan/optimizers/adamirror.py
+++ hypergan/optimizers/adamirror.py
@@ -76,6 +76,7 @@ class Adamirror(Optimizer):
                 # State initialization
                 if len(state) == 0:
                     state['step'] = 0
+                    state['step_size'] = torch.tensor(0.0)
                     # Exponential moving average of gradient values
                     state['exp_avg'] = torch.zeros_like(p.data, memory_format=torch.preserve_format)
                     # Exponential moving average of squared gradient values
@@ -91,13 +92,14 @@ class Adamirror(Optimizer):
                 beta1, beta2 = group['betas']
 
                 state['step'] += 1
+
+                denom = (exp_avg_sq.sqrt()).add_(group['eps'])
+                p.data.addcdiv_(state['step_size'], exp_avg, denom)
+
                 bias_correction1 = 1 - beta1 ** state['step']
                 bias_correction2 = 1 - beta2 ** state['step']
 
-                step_size = group['lr'] / bias_correction1
-
-                denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
-                p.data.addcdiv_(step_size, exp_avg, denom)
+                state['step_size'] = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
 
                 if group['weight_decay'] != 0:
                     grad.add_(group['weight_decay'], p.data)
@@ -109,8 +111,8 @@ class Adamirror(Optimizer):
                     # Maintains the maximum of all 2nd moment running avg. till now
                     torch.max(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
                     # Use the max. for normalizing running avg. of gradient
-                    denom = (max_exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
+                    denom = (max_exp_avg_sq.sqrt()).add_(group['eps'])
                 else:
-                    denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
+                    denom = (exp_avg_sq.sqrt()).add_(group['eps'])
 
-                p.data.addcdiv_(-2 * step_size, exp_avg, denom)
+                p.data.addcdiv_(-2 * state['step_size'], exp_avg, denom)

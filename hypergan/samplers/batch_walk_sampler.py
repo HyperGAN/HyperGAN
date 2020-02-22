314f925d0a089d5a44331c95eb2bfefcae1b0bc7
Mikkel Garcia <mikkel@255bits.com>
2020-02-21 21:12:55 -0700

    [fix] batch walk sampler

--- hypergan/samplers/batch_walk_sampler.py
+++ hypergan/samplers/batch_walk_sampler.py
@@ -3,23 +3,20 @@ from hypergan.samplers.base_sampler import BaseSampler
 from hypergan.viewer import GlobalViewer
 import numpy as np
 import torch
+import torch.nn as nn
 import time
 
 class BatchWalkSampler(BaseSampler):
     def __init__(self, gan, samples_per_row=8, session=None):
         BaseSampler.__init__(self, gan, samples_per_row)
-        self.z_start = None
-        self.y = None
-        self.x = None
         self.step = 0
-        self.steps = []
         self.step_count = 30
-        self.target = None
-        self.rows = 2
-        self.columns = 4
-        self.set_x_step = 0
-        self.latent1 = self.gan.latent.sample().data.cpu()
-        self.latent2 = self.gan.latent.sample().data.cpu()
+        self.latent1 = self.gan.latent.sample()
+        self.latent2 = self.gan.latent.sample()
+        direction = self.gan.latent.sample()
+        self.direction = direction / torch.norm(direction, p=2, dim=1, keepdim=True).expand_as(direction)
+        self.velocity = 10.0
+        self.hardtanh = nn.Hardtanh()
 
     def compatible_with(gan):
         if hasattr(gan, 'latent'):
@@ -31,12 +28,16 @@ class BatchWalkSampler(BaseSampler):
         self.step+=1
         if self.step > self.step_count:
             self.latent1 = self.latent2
-            self.latent2 = self.gan.latent.sample().data.cpu()
+            direction = self.gan.latent.sample()
+            self.direction = direction / torch.norm(direction, p=2, dim=1, keepdim=True).expand_as(direction)
+            self.velocity = 10.0
             self.step = 0
 
-        latent = self.latent2 * self.step / self.step_count + (1.0 - self.step / self.step_count) * self.latent1
+        latent = self.direction * self.step / self.step_count * self.velocity + self.latent1
+        latent = self.hardtanh(latent)
+        self.latent2 = latent
 
-        g = gan.generator.forward(latent.cuda().float())
+        g = gan.generator.forward(latent)
         #    gs.append(g)
         return [
             ('generator', g)

fe56573f318578b6359d06d0454af6992f344c20
Mikkel Garcia <mikkel@255bits.com>
2020-03-02 16:32:28 -0700

    [feature] load optimizer from config

--- hypergan/trainers/simultaneous_trainer.py
+++ hypergan/trainers/simultaneous_trainer.py
@@ -3,6 +3,7 @@ import torch
 import hyperchamber as hc
 import inspect
 
+from hypergan.gan_component import ValidationException, GANComponent
 from hypergan.trainers.base_trainer import BaseTrainer
 from hypergan.optimizers.adamirror import Adamirror
 
@@ -11,14 +12,19 @@ TINY = 1e-12
 class SimultaneousTrainer(BaseTrainer):
     """ Steps G and D simultaneously """
     def _create(self):
-        self.optimizer = torch.optim.Adam(self.gan.parameters(), lr=self.config.optimizer["learn_rate"], betas=(0.0,.999))
+        #self.optimizer = torch.optim.Adam(self.gan.parameters(), lr=self.config.optimizer["learn_rate"], betas=(0.0,.999))
         #self.optimizer = Adamirror(self.gan.parameters(), lr=self.config.optimizer["learn_rate"], betas=(0.0,.999))
         #self.adamirror = Adamirror(self.gan.parameters(), lr=self.config.optimizer["learn_rate"], betas=(0.9074537537537538,.997))
         #self.adamirror2 = Adamirror(self.gan.parameters(), lr=self.config.optimizer["learn_rate"]*3, betas=(0.9074537537537538,.997))
         #self.optimizer = self.adamirror
-        self.gan.add_component("optimizer", self.optimizer)
+        #self.gan.add_component("optimizer", self.optimizer)
         #self.gan.add_component("optimizer", self.adamirror2)
         #self.gan.add_component("optimizer", self.adamirror)
+        defn = self.config.optimizer
+        klass = GANComponent.lookup_function(None, defn['class'])
+        del defn["class"]
+        self.optimizer = klass(self.gan.parameters(), **defn)
+        self.gan.add_component("optimizer", self.optimizer)
 
     def required(self):
         return "".split()

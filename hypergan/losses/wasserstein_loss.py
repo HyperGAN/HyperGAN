00da5ce05b39dbe0a5933e65df6e689b0782a174
Mikkel Garcia <mikkel@255bits.com>
2020-03-05 21:06:53 -0700

    [feature] porting losses

--- hypergan/losses/wasserstein_loss.py
+++ hypergan/losses/wasserstein_loss.py
@@ -1,4 +1,5 @@
 import hyperchamber as hc
+import torch
 
 from hypergan.losses.base_loss import BaseLoss
 
@@ -7,17 +8,13 @@ class WassersteinLoss(BaseLoss):
     def _forward(self, d_real, d_fake):
         config = self.config
 
-        d_loss = -d_real + d_fake
-        g_loss = -d_fake
-
         if config.kl:
             # https://arxiv.org/abs/1910.09779
-            loss_real = tf.reduce_mean(tf.nn.relu(1.-d_real))
-            d_fake_norm = tf.reduce_mean(tf.math.exp(d_fake))+1e-8
-            d_fake_ratio = (tf.math.exp(d_fake)+1e-8) / d_fake_norm
-            loss_fake = d_fake * d_fake_ratio
-            loss_fake = tf.reduce_mean(tf.nn.relu(1.+loss_fake))
-            d_loss = loss_real + loss_fake
-            g_loss = -loss_fake
+            d_fake_norm = torch.mean(d_fake.exp())+1e-8
+            d_fake_ratio = (d_fake.exp()+1e-8) / d_fake_norm
+            d_fake = d_fake * d_fake_ratio
+
+        d_loss = -d_real + d_fake
+        g_loss = -d_fake
 
         return [d_loss, g_loss]

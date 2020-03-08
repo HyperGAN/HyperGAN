691e0b50d0b15665df5ec77eeec7c605c4283e1f
Mikkel Garcia <mikkel@255bits.com>
2020-03-08 15:31:49 -0600

    [fix] alternating trainer

--- hypergan/trainers/alternating_trainer.py
+++ hypergan/trainers/alternating_trainer.py
@@ -16,34 +16,84 @@ class AlternatingTrainer(BaseTrainer):
     def required(self):
         return "".split()
 
+    def calculate_gradients(self, targets=['d','g']):
+        if 'g' in targets and \
+            ( self.config.train_g_every == None or
+              self.config.train_g_every and (self.gan.steps % self.config.train_g_every == 0) ):
+
+            self.g_optimizer.zero_grad()
+            for p in self.gan.g_parameters():
+                p.requires_grad = True
+            for p in self.gan.d_parameters():
+                p.requires_grad = False
+            _, g_loss = self.gan.forward_loss()#TODO targets=['d']
+            self.gan.add_metric('g_loss', g_loss.mean())
+            for hook in self.train_hooks:
+                loss = hook.forward()#TODO targets=['d']
+                if loss[1] is not None:
+                    g_loss += loss[1]
+
+            g_loss = g_loss.mean()
+            g_loss.backward(retain_graph=True)
+            g_grads = [p.grad for p in self.gan.g_parameters()]
+        else:
+            g_grads = []
+
+        if 'd' in targets \
+            and ( self.config.train_d_every == None or
+              (self.gan.steps % self.config.train_d_every == 0)) \
+            and ( self.config.freeze_d_every == None or 
+              (self.gan.steps % self.config.freeze_d_every) < \
+                (self.config.freeze_d_every - self.config.freeze_d_duration)):
+            self.d_optimizer.zero_grad()
+            for p in self.gan.d_parameters():
+                p.requires_grad = True
+            for p in self.gan.g_parameters():
+                p.requires_grad = False
+
+            d_loss, _ = self.gan.forward_loss()#TODO targets=['d']
+            self.gan.add_metric('d_loss', d_loss.mean())
+            for hook in self.train_hooks:
+                loss = hook.forward()#TODO targets=['d']
+                if loss[0] is not None:
+                    d_loss += loss[0]
+
+            d_loss = d_loss.mean()
+            d_loss.backward(retain_graph=True)
+            d_grads = [p.grad for p in self.gan.d_parameters()]
+        else:
+            d_grads = []
+
+        return d_grads, g_grads
+
+
     def _step(self, feed_dict):
         gan = self.gan
         config = self.config
         loss = gan.loss
         metrics = gan.metrics()
 
-        self.d_optimizer.zero_grad()
-
         self.before_step(self.current_step, feed_dict)
 
-        d_loss, g_loss = self.gan.forward_loss()
+        d_grads, _ = self.calculate_gradients(['d'])
+
         for hook in self.train_hooks:
-            loss = hook.forward()
-            if loss[0] is not None:
-                d_loss += loss[0]
+            d_grads, _ = hook.gradients(d_grads, _)
+        for p, np in zip(self.gan.d_parameters(), d_grads):
+            p.grad = np
 
+        if(len(d_grads) > 0):
+                self.d_optimizer.step()
 
-        d_loss.mean().backward()
-        self.d_optimizer.step()
+        _, g_grads = self.calculate_gradients(['g'])
 
-        self.g_optimizer.zero_grad()
-        d_loss, g_loss = self.gan.forward_loss()
         for hook in self.train_hooks:
-            loss = hook.forward()
-            if loss[1] is not None:
-                g_loss += loss[1]
-        g_loss.mean().backward()
-        self.g_optimizer.step()
+            _, g_grads = hook.gradients(_, g_grads)
+        for p, np in zip(self.gan.g_parameters(), g_grads):
+            p.grad = np
+
+        if(len(g_grads) > 0):
+            self.g_optimizer.step()
 
         if self.current_step % 10 == 0:
             self.print_metrics(self.current_step)

cc228878c9a09d10926fcc9c2b119aa586d0ce02
Mikkel Garcia <mikkel@255bits.com>
2020-03-11 15:22:01 -0600

    [feature] variance type option

--- hypergan/trainers/accumulate_gradient_trainer.py
+++ hypergan/trainers/accumulate_gradient_trainer.py
@@ -13,11 +13,22 @@ class AccumulateGradientTrainer(AlternatingTrainer):
         self.g_optimizer = self.create_optimizer("g_optimizer")
         self.accumulated_g_grads = None
         self.accumulation_steps = 0
+        self.max_grads = None
+        self.min_grads = None
+        self.max_variance = None
         self.relu = torch.nn.ReLU()
 
     def calculate_gradients(self):
         accumulate = (self.config.accumulate or 3)
         if self.accumulation_steps == accumulate:
+            if self.config.type == 'variance':
+                for i, g in enumerate(self.accumulated_g_grads):
+                    range_stddev = (self.max_grads[i]-self.min_grads[i])/4.0
+                    spread_ratio = (range_stddev / (g+1e-12)).abs()
+                    doubt = torch.clamp(self.relu((self.config.allowed_variance or 1.0) - spread_ratio/(self.config.max_spread or 0.2)), max=1.0)
+                    if self.config.verbose:
+                        print("confidence >>", i, doubt.sum(), "/", np.prod(g.shape), "=", ("%d" % (doubt.sum()/np.prod(g.shape) * 100.0).item())+"%")
+                    self.accumulated_g_grads[i] = g * doubt
             g_grads = self.accumulated_g_grads
             d_grads = []
             #print("G_G", sum([g.abs().sum() for g in self.accumulated_g_grads[0]]), len(self.accumulated_g_grads))
@@ -27,11 +38,17 @@ class AccumulateGradientTrainer(AlternatingTrainer):
             gs = self.g_grads()
             if self.accumulated_g_grads is None:
                 self.accumulated_g_grads = [g.clone()/accumulate for g in gs]
+                if self.config.type == 'variance':
+                    self.max_grads = [g.clone() for g in gs]
+                    self.min_grads = [g.clone() for g in gs]
             else:
                 for i, g in enumerate(self.accumulated_g_grads):
                     if self.config.type == 'agree':
                         self.accumulated_g_grads[i] = (self.accumulated_g_grads[i] + gs[i].clone()/accumulate) * self.relu(torch.sign(self.accumulated_g_grads[i]*gs[i].clone()))
                     else:
+                        if self.config.type == 'variance':
+                            self.max_grads = torch.max(self.accumulated_g_grads[i], gs[i].clone())
+                            self.min_grads = torch.min(self.accumulated_g_grads[i], gs[i].clone())
                         self.accumulated_g_grads[i] += gs[i].clone() / accumulate
 
             #print("D_G", sum([g.abs().sum() for g in gs]), len(self.accumulated_g_grads))

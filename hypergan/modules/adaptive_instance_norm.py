c1e72466edb0dfefcce72c189bf66c614e62b719
Martyn Garcia <martyn@255bits.com>
2020-06-10 20:35:29 -0600

    [fix] add missing modules

--- hypergan/modules/adaptive_instance_norm.py
+++ hypergan/modules/adaptive_instance_norm.py
@@ -15,10 +15,24 @@ class AdaptiveInstanceNorm(nn.Module):
         feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
         return feat_mean, feat_std
 
+    def calc_mean_std1d(self, feat, eps=1e-5):
+        size = feat.size()
+        assert (len(size) == 3)
+        N, C = size[:2]
+        feat_var = feat.view(N, C, -1).var(dim=2) + eps
+        feat_std = feat_var.sqrt().view(N, C, 1)
+        feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1)
+        return feat_mean, feat_std
+
+
     def forward(self, content, style, epsilon=1e-5):
         style = style.view(content.shape[0], -1)
         gamma = self.gamma(style)
         beta = self.beta(style)
-        c_mean, c_var = self.calc_mean_std(content, epsilon)
+        if len(content.shape) == 4:
+            c_mean, c_var = self.calc_mean_std(content, epsilon)
+        elif len(content.shape) == 3:
+            c_mean, c_var = self.calc_mean_std1d(content, epsilon)
+
         c_std = (c_var + epsilon).sqrt()
         return (1+gamma.view(c_std.shape)) * ((content - c_mean) / c_std) + beta.view(c_std.shape)
--- hypergan/modules/residual.py
+++ hypergan/modules/residual.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class Residual(nn.Module):
-    def __init__(self, channels, filter=3, stride=1, padding=1, activation=nn.LeakyReLU):
+    def __init__(self, channels, filter=3, stride=1, padding=1, activation=nn.ReLU):
         super(Residual, self).__init__()
         self.conv = nn.Conv2d(channels, channels, filter, stride, padding)
         self.activation = activation()

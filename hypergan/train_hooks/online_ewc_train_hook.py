8adc241c01e159e7a40ff9bf064fb5b8339fa8ba
Mikkel Garcia <mikkel@255bits.com>
2020-06-16 23:00:21 -0600

    [fix] online ewc train hook

--- hypergan/train_hooks/online_ewc_train_hook.py
+++ hypergan/train_hooks/online_ewc_train_hook.py
@@ -25,38 +25,44 @@ class OnlineEWCTrainHook(BaseTrainHook):
           self.d_ewc_g = []
 
           for p in self.gan.d_parameters():
-              self.d_ewc_params += [Variable(p)]
-              self.d_ewc_g += [Variable(torch.zeros_like(p))]
+              self.d_ewc_params += [Variable(p, requires_grad=False)]
+              self.d_ewc_g += [Variable(torch.zeros_like(p), requires_grad=False)]
 
           self.g_ewc_params = []
           self.g_ewc_g = []
 
           for p in self.gan.g_parameters():
-              self.g_ewc_params += [Variable(p)]
-              self.g_ewc_g += [Variable(torch.zeros_like(p))]
+              self.g_ewc_params += [Variable(p, requires_grad=False)]
+              self.g_ewc_g += [Variable(torch.zeros_like(p), requires_grad=False)]
       
-      g_loss = self.gan.trainer.g_loss.mean()
       d_loss = self.gan.trainer.d_loss.mean()
       d_params = list(self.gan.d_parameters())
       g_params = list(self.gan.g_parameters())
       d_grads = torch_grad(d_loss, d_params, create_graph=True, retain_graph=True)
       self.d_loss = 0
-      with torch.no_grad():
-          for i, (dp, dp_g) in enumerate(zip(d_params, d_grads)):
-              self.d_ewc_g[i] += dp_g
-              self.d_loss += (self.config.gamma or 1.0) * ((dp - self.d_ewc_params[i]) ** 2 * self.d_ewc_g[i] ** 2).sum()
-              self.d_ewc_g[i] += dp_g
-              self.d_ewc_params[i] = 0.5*dp + 0.5*self.d_ewc_params[i]
-      self.g_loss = 0
-      g_grads = torch_grad(g_loss, g_params, create_graph=True, retain_graph=True)
-      with torch.no_grad():
-          for i, (gp, gp_g) in enumerate(zip(g_params, g_grads)):
-              self.g_ewc_g[i] += gp_g.detach()
-              self.g_loss += (self.config.gamma or 1.0) * ((gp - self.g_ewc_params[i]) ** 2 * self.g_ewc_g[i] ** 2).sum()
-              self.g_ewc_g[i] += gp_g.detach()
-              self.g_ewc_params[i] = 0.5*gp + 0.5*self.g_ewc_params[i]
- 
+      decay = (self.config.d_decay or self.config.decay or 0.1)
+      grad_decay = (self.config.d_grad_decay or self.config.grad_decay or 1.0)
+      for i, (dp, dp_g) in enumerate(zip(d_params, d_grads)):
+          self.d_loss += (self.config.gamma or 1.0) * ((dp - self.d_ewc_params[i]) ** 2 * self.d_ewc_g[i]).sum()
+          with torch.no_grad():
+              self.d_ewc_g[i] = (1.0-grad_decay) * (self.d_ewc_g[i]**2) + dp_g**2
+              self.d_ewc_params[i] = decay*dp + (1.0-decay)*self.d_ewc_params[i]
       self.gan.add_metric('ewc_d', self.d_loss)
+      self.d_loss = torch.min(torch.tensor([self.d_loss, 100.0]))
+      if self.config.skip_g:
+          return [self.d_loss, None]
+      self.g_loss = 0
+      g_loss = self.gan.trainer.g_loss.mean()
+      g_grads = torch_grad(g_loss, g_params, create_graph=True, retain_graph=True)
+      decay = (self.config.g_decay or self.config.decay or 0.1)
+      grad_decay = (self.config.g_grad_decay or self.config.grad_decay or 1.0)
+      for i, (gp, gp_g) in enumerate(zip(g_params, g_grads)):
+          self.g_loss += (self.config.gamma or 1.0) * ((gp - self.g_ewc_params[i]) ** 2 * self.g_ewc_g[i]).sum()
+          with torch.no_grad():
+              self.g_ewc_g[i] = (1.0-grad_decay) * (self.g_ewc_g[i]**2) + gp_g**2
+              self.g_ewc_params[i] = decay*gp + (1.0-decay)*self.g_ewc_params[i]
+      self.g_loss = torch.min(torch.tensor([self.g_loss, 100.0]))
+ 
       self.gan.add_metric('ewc_g', self.g_loss)
 
       return [self.d_loss, self.g_loss]

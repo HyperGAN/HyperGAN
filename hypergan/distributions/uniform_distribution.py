3c9b3fa72adcf0f2f035830e626391fa38c8dc01
Mikkel Garcia <mikkel@255bits.com>
2020-06-20 14:42:02 -0600

    [feature] uniform_distribution remove tensorflow

--- hypergan/distributions/uniform_distribution.py
+++ hypergan/distributions/uniform_distribution.py
@@ -15,6 +15,9 @@ class UniformDistribution(BaseDistribution):
         self.current_width = 1
         self.current_height = 1
         self.current_input_size = config["z"]
+        if self.config.projections is not None:
+            self.current_input_size *= len(self.config.projections)
+            self.current_channels *= len(self.config.projections)
 
     def required(self):
         return "".split()
@@ -46,12 +49,16 @@ class UniformDistribution(BaseDistribution):
         return self.lookup_function(projection)
 
     def sample(self):
-        #if 'projections' in config:
-        #    for projection in config.projections:
-        #        projections.append(self.lookup(projection)(config, gan, self.z))
-        #else:
-        #        projections.append(self.z)
-        return self.z.sample(torch.Size([self.gan.batch_size(), self.config.z])).view(self.gan.batch_size(), self.config.z).cuda()
+        z = self.z.sample(torch.Size([self.gan.batch_size(), self.config.z])).view(self.gan.batch_size(), self.config.z).cuda()
+        if self.config.projections is None:
+            return z
+        projections = []
+        for projection in self.config.projections:
+            projections.append(self.lookup(projection)(self.config, self.gan, z))
+        ps = []
+        for p in projections:
+            ps.append(z)
+        return torch.cat(ps, -1)
 
     def next(self):
         self.instance = self.sample()
@@ -60,77 +67,14 @@ class UniformDistribution(BaseDistribution):
 def identity(config, gan, net):
     return net
 
-def sphere(config, gan, net):
-    net = gaussian(config, gan, net)
-    spherenet = tf.square(net)
-    if len(spherenet.get_shape()) == 2:
-        spherenet = tf.reduce_sum(spherenet, 1)
-        lam = tf.sqrt(spherenet+TINY)
-        return net/tf.reshape(lam,[int(lam.get_shape()[0]), 1])
-    else:
-        spherenet = tf.reduce_sum(spherenet, 3)
-        lam = tf.sqrt(spherenet+TINY)
-        return net/tf.reshape(lam,[int(lam.get_shape()[0]), int(lam.get_shape()[1]), int(lam.get_shape()[2]), 1])
-
-def modal(config, gan, net):
-    net = tf.round(net*float(config.modes))/float(config.modes)
+def round(config, gan, net):
+    net = torch.round(net)
     return net
 
 def binary(config, gan, net):
-    net = tf.greater(net, 0)
-    net = tf.cast(net, tf.float32)
+    net = torch.gt(net, 0)
+    net = torch.type(net, torch.Float)
     return net
 
 def zero(config, gan, net):
-    return tf.zeros_like(net)
-
-def modal_gaussian(config, gan, net):
-    a = modal(config, gan, net)
-    b = gaussian(config, gan, net)
-    return a + b * 0.1
-
-def modal_sphere(config, gan, net):
-    net = gaussian(config, gan, net)
-    net = modal(config, gan, net)
-    spherenet = tf.square(net)
-    spherenet = tf.reduce_sum(spherenet, 1)
-    lam = tf.sqrt(spherenet+TINY)
-    return net/tf.reshape(lam,[int(lam.get_shape()[0]), 1])
-
-def modal_sphere_gaussian(config, gan, net):
-    net = modal_sphere(config, gan, net)
-    return net + (gaussian(config, gan, net) * 0.01)
-
-# creates normal distribution from uniform values https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
-def gaussian(config, gan, net):
-    z_dim = net.get_shape().as_list()[-1]
-    net = (net + 1) / 2
-
-    if len(gan.ops.shape(net)) == 4:
-        za = tf.slice(net, [0,0,0,0], [gan.batch_size(), -1, -1, z_dim//2])
-        zb = tf.slice(net, [0,0,0,z_dim//2], [gan.batch_size(), -1, -1, z_dim//2])
-    else:
-        za = tf.slice(net, [0,0], [gan.batch_size(), z_dim//2])
-        zb = tf.slice(net, [0,z_dim//2], [gan.batch_size(), z_dim//2])
-
-    pi = np.pi
-    ra = tf.sqrt(-2 * tf.log(za+TINY))*tf.cos(2*pi*zb)
-    rb = tf.sqrt(-2 * tf.log(za+TINY))*tf.sin(2*pi*zb)
-
-    return tf.reshape(tf.concat(axis=len(net.get_shape())-1, values=[ra, rb]), net.get_shape())
-
-
-def periodic(config, gan, net):
-    return periodic_triangle_waveform(net, config.periods)
-
-def periodic_gaussian(config, gan, net):
-    net = periodic_triangle_waveform(net, config.periods)
-    return gaussian(config, gan, net)
-
-def periodic_triangle_waveform(z, p):
-    return 2.0 / np.pi * tf.asin(tf.sin(2*np.pi*z/p))
-
-def bounded(net):
-    minim = -1
-    maxim = 1
-    return tf.minimum(tf.maximum(net, minim), maxim)
+    return torch.zeros_like(net)

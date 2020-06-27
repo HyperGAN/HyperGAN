09fd9b99a13bdbf537be66510c36567281b627f2
Martyn Garcia <martyn@255bits.com>
2020-06-27 01:16:15 -0600

    [fix] learned_noise ez_norm layers

--- hypergan/modules/ez_norm.py
+++ hypergan/modules/ez_norm.py
@@ -3,14 +3,18 @@ import torch.nn as nn
 from hypergan.modules.modulated_conv2d import EqualLinear
 
 class EzNorm(nn.Module):
-    def __init__(self, style_size, content_size, equal_linear=False, dim=1):
+    def __init__(self, style_size, content_size, channels, equal_linear=False, use_conv=True, dim=1):
         super(EzNorm, self).__init__()
         if equal_linear:
-            self.beta = EqualLinear(style_size, content_size, lr_mul=1e-8)
+            #self.beta = EqualLinear(style_size, content_size, lr_mul=1e-8)
+            layer = nn.Conv2d(channels, 1, 1, 1, padding = (0, 0))
             #self.gamma = EqualLinear(style_size, content_size, lr_mul=0.01)
         else:
             self.beta = nn.Linear(style_size, content_size)
-            #self.gamma = nn.Linear(style_size, content_size)
+        self.use_conv = use_conv
+        if self.use_conv:
+            self.conv = nn.Conv2d(channels, 1, 1, 1, padding = (0, 0))
+            #self.conv2 = nn.Conv2d(channels, 1, 1, 1, padding = (0, 0))
         self.dim = dim
 
     def forward(self, content, style, epsilon=1e-5):
@@ -19,5 +23,7 @@ class EzNorm(nn.Module):
         view = [N, 1, 1, 1]
         view[self.dim] = D
 
-        #return content * self.gamma(style).view(*view) + self.beta(style).view(*view)
-        return content + self.beta(style).view(*view)
+        if self.use_conv:
+            return content + self.beta(style).view(*view) * self.conv(content)
+        else:
+            return content + self.beta(style).view(*view)
--- hypergan/modules/learned_noise.py
+++ hypergan/modules/learned_noise.py
@@ -2,19 +2,11 @@ import torch.nn as nn
 import torch
 
 class LearnedNoise(nn.Module):
-    def __init__(self):
+    def __init__(self, c, h, w):
         super().__init__()
 
-        self.weight = nn.Parameter(torch.zeros(1))
+        self.weight = nn.Parameter(torch.randn(1, c, h, w)*0.1)
 
-    def forward(self, image, noise=None):
-        if noise is None:
-            if len(image.shape) == 4:
-                batch, _, height, width = image.shape
-                noise = image.new_empty(batch, 1, height, width).normal_()
-            elif len(image.shape) == 5:
-                batch, n, _, height, width = image.shape
-                noise = image.new_empty(batch, n, 1, height, width).normal_()
+    def forward(self, input):
 
-        return image + self.weight * noise
-
+        return input + self.weight

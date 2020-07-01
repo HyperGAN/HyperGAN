7089233eb8fe6aaf06575d48c8ad6642faa579ec
Mikkel Garcia <mikkel@255bits.com>
2020-07-01 01:19:29 -0600

    [fix] adversarial norm using discriminator

--- hypergan/train_hooks/adversarial_norm_train_hook.py
+++ hypergan/train_hooks/adversarial_norm_train_hook.py
@@ -22,8 +22,8 @@ class AdversarialNormTrainHook(BaseTrainHook):
                     ]
         self.relu = torch.nn.ReLU()
         self.target = [Parameter(x, requires_grad=True) for x in self.gan.discriminator_real_inputs()]
-        #self.x_mod_target = torch.zeros_like(self.target[0])
-        #self.g_mod_target = torch.zeros_like(self.target[0])
+        self.x_mod_target = torch.zeros_like(self.target[0])
+        self.g_mod_target = torch.zeros_like(self.target[0])
 
     def forward(self, d_loss, g_loss):
         if self.config.mode == "real" or self.config.mode is None:
@@ -31,21 +31,20 @@ class AdversarialNormTrainHook(BaseTrainHook):
                 target.data = data.clone()
             d_fake = self.gan.d_fake
             d_real = self.gan.forward_discriminator(self.target)
-            loss, norm, mod_target = self.regularize_adversarial_norm(d_real, d_fake, self.target)
-            #if mod_target is not None:
-            #    self.x_mod_target = mod_target[0]
+            norm = self.regularize_adversarial_norm(d_real, d_fake, self.target)
+            if mod_target is not None:
+                self.x_mod_target = mod_target[0]
+            #todo broken
         elif self.config.mode == "fake":
             for target, data in zip(self.target, self.gan.discriminator_fake_inputs()):
                 target.data = data.clone()
             d_fake = self.gan.forward_discriminator(self.target)
             d_real = self.gan.d_real
-            loss, norm, mod_target = self.regularize_adversarial_norm(d_real, d_fake, self.target)
-            if mod_target is not None:
-                norm = self.relu(self.config.offset-((mod_target[0] - self.gan.g) ** 2))
+            grads = self.regularize_adversarial_norm(d_real, d_fake, self.target)
+            gadv = [_d1 + _t for _d1, _t in zip(grads, [self.gan.g])]
+            self.g_mod_target = gadv[0]
 
-
-        if loss is None:
-            return [None, None]
+            norm = self.gan.loss.forward_adversarial_norm(self.gan.forward_discriminator(gadv), self.gan.d_fake)
 
         if self.config.loss:
           if "g" in self.config.loss:
@@ -58,24 +57,14 @@ class AdversarialNormTrainHook(BaseTrainHook):
               self.d_loss = self.gammas[0] * norm.mean()
               self.gan.add_metric('an_d', self.d_loss)
               self.g_loss = self.gammas[1] * norm.mean()
-              self.gan.add_metric('an_g', self.d_loss)
+              self.gan.add_metric('an_g', self.g_loss)
         else:
             self.d_loss = self.gamma * norm.mean()
             self.gan.add_metric('an_d', self.d_loss)
 
         return [self.d_loss, self.g_loss]
 
-    def regularize_adversarial_norm(self, d1_logits, d2_logits, target):
-        loss = self.gan.loss.forward_adversarial_norm(d1_logits, d2_logits)
+    def regularize_adversarial_norm(self, d_real, d_fake, target):
+        loss = self.gan.loss.forward_adversarial_norm(d_real, d_fake)
 
-        if loss == 0:
-            return [None, None, None]
-
-        d1_grads = torch_grad(outputs=loss, inputs=target, retain_graph=True, create_graph=True)
-        d1_norm = [torch.norm(_d1_grads.view(-1),p=2,dim=0) for _d1_grads in d1_grads]
-        reg_d1 = d1_norm[0]
-        for d1 in d1_norm[1:]:
-            reg_d1 = reg_d1 + d1
-        mod_target = [_d1 + _t for _d1, _t in zip(d1_grads, target)]
-
-        return loss, reg_d1, mod_target
+        return torch_grad(outputs=loss, inputs=target, retain_graph=True, create_graph=True)

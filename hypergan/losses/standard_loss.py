04e3b80877745c3c721c64b16a358a48f4159abb
Martyn Garcia <martyn@255bits.com>
2020-07-09 01:14:07 -0600

    [fix] use kl div for norm

--- hypergan/losses/standard_loss.py
+++ hypergan/losses/standard_loss.py
@@ -2,6 +2,7 @@ import hyperchamber as hc
 import torch
 
 from hypergan.losses.base_loss import BaseLoss
+import torch.nn.functional as F
 
 class StandardLoss(BaseLoss):
     def _forward(self, d_real, d_fake):
@@ -11,3 +12,8 @@ class StandardLoss(BaseLoss):
 
         return [d_loss, g_loss]
 
+    def div(self, p, q):
+        return F.sigmoid(p)*torch.log(F.sigmoid(p)/(F.sigmoid(q)+self.eps))
+        #return F.sigmoid(p)*torch.log(self.two*F.sigmoid(p)/(F.sigmoid(p)+F.sigmoid(q)+self.eps))
+    def forward_adversarial_norm(self, d_real, d_fake):
+        return self.div(d_real, d_fake).mean()

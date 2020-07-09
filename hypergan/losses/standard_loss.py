6ecffc6b419ed67c8738aa1487fe3a6c5d884710
Martyn Garcia <martyn@255bits.com>
2020-07-09 01:19:44 -0600

    [fix] add constants

--- hypergan/losses/standard_loss.py
+++ hypergan/losses/standard_loss.py
@@ -5,6 +5,12 @@ from hypergan.losses.base_loss import BaseLoss
 import torch.nn.functional as F
 
 class StandardLoss(BaseLoss):
+    def __init__(self, gan, config):
+        super(StandardLoss, self).__init__(gan, config)
+        self.relu = torch.nn.ReLU()
+        self.two = torch.Tensor([2.0]).cuda()
+        self.eps = torch.Tensor([1e-12]).cuda()
+
     def _forward(self, d_real, d_fake):
         criterion = torch.nn.BCEWithLogitsLoss()
         g_loss = criterion(d_fake, torch.ones_like(d_fake))

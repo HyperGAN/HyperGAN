15700dc6bbf845a2f212063982c00642a386d57e
Martyn Garcia <martyn@255bits.com>
2020-07-11 20:04:34 -0600

    [fix] batch_walk reflect direction

--- hypergan/samplers/batch_walk_sampler.py
+++ hypergan/samplers/batch_walk_sampler.py
@@ -9,12 +9,14 @@ import time
 class BatchWalkSampler(BaseSampler):
     def __init__(self, gan, samples_per_row=4, session=None):
         BaseSampler.__init__(self, gan, samples_per_row)
-        self.step = 0
-        self.step_count = 30
         self.latent1 = self.gan.latent.next()
         self.latent2 = self.gan.latent.next()
-        self.direction = (self.latent2-self.latent1) / self.step_count
+        self.velocity = 10/24.0
+        direction = self.gan.latent.next()
+        self.pos = self.latent1
+        self.direction = direction / torch.norm(direction, p=2, dim=1, keepdim=True).expand_as(direction)
         self.hardtanh = nn.Hardtanh()
+        self.ones = torch.ones_like(self.direction, device="cuda:0")
 
     def compatible_with(gan):
         if hasattr(gan, 'latent'):
@@ -23,18 +25,13 @@ class BatchWalkSampler(BaseSampler):
 
     def _sample(self):
         gan = self.gan
-        self.step+=1
-        if self.step > self.step_count:
-            self.latent1 = self.latent2
-            self.latent2 = self.gan.latent.next()
-            self.direction = (self.latent2-self.latent1) / self.step_count
-            self.step = 0
 
-        latent = self.direction * self.step + self.latent1
-        latent = self.hardtanh(latent)
-        self.latent2 = latent
+        self.pos = self.direction * self.velocity + self.pos
+        mask = torch.gt(self.pos, self.ones)
+        mask += torch.lt(self.pos, -self.ones)
+        self.direction = self.direction + 2 * self.direction * (-self.ones * mask)
 
-        g = gan.generator.forward(latent)
+        g = gan.generator.forward(self.pos)
         #    gs.append(g)
         return [
             ('generator', g)

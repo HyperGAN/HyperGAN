1baa4d314f19b7ddb93f4c2eabcf9805650c4e25
Mikkel Garcia <mikkel@255bits.com>
2020-07-15 23:10:43 -0600

    [fix] attention

--- hypergan/modules/attention.py
+++ hypergan/modules/attention.py
@@ -6,31 +6,20 @@ class Attention(nn.Module):
     def __init__(self,in_dim):
         super(Attention,self).__init__()
         self.chanel_in = in_dim
-        
         self.f = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
         self.g = nn.Conv2d(in_channels = in_dim , out_channels = in_dim, kernel_size= 1)
         self.h = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
-        self.o = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
-        #self.gamma = nn.Parameter(torch.zeros(1))
+        self.v = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.gamma = nn.Parameter(torch.tensor(0.0))
 
-        self.softmax  = nn.Softmax(dim=-1) #
+        self.softmax  = nn.Softmax(dim=1) #
     def forward(self,x):
-        """
-            inputs :
-                x : input feature maps( B X C X W X H)
-            returns :
-                out : self attention value + input feature 
-                attention: B X N X N (N is Width*Height)
-        """
         m_batchsize,C,width ,height = x.size()
-        f  = self.f(x).view(m_batchsize,width*height, -1)
-        g =  self.g(x).view(m_batchsize,width*height, -1).permute(0,2,1)
-        s =  torch.bmm(g,f)
-        beta = self.softmax(s)
-        h = self.h(x).view(m_batchsize,width*height,-1)
+        f  = self.f(x).view(m_batchsize,C,width*height).permute(0,2,1)
+        g =  self.g(x).view(m_batchsize,C,width*height)
+        fg =  torch.bmm(f,g)
+        attention_map = self.softmax(fg)
+        h = self.h(x).view(m_batchsize,C,width*height)
 
-        out = torch.bmm(beta, h.permute(0,2,1) )
-        out = self.o(out.view(x.shape))
-        out = out.view(m_batchsize,C,width,height)
-        
-        return out
+        fgh = torch.bmm(h, attention_map )
+        return self.v(fgh.view(x.shape))

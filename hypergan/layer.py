a9a82324d2c7a9b04c32c84dad4be114d8af1c27
Martyn Garcia <martyn@255bits.com>
2020-07-20 04:12:50 -0600

    [fix] add missing file

---
+++ hypergan/layer.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class Layer(nn.Module):
+    def __init__(self, component, args, options):
+        super(Layer, self).__init__()
+        self.args = args
+        self.options = options
+
+    def forward(self, input, context):
+        pass
+
+    def output_size(self):
+        pass

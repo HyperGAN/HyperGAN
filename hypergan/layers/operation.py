d00575c002b4273ba688d190926ebb229db38c22
Martyn Garcia <martyn@255bits.com>
2020-07-26 03:24:58 -0600

    [fix] cat layer

--- hypergan/layers/operation.py
+++ hypergan/layers/operation.py
@@ -5,6 +5,8 @@ import hypergan as hg
 import hyperchamber as hc
 import pyparsing
 
+from hypergan.gan_component import ValidationException
+
 class Operation(hg.Layer):
     def __init__(self, operation, component, args, options):
         super(Operation, self).__init__(component, args, options)
@@ -28,19 +30,21 @@ class Operation(hg.Layer):
                 layers.append(LearnedNoise())
                 layer_names.append(None)
             elif arg in component.named_layers:
-                layers.append(NoOp())
+                layers.append(None)
                 layer_names.append("layer "+arg)
             elif arg in component.gan.named_layers:
                 layers.append(component.gan.named_layers[arg])
                 layer_names.append(None)
+            elif arg in component.context_shapes:
+                layers.append(None)
+                layer_names.append(arg)
             elif type(arg) == pyparsing.ParseResults and type(arg[0]) == hg.parser.Pattern:
                 parsed = arg[0]
                 parsed.parsed_options = hc.Config(parsed.options)
                 layers.append(component.build_layer(parsed.layer_name, parsed.args, parsed.parsed_options))
                 layer_names.append(parsed.layer_name)
             else:
-                print("arg", type(arg))
-                raise "Could not parse add layer "
+                raise ValidationException("Could not parse operation layer '" + arg + "'")
         return layers, layer_names
 
     def output_size(self):
@@ -51,10 +55,12 @@ class Operation(hg.Layer):
         for layer, layer_name in zip(self.layers, self.layer_names):
             if layer_name == "self":
                 layer_output = input
-            elif layer_name.split(" ")[0] == 'layer':
-                layer_output = context[layer_name.split(" ")[1]]
             elif isinstance(layer, hg.Layer):
                 layer_output = layer(input, context)
+            elif layer_name.split(" ")[0] == 'layer':
+                layer_output = context[layer_name.split(" ")[1]]
+            elif layer_name in context:
+                layer_output = context[layer_name]
             else:
                 layer_output = layer(input)
             if output is None:
--- hypergan/layers/segment_softmax.py
+++ hypergan/layers/segment_softmax.py
@@ -11,12 +11,12 @@ class SegmentSoftmax(hg.Layer):
         self.softmax = nn.Softmax(dim=2)
 
     def output_size(self):
-        return LayerSize(self.channels, self.dims[1], self.dims[2])
+        return LayerSize(*([self.channels]+self.dims[1:]))
 
     def forward(self, input, context):
         content, segment = torch.split(input, input.shape[1]//2, 1)
         net_in = content.view(content.shape[0], content.shape[1]//self.channels, self.channels, content.shape[2], content.shape[3])
         segment = segment.view(content.shape[0], content.shape[1]//self.channels, self.channels, content.shape[2], content.shape[3])
         selection = self.softmax(segment)
-        rendered = (selection * net_in).sum(dim=1)
-        return rendered.view([input.shape[0]]+list(self.output_size().dims))
+        rendered = (selection * net_in).sum(dim=1).view([input.shape[0]]+list(self.output_size().dims))
+        return rendered

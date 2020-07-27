01fff4d4fa465ed2afd97c6531e997838ab4c788
Martyn Garcia <martyn@255bits.com>
2020-07-27 10:46:41 -0600

    [doc] layer add

--- hypergan/layers/add.py
+++ hypergan/layers/add.py
@@ -3,6 +3,35 @@ from hypergan.layer_size import LayerSize
 from . import Operation
 
 class Add(Operation):
+    """
+        ---
+        description: 'layer add for configurable component'
+        ---
+
+        # add layer
+
+        Adds two or more layers together. Accepts nested layer definitions.
+
+        ## input size
+
+        Any number of matching tensors
+
+        ## output size
+
+        Same as input size
+
+        ## syntax
+
+        ```json
+          "add [layer]*"
+        ```
+
+        ## examples
+
+        ```json
+          "add self (attention)"
+        ```
+    """
     def __init__(self, component, args, options):
         super(Add, self).__init__("+", component, args, options)
 

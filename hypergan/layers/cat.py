7edd4d310a9090707fd878d1ee49777ecbe0765e
Martyn Garcia <martyn@255bits.com>
2020-08-03 11:01:41 -0600

    [fix] cat layer output shape

--- hypergan/layers/cat.py
+++ hypergan/layers/cat.py
@@ -35,22 +35,5 @@ class Cat(Operation):
 
     def __init__(self, component, args, options):
         super(Cat, self).__init__("cat", component, args, options)
-
-    #TODO custom output_size
-    #def layer_cat(self, net, args, options):
-    #    result = self.operation_layer(net, args, options, "cat")
-    #    dims = None
-    #    for arg in args:
-    #        if arg == "self":
-    #            new_size = self.current_size
-    #        else:
-    #            new_size = self.layer_output_sizes[args[1]]
-    #        if dims is None:
-    #            dims = new_size.dims
-    #        else:
-    #            dims = [dims[0]+new_size.dims[0]] + list(dims[1:])
-
-    #    self.current_size = LayerSize(*dims)
-    #    return result
-
+        self.size = LayerShape(sum([layer.channels for layer in self.layer_sizes]), *self.size.dims[1:])
 

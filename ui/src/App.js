e85d41cb57f6f4aca59af2f71e4748f7fbee8185
Martyn Garcia <martyn@255bits.com>
2020-08-11 08:00:52 -0600

    [feature] initial websocket connection

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const socket = new WebSocket("ws://localhost:9999")
 //import HgApiClient from './HgApiClient.js';
 
 function App() {
+  const [messages, setMessages] = React.useState([]);
+  socket.onopen = () => {
+    setMessages([...messages, "connected"])
+  };
+  socket.onmessage = (message) => {
+    setMessages([...messages, message.data])
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Listening for HyperGAN
+          <button onClick={_=>{
+            socket.send("ping")
+          }}>
+          Ping
+          </button>
+          {messages.map((msg,index) => <li>{msg}</li>)}
         </p>
       </header>
     </div>

f930a2752c7eb304f042e87a873afa19a1bda340
Martyn Garcia <martyn@255bits.com>
2020-08-10 20:35:05 -0600

    Setup react app

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,19 @@
 const electron = require('electron');
 const {app, BrowserWindow, Menu} = require('electron');
+const isDev = require('electron-is-dev');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is garbage collected.
 let mainWindow;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   if (process.platform != 'darwin')
     app.quit();
 });
 
-// This method will be called when Electron has done everything
-// initialization and ready for creating browser windows.
 app.on('ready', function() {
-  // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration: true}});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  const startURL = isDev ? 'http://localhost:3000' : ('file://' + __dirname + '/build/index.html');
+  mainWindow.loadURL(startURL);
 
   var application_menu = [
     {
@@ -62,17 +57,14 @@ app.on('ready', function() {
           ]
         }
       ]
-    }
+    },
+
   ];
 
   menu = Menu.buildFromTemplate(application_menu);
   Menu.setApplicationMenu(menu);
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 });
---
+++ ui/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//import HgApiClient from './HgApiClient.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Listening for HyperGAN
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

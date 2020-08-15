43301e924c2c33de5dbd8299c0f36d4438535712
Martyn Garcia <martyn@255bits.com>
2020-08-15 14:45:53 -0600

    [fix] logging/initial websocket connection

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-const socket = new WebSocket("ws://localhost:9999")
+var socket;
 //import HgApiClient from './HgApiClient.js';
 
 function App() {
   const [messages, setMessages] = React.useState([]);
-  socket.onopen = () => {
-    setMessages([...messages, "connected"])
-  };
-  socket.onmessage = (message) => {
-    setMessages([...messages, message.data])
-  };
+  function addMessage() {
+    setMessages([...messages.slice(-3), "reconnecting"])
+  }
+  function connect() {
+    socket = new WebSocket("ws://localhost:9999")
+    socket.onclose = () => {
+      addMessage("reconnecting");
+      setTimeout(() => {
+        connect();
+      }, 10000);
+    };
+    socket.onopen = () => {
+      addMessage("connected");
+    };
+    socket.onmessage = (message) => {
+      addMessage(message.data);
+    };
+  }
+  connect();
   return (
     <div className="App">
       <header className="App-header">

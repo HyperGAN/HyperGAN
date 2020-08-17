c7a185464060fbce142b789c195ddb2156712e38
Mikkel Garcia <mikkel@255bits.com>
2020-08-16 22:12:59 -0600

    [feature] optimize roundrobin

--- hypergan/backends/roundrobin_backend.py
+++ hypergan/backends/roundrobin_backend.py
@@ -13,7 +13,7 @@ def create_input(input_config):
     return klass(input_config)
 
 
-def train(device, gan, inputs, loaded_event, report_weights_queue, set_weights_queue, report_weights_event):
+def train(device, head_device, gan, inputs, loaded_event, report_weights_queue, set_weights_queue, report_weights_event):
     gan.inputs = inputs
     #torch.manual_seed(device)
     gan.generator = gan.generator.to(device)
@@ -27,11 +27,17 @@ def train(device, gan, inputs, loaded_event, report_weights_queue, set_weights_q
     while(True):
         gan.step()
         if report_weights_event.is_set():
-            report_weights_queue.put(list(gan.parameters()))
+            if device == head_device:
+                report_weights_queue.put(list([p.clone().detach() for p in gan.parameters()]))
+            else:
+                report_weights_queue.put(list(gan.parameters()))
+
             params = set_weights_queue.get()
             for p1, p2 in zip(gan.parameters(), params):
                 with torch.no_grad():
-                    p1.set_(p2.to(p1.device))
+                    if(p2.device != p1.device):
+                        p2 = p2.to(p1.device)
+                    p1.set_(p2)
             del params
             report_weights_event.clear()
 
@@ -47,12 +53,15 @@ class RoundrobinBackend(Backend):
         self.report_weights_queue = []
         self.set_weights_queue = []
         loaded_events = []
+        head_device = torch.device(list(self.gan.parameters())[0].device).index
+        self.head_device=head_device
         mp.set_start_method('spawn', force=True)
         if devices == "-1":
             print("Running on all available devices: ", torch.cuda.device_count())
             devices = list(range(torch.cuda.device_count()))
         else:
             devices = [int(d) for d in devices.split(",")]
+        self.devices = devices
         print("Devices:", devices)
         for device in devices:
             loaded_event = mp.Event()
@@ -60,7 +69,7 @@ class RoundrobinBackend(Backend):
             set_weights_queue = mp.Queue()
             report_weights_queue = mp.Queue()
             inputs = cli.create_input(rank=device)
-            p = mp.Process(target=train, args=(device, gan, inputs, loaded_event, report_weights_queue, set_weights_queue, report_weights_event))
+            p = mp.Process(target=train, args=(device, head_device, gan, inputs, loaded_event, report_weights_queue, set_weights_queue, report_weights_event))
             p.start()
             self.processes.append(p)
             self.report_weights_event.append(report_weights_event)
@@ -76,8 +85,13 @@ class RoundrobinBackend(Backend):
         params = self.report_weights_queue[selected].get()
         for p1, p2 in zip(self.gan.parameters(), params):
             with torch.no_grad():
-                p1.set_((p1+p2.to(p1.device))/2.0)
+                if(p2.device != p1.device):
+                    p2 = p2.to(p1.device)
+                p1.set_((p1+p2)/2.0)
         del params
-        self.set_weights_queue[selected].put(list(self.gan.parameters()))
+        if self.devices[selected] == self.head_device:
+            self.set_weights_queue[selected].put(list([p.clone().detach() for p in self.gan.parameters()]))
+        else:
+            self.set_weights_queue[selected].put(list(self.gan.parameters()))
         self.sync += 1
         self.cli.sample()

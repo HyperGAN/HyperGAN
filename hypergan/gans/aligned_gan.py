613d4f902f6cbea8d2a078a588298e260eb1a9b3
Martyn Garcia <martyn@255bits.com>
2020-12-24 06:38:45 -0700

    [feature] aligned gan ali term

--- hypergan/gans/aligned_gan.py
+++ hypergan/gans/aligned_gan.py
@@ -30,30 +30,55 @@ class AlignedGAN(BaseGAN):
     def create(self):
         self.latent = self.create_component("latent")
         self.x = self.inputs.next()[0]
-        self.generator = self.create_component("generator", input=self.x, context_shapes={"y": LayerShape(1)})
-        self.discriminator = self.create_component("discriminator")
+        if self.config.ali:
+            self.encoder = self.create_component("encoder", input=self.x, context_shapes={"y": LayerShape(1)})
+            self.generator = self.create_component("generator", input=self.encoder, context_shapes={"y": LayerShape(1)})
+            self.discriminator = self.create_component("discriminator", context_shapes={"z": self.encoder.layer_shape()})
+        else:
+            self.generator = self.create_component("generator", input=self.x, context_shapes={"y": LayerShape(1)})
+            self.discriminator = self.create_component("discriminator")
+
         self.classification = 0
 
     def forward_discriminator(self, inputs):
-        return self.discriminator(inputs[0])
+        if self.config.ali:
+            return self.discriminator(inputs[0], context={"z":inputs[1]})
+        else:
+            return self.discriminator(inputs[0])
 
     def forward_pass(self):
-        if len(self.inputs.datasets) > 1:
-            self.classification = self.classification % len(self.inputs.datasets)
         self.x = self.inputs.next()
         self.y = self.inputs.next(1)
         self.augmented_latent = self.train_hooks.augment_latent(self.latent.next())
         b = self.x.shape[0]
-        y_ = torch.randint(0, len(self.inputs.datasets), (b, )).to(self.x.device)
-        g = self.generator(self.x, context={"y": y_.float().view(b, 1)})
-        self.latent_y = y_
-        self.g = g
         self.augmented_x = self.train_hooks.augment_x(self.x)
         self.augmented_y = self.train_hooks.augment_x(self.y)
-        self.augmented_g = self.train_hooks.augment_g(self.g)
-        d_real = self.forward_discriminator([self.augmented_x]) * (1 - self.config.interpolate)
-        d_real += self.forward_discriminator([self.augmented_y]) * (self.config.interpolate)
-        d_fake = self.forward_discriminator([self.augmented_g])
+
+        if self.config.ali:
+            encoding_x = self.encoder(self.x)
+            encoding_y = self.encoder(self.y)
+            self.encoding_y = encoding_y
+            self.encoding_x = encoding_x
+            self.g = self.generator(encoding_x)
+            self.augmented_g = self.train_hooks.augment_g(self.g)
+            x_args = [self.augmented_x, encoding_y]
+            g_args = [self.augmented_g, encoding_x]
+            y_args = [self.augmented_y, encoding_y]
+
+        else:
+            self.g = self.generator(self.x, context={"y": y_.float().view(b, 1)})
+            self.augmented_g = self.train_hooks.augment_g(self.g)
+            x_args = [self.augmented_x]
+            g_args = [self.augmented_g]
+            y_args = [self.augmented_y]
+
+        if self.config.interpolate:
+            d_real = self.forward_discriminator(x_args)
+            d_real *= (1 - self.config.interpolate)
+            d_real += self.forward_discriminator(y_args) * (self.config.interpolate)
+        else:
+            d_real = self.forward_discriminator(y_args)
+        d_fake = self.forward_discriminator(g_args)
         self.d_fake = d_fake
         self.d_real = d_real
         self.classification += 1
@@ -71,7 +96,6 @@ class AlignedGAN(BaseGAN):
 
         return [d_loss, g_loss]
 
-
     def discriminator_components(self):
         return [self.discriminator]
 
@@ -79,10 +103,17 @@ class AlignedGAN(BaseGAN):
         return [self.generator]
 
     def discriminator_fake_inputs(self):
-        return [[self.augmented_g]]
+        if self.config.ali:
+            return [[self.augmented_g, self.encoding_x]]
+        else:
+            return [[self.augmented_g]]
 
     def discriminator_real_inputs(self):
         if hasattr(self, 'y'):
-            return [self.augmented_y]
+            return [self.y, self.encoding_y]
+        elif hasattr(self, 'encoder'):
+            y = self.inputs.next(1)
+            return [y, self.encoder(y)]
         else:
-            return [self.inputs.next(1)]
+            y = self.inputs.next(1)
+            return [y]

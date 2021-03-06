a01ba4ea308272ce4bb49c1c1d4f4294951be4db
Martyn Garcia <martyn@255bits.com>
2021-03-06 10:39:49 -0700

    [refactor] train hook

--- hypergan/train_hooks/adversarial_norm_train_hook.py
+++ hypergan/train_hooks/adversarial_norm_train_hook.py
@@ -11,96 +11,35 @@ from torch.autograd import grad as torch_grad
 class AdversarialNormTrainHook(BaseTrainHook):
     def __init__(self, gan=None, config=None):
         super().__init__(config=config, gan=gan)
-        self.d_loss = None
-        self.g_loss = None
-        if self.config.gamma is not None:
-            self.gamma = self.config.gamma#torch.Tensor([self.config.gamma]).float()[0].cuda()#self.gan.configurable_param(self.config.gamma or 1.0)
-        if self.config.gammas is not None:
-            self.gammas = [
-                        self.config.gammas[0],#torch.Tensor([self.config.gammas[0]]).float()[0].cuda(),#self.gan.configurable_param(self.config.gamma or 1.0)
-                        self.config.gammas[1]#torch.Tensor([self.config.gammas[1]]).float()[0].cuda()#self.gan.configurable_param(self.config.gamma or 1.0)
-                    ]
-        self.relu = torch.nn.ReLU()
-        self.target = [Parameter(x, requires_grad=True) for x in self.gan.discriminator_real_inputs()]
-        self.x_mod_target = torch.zeros_like(self.target[0])
-        self.g_mod_target = torch.zeros_like(self.target[0])
+        self.gamma = self.config.gamma or 4.0
+        self.target_x = None
+        self.target_g = None
         self.loss = self.gan.create_component("loss")
 
     def forward(self, d_loss, g_loss):
-        if self.config.mode == "real" or self.config.mode is None:
-            for target, data in zip(self.target, self.gan.discriminator_real_inputs()):
-                target.data = data.clone()
-            d_fake = self.gan.d_fake
-            d_real = self.gan.forward_discriminator(self.target)
-            loss, _, mod_target = self.regularize_adversarial_norm(d_fake, d_real, self.target) #wrong args?
-            norm = (-((mod_target[0] - self.gan.discriminator_real_inputs()[0])**2)).mean()
-            for mt, t in zip(mod_target[1:], self.gan.discriminator_real_inputs()[1:]):
-                norm += (-((mt - t) ** 2)).mean()
-            if self.config.forward_discriminator:
-                dadv = self.gan.forward_discriminator(mod_target)
-                norm += (-((dadv - self.gan.d_real) ** 2)).mean()
-        elif self.config.mode == "fake":
-            norm = None
-            for d_fake_in in self.gan.discriminator_fake_inputs():
-                for target, data in zip(self.target, d_fake_in):
-                    target.data = data.clone()
-                d_fake = self.gan.forward_discriminator(self.target)
-                d_real = self.gan.d_real
-                loss, norm, mod_target = self.regularize_adversarial_norm(d_real, d_fake, self.target)
-                for mt, t in zip(mod_target, d_fake_in):
-                    if norm == None:
-                        norm = (-((mt - t) ** 2)).mean()
-                    else:
-                        norm += (-((mt - t) ** 2)).mean()
-                if self.config.forward_discriminator:
-                    dadv = self.gan.forward_discriminator(mod_target)
-                    norm += (-((dadv - self.gan.d_fake) ** 2)).mean()
-            norm /= len(self.gan.discriminator_fake_inputs())
-        elif self.config.mode == "autoenc":
-            d_fake_in = self.gan.discriminator_fake_inputs()[0]
-            for target, data in zip(self.target, d_fake_in):
-                target.data = data.clone()
-            d_fake = self.gan.forward_discriminator(self.target)
-            d_real = self.gan.d_real
-            loss, norm, mod_target = self.regularize_adversarial_norm(d_real, d_fake, self.target)
-            d_real = self.gan.forward_discriminator(mod_target)
-            d_fake = self.gan.forward_discriminator(d_fake_in)
-            d_l, g_l = self.loss.forward(d_real, d_fake)
-            return (self.config.gammas[0]*d_l), None#(self.config.gammas[1]*g_l)
+        if self.target_x == None:
+            self.target_x = [Parameter(x, requires_grad=True) for x in self.gan.discriminator_real_inputs()]
+            self.target_g = [Parameter(g, requires_grad=True) for g in self.gan.discriminator_fake_inputs()[0]]
+        for target, data in zip(self.target_g, self.gan.discriminator_fake_inputs()[0]):
+            target.data = data.clone()
+        for target, data in zip(self.target_x, self.gan.discriminator_real_inputs()):
+            target.data = data.clone()
+        reg_fake = self.regularize(False, self.gan.discriminator_fake_inputs()[0], self.target_g, self.gan.d_real, self.gan.forward_discriminator(self.target_g))
+        reg_real = self.regularize(True, self.gan.discriminator_real_inputs(), self.target_x, self.gan.forward_discriminator(self.target_x), self.gan.d_fake)
+        return (reg_fake + reg_real), None
 
-        elif self.config.mode == "autoenc_x":
-            d_real_in = self.gan.discriminator_real_inputs()
-            for target, data in zip(self.target, d_real_in):
-                target.data = data.clone()
-            d_fake = self.gan.d_fake
-            d_real = self.gan.forward_discriminator(self.target)
-            loss, norm, mod_target = self.regularize_adversarial_norm(d_real, d_fake, self.target)
-            d_fake = self.gan.forward_discriminator(mod_target)
-            d_real = self.gan.forward_discriminator(d_real_in)
-            d_l, g_l = self.loss.forward(d_real, d_fake)
-            return (self.config.gammas[0]*d_l), None#(self.config.gammas[1]*g_l)
-
-        if self.config.loss:
-          if "g" in self.config.loss:
-              self.g_loss = self.gamma * norm.mean()
-              self.gan.add_metric('an_g', self.g_loss)
-          if "d" in self.config.loss:
-              self.d_loss = self.gamma * norm.mean()
-              self.gan.add_metric('an_d', self.d_loss)
-          if "dg" in self.config.loss:
-              self.d_loss = self.gammas[0] * norm.mean()
-              self.gan.add_metric('an_d', self.d_loss)
-              self.g_loss = self.gammas[1] * norm.mean()
-              self.gan.add_metric('an_g', self.g_loss)
+    def regularize(self, invert, inputs, target, d_real, d_fake):
+        loss, norm, mod_target = self.regularize_adversarial_norm(d_real, d_fake, target)
+        d_real = self.gan.forward_discriminator(mod_target)
+        d_fake = self.gan.forward_discriminator(inputs)
+        if invert:
+            d_l, _g_l = self.loss.forward(d_fake, d_real)
         else:
-            self.d_loss = self.gamma * norm.mean()
-            self.gan.add_metric('an_d', self.d_loss)
-
-        return [self.d_loss, self.g_loss]
+            d_l, _g_l = self.loss.forward(d_real, d_fake)
+        return self.gamma*d_l
 
     def regularize_adversarial_norm(self, d_real, d_fake, target):
         loss = self.forward_adversarial_norm(d_real, d_fake)
-
         d1_grads = torch_grad(outputs=loss, inputs=target, retain_graph=True, create_graph=True)
         mod_target = [_d1 + _t for _d1, _t in zip(d1_grads, target)]
 

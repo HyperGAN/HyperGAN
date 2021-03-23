a2bf5316e335c1d0dab4d53b4d63be01936d2434
Mikkel Garcia <mikkel@255bits.com>
2021-03-23 14:10:50 -0600

    [fix] single-gpu mode Image.next() on cpu instead of gpu

--- hypergan/inputs/image_loader.py
+++ hypergan/inputs/image_loader.py
@@ -20,6 +20,10 @@ class ImageLoader:
         h, w = self.config.height, self.config.width
         if self.config.blank:
             return
+        if device is None:
+            device = self.config.device
+        self.multiple = None
+        self.offset = None
 
         if config.crop:
             transform_list.append(CropResizeTransform((h, w)))
@@ -72,8 +76,9 @@ class ImageLoader:
         if self.config.blank:
             return torch.zeros([self.config.batch_size, self.config.channels, self.config.height, self.config.width]).cuda()
         try:
-            self.multiple = torch.tensor(2.0, device=self.device)
-            self.offset = torch.tensor(-1.0, device=self.device)
+            if self.multiple is None:
+                self.multiple = torch.tensor(2.0, device=self.device)
+                self.offset = torch.tensor(-1.0, device=self.device)
             return self.datasets[index].next()[0].to(self.device) * self.multiple + self.offset
         except ValueError:
             return self.next(index)

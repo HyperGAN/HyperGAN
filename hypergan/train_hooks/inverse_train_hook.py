a6ab158cc4634ac713018ed15678d20ebd7dfd5a
Mikkel Garcia <mikkel@255bits.com>
2021-03-30 14:29:41 -0600

    [feature] separate gammas, redefine invert option

--- hypergan/train_hooks/inverse_train_hook.py
+++ hypergan/train_hooks/inverse_train_hook.py
@@ -16,11 +16,13 @@ class InverseTrainHook(BaseTrainHook):
         D(inverse g, g) + D(x, inverse x)
 
     invert: for
-        D(-inverse x, g) + D(x, -inverse g)
+        D(-inverse x, inverse x)
     """
     def __init__(self, gan=None, config=None):
         super().__init__(config=config, gan=gan)
-        self.gamma = self.config.gamma or 4.0
+        self.gamma = self.config.gamma or [4.0, 4.0]
+        if type(self.gamma) == float:
+            self.gamma = [self.gamma, self.gamma]
         self.target_x = None
         self.target_g = None
         self.loss = self.gan.create_component("loss")
@@ -30,24 +32,24 @@ class InverseTrainHook(BaseTrainHook):
             self.target_g = [Parameter(g, requires_grad=True) for g in self.gan.discriminator_fake_inputs()[0]]
             self.target_x = [Parameter(x, requires_grad=True) for x in self.gan.discriminator_real_inputs()]
 
-        for target, data in zip(self.target_g, self.gan.discriminator_fake_inputs()[0]):
-            target.data = data.clone()
         for target, data in zip(self.target_x, self.gan.discriminator_real_inputs()):
             target.data = data.clone()
 
         if self.config.invert:
-            neg_inverse_fake = self.inverse(self.gan.forward_discriminator(self.target_g), self.gan.d_real, self.target_g)
-            neg_inverse_real = self.inverse(self.gan.d_fake, self.gan.forward_discriminator(self.target_x), self.target_x)
+            for target, data in zip(self.target_g, self.gan.discriminator_real_inputs()):
+                target.data = data.clone()
+            neg_inverse_real = self.inverse(self.gan.d_fake, self.gan.forward_discriminator(self.target_g), self.target_g)
+            neg_inverse_fake = self.inverse(self.gan.forward_discriminator(self.target_x), self.gan.d_fake, self.target_x)
 
-            reg_fake, _ = self.loss.forward(self.gan.forward_discriminator(self.gan.discriminator_real_inputs()), self.gan.forward_discriminator(neg_inverse_fake))
-            reg_real, g_ = self.loss.forward(self.gan.forward_discriminator(neg_inverse_real), self.gan.forward_discriminator(self.gan.discriminator_fake_inputs()[0]))
+            reg_fake, g_ = self.loss.forward(self.gan.forward_discriminator(neg_inverse_real), self.gan.forward_discriminator(neg_inverse_fake))
             
             self.add_metric("iig_loss", g_)
-            self.add_metric("iid_loss_real", reg_fake)
             self.add_metric("iid_loss_fake", reg_fake)
 
-            return self.gamma*(reg_fake+reg_real), (self.config.g_gamma or 0.1) * g_
+            return self.gamma[0]*reg_fake, (self.config.g_gamma or 0.1) * g_
 
+        for target, data in zip(self.target_g, self.gan.discriminator_fake_inputs()[0]):
+            target.data = data.clone()
 
         inverse_fake = self.inverse(self.gan.d_real, self.gan.forward_discriminator(self.target_g), self.target_g)
         inverse_real = self.inverse(self.gan.forward_discriminator(self.target_x), self.gan.d_fake, self.target_x)
@@ -58,7 +60,7 @@ class InverseTrainHook(BaseTrainHook):
         self.add_metric("ig_loss", g_)
         self.add_metric("id_loss_real", reg_fake)
         self.add_metric("id_loss_fake", reg_fake)
-        return self.gamma*(reg_fake+reg_real), (self.config.g_gamma or 0.1) * g_
+        return self.gamma[0]*reg_fake+self.gamma[1]*reg_real, (self.config.g_gamma or 0.1) * g_
 
     def inverse(self, d_real, d_fake, target):
         loss = self.loss.forward(d_fake, d_real)[0]

d9e9cb1fffa3aa31b5cd846644c377e5cf2505c8
Martyn Garcia <martyn@255bits.com>
2021-04-21 11:47:14 -0600

    [fix] try torch.log form of standard loss

--- hypergan/losses/standard_loss.py
+++ hypergan/losses/standard_loss.py
@@ -7,14 +7,17 @@ import torch.nn.functional as F
 class StandardLoss(BaseLoss):
     def __init__(self, gan, config):
         super(StandardLoss, self).__init__(gan, config)
-        self.relu = torch.nn.ReLU()
+        #self.relu = torch.nn.ReLU()
+        self.sigmoid = torch.nn.Sigmoid()
         self.two = torch.Tensor([2.0]).cuda()
         self.eps = torch.Tensor([1e-12]).cuda()
 
     def _forward(self, d_real, d_fake):
         criterion = torch.nn.BCEWithLogitsLoss()
-        g_loss = criterion(d_fake, torch.ones_like(d_fake))
-        d_loss = criterion(d_real, torch.ones_like(d_real)) + criterion(d_fake, torch.zeros_like(d_fake))
+        #g_loss = criterion(d_fake, torch.ones_like(d_fake))
+        #d_loss = criterion(d_real, torch.ones_like(d_real)) + criterion(d_fake, torch.zeros_like(d_fake))
+        g_loss = torch.log(self.sigmoid(d_real) + 1e-13)
+        d_loss = torch.log(self.sigmoid(d_real) + 1e-13) + torch.log(1-self.sigmoid(d_fake) + 1e-13)
 
         return [d_loss, g_loss]
 

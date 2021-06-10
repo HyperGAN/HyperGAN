1425b141a15f489775bee75d3ed7795bd8bfa724
Mikkel Garcia <mikkel@255bits.com>
2021-06-10 17:52:41 -0600

    [fix] standard loss

--- hypergan/losses/standard_loss.py
+++ hypergan/losses/standard_loss.py
@@ -16,8 +16,9 @@ class StandardLoss(BaseLoss):
         criterion = torch.nn.BCEWithLogitsLoss()
         #g_loss = criterion(d_fake, torch.ones_like(d_fake))
         #d_loss = criterion(d_real, torch.ones_like(d_real)) + criterion(d_fake, torch.zeros_like(d_fake))
-        g_loss = torch.log(self.sigmoid(d_real) + 1e-13)
-        d_loss = torch.log(self.sigmoid(d_real) + 1e-13) + torch.log(1-self.sigmoid(d_fake) + 1e-13)
+        g_loss = torch.log(1-self.sigmoid(d_fake) + 1e-13)
+        d_loss = torch.log(self.sigmoid(d_real) + 1e-13) +torch.log(1-self.sigmoid(d_fake) + 1e-13)
+        d_loss = -d_loss
 
         return [d_loss, g_loss]
 

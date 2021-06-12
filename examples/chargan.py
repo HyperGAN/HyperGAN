d64c92ed37ecdf1449cdff3a89ba40d03c21fe03
Martyn Garcia <martyn@255bits.com>
2021-06-11 21:10:33 -0600

    [wip] chargan

--- examples/chargan.py
+++ examples/chargan.py
@@ -1,4 +1,5 @@
-from examples.common import *
+from common import *
+from hypergan.viewer import GlobalViewer
 from hyperchamber import Config
 from hypergan.discriminators import *
 from hypergan.distributions import *
@@ -26,13 +27,13 @@ import torch.utils.data as data
 import uuid
 
 class TextData(data.Dataset):
-    def __init__(self, path, length, device):
+    def __init__(self, path, length, device, mode):
         self.size = os.path.getsize(path)
         self.file = None
         self.path = path
-
         self.device = device
         self.length = length
+        self.mode = mode
 
     def encode_line(self, line):
         return torch.tensor([(c/256.0 * 2 - 1) for c in line])
@@ -42,7 +43,10 @@ class TextData(data.Dataset):
             self.file = open(self.path, 'rb')
         self.file.seek(index)
         data = self.file.read(self.length)
-        encoded = self.encode_line(data).view(1, -1)
+        if self.mode == "constant":
+            encoded = torch.ones_like(self.encode_line(data).view(1, -1)) * 0.5
+        else:
+            encoded = self.encode_line(data).view(1, -1)
         return [encoded]
 
     def __len__(self):
@@ -64,8 +68,8 @@ class TextData(data.Dataset):
 
 
 class TextInput:
-    def __init__(self, config, batch_size, filename, length, one_hot=False, device=0):
-        self.textdata = TextData(filename, length, device=device)
+    def __init__(self, config, batch_size, filename, length, mode='seek', one_hot=False, device=0):
+        self.textdata = TextData(filename, length, device=device, mode=mode)
         self.dataloader = data.DataLoader(self.textdata, batch_size=batch_size, shuffle=True, num_workers=1, drop_last=True)
         self.dataset = None
         self._batch_size = batch_size
@@ -180,23 +184,29 @@ if __name__ == '__main__':
     arg_parser.parser.add_argument('--length', type=int, default=256, help='Length of string per line');
     args = arg_parser.parse_args()
 
+    def lookup_config(args):
+        if args.action != 'search':
+            return hg.configuration.Configuration.load(args.config+".json")
+        else:
+            return hc.Config({"length": 1024})
 
     config = lookup_config(args)
 
     config_name = args.config
     save_file = "saves/"+config_name+"/model.ckpt"
 
-    inputs = TextInput(config, args.batch_size, args.filename, config.length, one_hot=config.length)
+    mode = "seek"
+
+    if args.action == 'search':
+        mode = 'constant'
+
+    inputs = TextInput(config, args.batch_size, args.filename, config.length, mode=mode, one_hot=config.length)
 
     def parse_size(size):
         width = int(size.split("x")[0])
         height = int(size.split("x")[1])
         channels = int(size.split("x")[2])
         return [width, height, channels]
-
-    def lookup_config(args):
-        if args.action != 'search':
-            return hg.configuration.Configuration.load(args.config+".json")
 
     def random_config_from_list(config_list_file):
         """ Chooses a random configuration from a list of configs (separated by newline) """
@@ -219,7 +229,8 @@ if __name__ == '__main__':
         gan = setup_gan(config, inputs, args)
 
     def search(config, inputs, args):
-        gan = setup_gan(config, inputs, args)
+        metrics = train(config, inputs, args)
+        return metrics
 
     def train(config, inputs, args):
         gan = setup_gan(config, inputs, args)
@@ -236,6 +247,7 @@ if __name__ == '__main__':
         dlog = 0
         last_i = 0
         steps = 0
+        metric = 100
 
         while(True):
             steps +=1
@@ -247,7 +259,7 @@ if __name__ == '__main__':
                 print("saving " + save_file)
                 trainable_gan.save()
 
-            if steps % args.sample_every == 0:
+            if steps % args.sample_every == 0 or steps == args.steps -1:
                 x_val = gan.inputs.next()
                 q, a = torch.split(x_val, x_val[0].shape[-1]//2, dim=-1)
                 g = gan.generator.forward(gan.latent.sample(), context={"q": q})
@@ -257,12 +269,22 @@ if __name__ == '__main__':
                 print(inputs.textdata.sample_output(a[0].cpu().detach().numpy()))
                 print("G answer:")
                 g_output = inputs.textdata.sample_output(g[0].cpu().detach().numpy())
-                g_output = re.sub(r'[\x00-\x1f\x7f-\x9f]', '�', g_output)
+                g_output = re.sub(r'[\x00-\x09\x7f-\x9f]', '�', g_output)
                 print(g_output)
+                print("Q mean:")
+                print(q.mean())
+                print("A mean:")
+                print(a.mean())
+                print("G mean:")
+                print(g.mean())
+                metric = (a-g).mean().cpu().detach().numpy()
+                print("METRIC")
+                print(metric)
+        return [metric]
 
-        if args.config is None:
-            with open("sequence-results-10k.csv", "a") as myfile:
-                myfile.write(config_name+","+str(ax_sum)+","+str(ag_sum)+","+ str(ax_sum+ag_sum)+","+str(ax_sum*ag_sum)+","+str(dlog)+","+str(diversity)+","+str(ax_sum*ag_sum*(1/diversity))+","+str(last_i)+"\n")
+        #if args.config is None:
+        #    with open("sequence-results-10k.csv", "a") as myfile:
+        #        myfile.write(config_name+","+str(ax_sum)+","+str(ag_sum)+","+ str(ax_sum+ag_sum)+","+str(ax_sum*ag_sum)+","+str(dlog)+","+str(diversity)+","+str(ax_sum*ag_sum*(1/diversity))+","+str(last_i)+"\n")
 
     if args.action == 'train':
         metrics = train(config, inputs, args)
@@ -270,7 +292,56 @@ if __name__ == '__main__':
     elif args.action == 'sample':
         sample(config, inputs, args)
     elif args.action == 'search':
-        search(config, inputs, args)
+        config_filename = "chargan-search-"+str(uuid.uuid4())+'.json'
+        config = hc.Config(json.loads(open(os.getcwd()+'/chargan-search.json', 'r').read()))
+        config.trainer["hooks"].append(
+          {
+            "class": "function:hypergan.train_hooks.inverse_train_hook.InverseTrainHook",
+            "gamma": [random.choice([1, 10, 1e-1, 100]), random.choice([1, 10, 1e-1, 100])],
+            "g_gamma": random.choice([1, 10, 1e-1, 100]),
+            "only_real": random.choice([False, False, True]),
+            "only_fake": random.choice([False, False, True]),
+            "invert": random.choice([False, False, True])
+          })
+        config.trainer["optimizer"] = random.choice([{
+            "class": "class:hypergan.optimizers.adamirror.Adamirror",
+            "lr": random.choice(list(np.linspace(0.0001, 0.002, num=1000))),
+            "betas":[random.choice([0.1, 0.9, 0.9074537537537538, 0.99, 0.999]),random.choice([0,0.9,0.997])]
+        },{
+            "class": "class:torch.optim.SGD",
+            "lr": random.choice([1e-1, 1, 1e-2, 4e-2])
+        },{
+            "class": "class:torch.optim.RMSprop",
+            "lr": random.choice([1e-3, 1e-4, 5e-4, 3e-3]),
+            "alpha": random.choice([0.9, 0.99, 0.999]),
+            "eps": random.choice([1e-8, 1e-13]),
+            "weight_decay": random.choice([0, 1e-2]),
+            "momentum": random.choice([0, 0.1, 0.9]),
+            "centered": random.choice([False, True])
+        },
+        {
+
+            "class": "class:torch.optim.Adam",
+            "lr": 1e-3,
+            "betas":[random.choice([0.1, 0.9, 0.9074537537537538, 0.99, 0.999]),random.choice([0,0.9,0.997])],
+            "eps": random.choice([1e-8, 1e-13]),
+            "weight_decay": random.choice([0, 1e-2]),
+            "amsgrad": random.choice([False, True])
+            }
+
+        ])
+
+     
+
+
+        metric_sum = search(config, inputs, args)
+        search_output = "chargan-search-results.csv"
+        hc.Selector().save(config_filename, config)
+        with open(search_output, "a") as myfile:
+            total = sum(metric_sum)
+            myfile.write(config_filename+","+",".join([str(x) for x in metric_sum])+","+str(total)+"\n")
+
     else:
         print("Unknown action: "+args.action)
 
+GlobalViewer.close()

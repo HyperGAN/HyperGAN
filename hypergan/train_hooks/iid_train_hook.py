e2d24da8ffc2aa4f9f3ed79631e72a801c93d807
Mikkel Garcia <mikkel@255bits.com>
2021-07-08 18:55:05 -0600

    [feature] iid train hooks options

--- hypergan/train_hooks/iid_train_hook.py
+++ hypergan/train_hooks/iid_train_hook.py
@@ -1,4 +1,5 @@
 import torch
+from hypergan.losses.stable_gan_loss import StableGANLoss
 import hyperchamber as hc
 import numpy as np
 import inspect
@@ -19,31 +20,72 @@ class IIDTrainHook(BaseTrainHook):
         super().__init__(config=config, gan=gan)
         self.g_to_z = gan.create_component("G-1", defn=self.config.g_to_z, input=torch.zeros_like(gan.inputs.next()))
         self.gan.decoded = torch.zeros_like(gan.inputs.next())
+        self.gan.g_to_z = self.g_to_z
         self.gan.source = torch.zeros_like(gan.inputs.next())
         self.source_x = None
+        if self.config.z_discriminator:
+            self.z_discriminator = gan.create_component("z_discriminator", defn=self.config.z_discriminator, input=torch.zeros_like(gan.latent.next()))
+            self.z_stable_loss = StableGANLoss(self.z_discriminator)
+
 
     def forward(self, d_loss, g_loss):
         z = self.gan.latent.z
+        if self.config.align:
+            x = self.gan.inputs.next(1)
+        else:
+            x = self.gan.x
+
         if self.source_x is None:
-            self.source_x = self.gan.x
-        zprime = self.g_to_z(self.gan.g)
-        reconstruct_x = self.gan.generator(self.g_to_z(self.gan.x)).clone().detach()
+            self.source_x = x
+        types = ["z-g'(g(z))", "x - g(g'(x))", "mean", "std"]
+        if self.config.types:
+            types = self.config.types
+        zprime = self.g_to_z(self.gan.g.clone().detach())
+        reconstruct_x = self.gan.generator(self.g_to_z(x)).clone().detach()
+
         #m = (zprime + z) / 2
         #jsd = zprime*torch.log(zprime/(m+1e-8)) + z * torch.log(z/(m+1e-8))
-        loss = ((z - zprime)**2).mean()
-        #z_reconstruct_diff = ((self.g_to_z(self.gan.x) - self.g_to_z(reconstruct_x))**2).mean()*1000
-        #self.gan.add_metric("IID1z", z_reconstruct_diff)
-        #loss += z_reconstruct_diff
-        x_reconstruct_diff = ((self.gan.x - reconstruct_x)**2).mean()
-        self.gan.add_metric("IID1x", x_reconstruct_diff)
-        loss += x_reconstruct_diff
-        loss2 = zprime.mean().abs() + (torch.std(zprime) - 1.0) ** 2
-        self.gan.add_metric("IID1", loss)
-        self.gan.add_metric("IID2", loss2)
+        loss = torch.zeros_like(z).mean()
+        d_l = None
+        for t in types:
+            if t == "z-g'(g(z))":
+                loss += ((z - zprime)**2).mean()
+                self.gan.add_metric("IID1d", loss)
+            elif t == 'zdisc':
+                z_loss = self.z_stable_loss.stable_loss(torch.cat([z,zprime], axis=1), torch.cat([zprime,zprime], axis=1))
+                d_l = z_loss[1]
+                #g_l = z_loss[0]
+                self.gan.add_metric('zd', d_l)
+                #self.gan.add_metric('zg', g_l)
+                loss += d_l
+                self.gan.add_metric('zw', self.z_discriminator.nn_layers[1][1].weight.sum())
+            elif t == "g'(x)-g'(g(g'(x)))":
+                z_reconstruct_diff = ((self.g_to_z(x) - self.g_to_z(self.gan.generator(self.g_to_z(x).clone().detach())))**2).mean() * 0.5
+                self.gan.add_metric("IID1z", z_reconstruct_diff)
+                loss += z_reconstruct_diff
+            elif t == "x - g(g'(x))":
+                x_reconstruct_diff = ((x - reconstruct_x)**2).mean()
+                self.gan.add_metric("IID1x", x_reconstruct_diff)
+                loss += x_reconstruct_diff
+            elif t == 'mean':
+                mean = zprime.mean().abs()
+                self.gan.add_metric("IIDmean", mean)
+                loss += mean
+            elif t == 'std':
+                std = (torch.std(zprime) - 1.0) ** 2
+                self.gan.add_metric("IIDstd", std)
+                loss += std
+            else:
+                raise ValidationException("Unknown type "+t)
         reconstruct_source_x = self.gan.generator(self.g_to_z(self.source_x)).clone().detach()
         self.gan.decoded = reconstruct_source_x
         self.gan.source = self.source_x
 
-        return None, loss+loss2
+        return d_l, loss
     def generator_components(self):
         return [self.g_to_z]
+
+    def discriminator_components(self):
+        if self.config.z_discriminator:
+            return [self.z_discriminator]
+        return []

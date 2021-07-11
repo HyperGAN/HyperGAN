c08439965f87d0f336fab6a42e248a0941bf2e59
Mikkel Garcia <mikkel@255bits.com>
2021-07-11 01:55:09 -0600

    [feature] static batch sampling additions

--- hypergan/samplers/static_batch_sampler.py
+++ hypergan/samplers/static_batch_sampler.py
@@ -12,11 +12,26 @@ class StaticBatchSampler(BaseSampler):
         return False
 
     def _sample(self):
+        if(self.x is None):
+            self.x = self.gan.inputs.next()
         self.gan.latent.z = self.latent
+        g = self.gan.generator.forward(self.latent)
         samples = [
-            ('generator', self.gan.generator.forward(self.latent)),
+            ('generator', g),
         ]
-        if hasattr(self.gan, 'decoded'):
-            samples.append(('decoded', self.gan.decoded))
+        if hasattr(self.gan, 'g_to_z'):
+            samples.append(('decoded', self.gan.generator(self.gan.g_to_z(g))))
+            reconstruct_source_x = self.gan.generator(self.gan.g_to_z(self.gan.source)).clone().detach()
+            samples.append(('source', self.gan.source))
+            samples.append(('decodedx', reconstruct_source_x))
+        if hasattr(self.gan, 'mask_layers'):
+            for mask_layer in self.gan.mask_layers:
+                samples.append(('mask_layer', mask_layer))
+        if hasattr(self.gan, 'encoder'):
+            encoding = self.gan.encoder(self.x)
+            encoded_x = self.gan.decoder.forward(encoding)
+            samples.append(('x', self.x))
+            samples.append(('ex', encoded_x))
+
 
         return samples

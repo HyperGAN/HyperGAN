cba0ec20488d70562b9adff4ed70c14ec1bb0a86
Mikkel Garcia <mikkel@255bits.com>
2021-08-03 21:07:59 -0600

    [fix] stable gan loss

--- hypergan/losses/stable_gan_loss.py
+++ hypergan/losses/stable_gan_loss.py
@@ -13,131 +13,75 @@ class StableGANLoss:
     ```
     """
     def __init__(self, device='cuda:0'):
-        self.gamma1 = torch.tensor(10.0, device=device)
-        self.g_gamma1 = torch.tensor(10.0, device=device)
-        self.gamma2 = torch.tensor(10.0, device=device)
-        self.g_gamma2 = torch.tensor(0.1, device=device)
-        self.inverse_gamma = torch.tensor(1e3, device=device)
-        self.target1_x = None
-        self.target1_g = None
-        self.target2_x = None
-        self.target2_g = None
+        self.one = torch.tensor(1.0, device=device)
 
     def loss_fn(self, d_real, d_fake):
-        #cr = torch.mean(d_real,0)
-        #cf = torch.mean(d_fake,0)
         criterion = torch.nn.BCEWithLogitsLoss()
-        #g_loss = criterion(d_fake-cr, torch.ones_like(d_fake))
-        #d_loss = criterion(d_real-cf, torch.ones_like(d_real)) + criterion(d_fake-cr, torch.zeros_like(d_fake))
         g_loss = criterion(d_fake, torch.ones_like(d_fake))
         d_loss = criterion(d_real, torch.ones_like(d_real)) + criterion(d_fake, torch.zeros_like(d_fake))
-        #g_loss = torch.log(1-torch.sigmoid(d_fake) + 1e-13)
-        #d_loss = torch.log(torch.sigmoid(d_real) + 1e-13) +torch.log(1-torch.sigmoid(d_fake) + 1e-13)
         return d_loss, g_loss
 
-    def stable_loss(self, discriminator, xs, gs, d_fake=None, d_real=None, DATGAN=True):
+    def stable_loss(self, discriminator, xs, gs, d_fake=None, d_real=None):
         if d_fake is None:
             d_fake = discriminator(*gs)
         if d_real is None:
             d_real = discriminator(*xs)
+        d_fake_prime = d_fake
+        d_real_prime = d_real
         d_losses = []
         g_losses = []
-        d_loss, g_loss = self.loss_fn(d_real, d_fake)
-        d_losses.append(d_loss)
-        g_losses.append(g_loss)
-        if DATGAN:
-            # From https://github.com/iceli1007/DAT-GAN
+        fake_grad_sum = 10000
+        real_grad_sum = 10000
+        real_adv_loss = 1e8
+        fake_adv_loss = 1e8
+        # https://github.com/iceli1007/DAT-GAN (modified)
+        i = 0
+        while(real_adv_loss > self.one or fake_adv_loss > self.one):
             t=1
             real_value = d_real.mean()
             fake_value = d_fake.mean()
-            fake_imgs_adv=gs[0].clone()
-            real_imgs_adv=xs[0].clone()
-            real_imgs_adv=Variable(real_imgs_adv,requires_grad=True)
-            fake_imgs_adv=Variable(fake_imgs_adv,requires_grad=True)
+            fake_imgs_adv=[_g.clone() for _g in gs]
+            real_imgs_adv=[_x.clone() for _x in xs]
+            real_imgs_adv=[Variable(a, requires_grad=True) for a in real_imgs_adv]
+            fake_imgs_adv=[Variable(a,requires_grad=True) for a in fake_imgs_adv]
 
-            fake_output= discriminator(fake_imgs_adv)
+            fake_output= discriminator(*fake_imgs_adv)
             fake_output=fake_output.mean()
             fake_adv_loss = torch.abs(fake_output-real_value)
             fake_grad=torch.autograd.grad(fake_adv_loss,fake_imgs_adv)
-            fake_imgs_adv=fake_imgs_adv-fake_grad[0].clamp(-1*t,t)
-            fake_imgs_adv=fake_imgs_adv.clamp(-1,1)
-            real_output= discriminator(real_imgs_adv)
+            fake_imgs_adv=[(a - _g) for a, _g in zip(fake_imgs_adv,fake_grad)]
+            fake_grad_sum = sum([f.abs().sum() for f in fake_grad])
+            #print('i', i, 'fake',fake_grad_sum, 'loss', fake_adv_loss)
+            real_output= discriminator(*real_imgs_adv)
             real_output=real_output.mean()
             real_adv_loss = torch.abs(real_output-fake_value)
             real_grad=torch.autograd.grad(real_adv_loss,real_imgs_adv)
-            real_imgs_adv=real_imgs_adv-real_grad[0].clamp(-1*t,t)
-            fake_adv_validity= discriminator(fake_imgs_adv.detach())
-            real_adv_validity = discriminator(real_imgs_adv)
-            real_imgs_adv=real_imgs_adv.clamp(-1,1) 
+            real_imgs_adv=[(a - _g) for a, _g in zip(real_imgs_adv,real_grad)]
+            real_grad_sum = sum([f.abs().sum() for f in real_grad])
+            #print('i', i, 'real',real_grad_sum, 'loss', real_adv_loss)
 
-            #return self.loss_fn(real_adv_validity, fake_adv_validity)
+            # if autoencode:
+                #fake_adv_validity= discriminator(gs[0].detach(), fake_imgs_adv[1].detach())
+                #real_adv_validity= discriminator(real_imgs_adv[0].detach(), fake_imgs_adv[0].detach())
+            #else:
+            fake_adv_validity= discriminator(*[a.detach() for a in fake_imgs_adv])
+
+            real_adv_validity = discriminator(*real_imgs_adv)
+
             d_loss, g_loss = self.loss_fn(real_adv_validity, fake_adv_validity)
-            g_losses.append(g_loss)
-            d_losses.append(d_loss)
-        else:
-            if self.target1_x == None:
-                self.target1_g = [Parameter(g, requires_grad=True) for g in gs]
-                self.target1_x = [Parameter(x, requires_grad=True) for x in xs]
-                self.target2_g = [Parameter(g, requires_grad=True) for g in gs]
-                self.target2_x = [Parameter(x, requires_grad=True) for x in xs]
-            for target, g in zip(self.target1_g + self.target2_g, gs + gs):
-                target.data = g.clone()
-            for target, x in zip(self.target1_x + self.target2_x, xs + xs):
-                target.data = x.clone()
+            gs = [x.detach() for x in fake_imgs_adv]
+            xs = [x.detach() for x in real_imgs_adv]
 
-            neg_inverse_fake = self.inverse(discriminator(*self.target2_g), d_real, self.target2_g)
-            neg_inverse_real = self.inverse(d_fake, discriminator(*self.target2_x), self.target2_x)
-
-            reg_fake, _ = self.loss_fn(d_real, discriminator(*neg_inverse_fake))
-            reg_real, g_ = self.loss_fn(discriminator(*neg_inverse_real), d_fake)
-
-            d_losses.append(self.gamma2*(reg_fake+reg_real))
-            g_losses.append(self.g_gamma2 * g_)
-
-            inverse_fake = self.inverse(d_real, discriminator(*self.target1_g), self.target1_g)
-            inverse_real = self.inverse(discriminator(*self.target1_x), d_fake, self.target1_x)
-
-            reg_fake, g_ = self.loss_fn(discriminator(*inverse_fake), d_fake)
-            reg_real = self.loss_fn(d_real, discriminator(*inverse_real))[0]
-
-            d_losses.append(self.gamma1*(reg_fake+reg_real))
-            g_losses.append(self.g_gamma1 * g_)
-
-
-
-
-        #else:
-        #    if self.target1_x == None:
-        #        self.target1_x = [Parameter(x, requires_grad=True) for x in xs]
-        #        self.target2_x = [Parameter(x, requires_grad=True) for x in xs]
-        #    for target, x in zip( self.target2_x, xs + xs):
-        #        target.data = x.clone()
-
-        #    #neg_inverse_fake = self.inverse(discriminator(*self.target2_g), d_real, self.target2_g)
-        #    neg_inverse_real = self.inverse(d_fake, discriminator(*self.target2_x), self.target2_x)
-
-        #    #reg_fake, _ = self.loss_fn(d_real, discriminator(*neg_inverse_fake))
-        #    neg_reg_real, g_ = self.loss_fn(discriminator(*neg_inverse_real), d_fake)
-
-        #    #d_losses.append(self.gamma2*(reg_fake+reg_real))
-        #    #g_losses.append(self.g_gamma2 * g_)
-
-        #    #inverse_fake = self.inverse(d_real, discriminator(*self.target1_g), self.target1_g)
-        #    inverse_real = self.inverse(discriminator(*self.target1_x), d_fake, self.target1_x)
-
-        #    #reg_fake, g_ = self.loss_fn(discriminator(*inverse_fake), d_fake)
-        #    reg_real = self.loss_fn(d_real, discriminator(*inverse_real))[0]
-
-        #    d_losses.append(self.gamma1*(neg_reg_real+reg_real))
-        #    g_losses.append(self.g_gamma1 * g_)
+            d_fake = discriminator(*gs)
+            d_real = discriminator(*xs)
+            i += 1
+            if i > 100:
+                print("Stable gan loss training step", i)
+        d_losses.append(d_loss)
+        d_loss, g_loss = self.loss_fn(d_real_prime, d_real)
+        d_losses.append(d_loss)
+        d_loss, g_loss = self.loss_fn(d_fake, d_fake_prime)
+        g_losses.append(-d_loss)
+        d_losses.append(d_loss)
 
         return sum(d_losses), sum(g_losses)
-
-    def inverse(self, d_real, d_fake, target):
-        #loss = (d_fake - d_real) * self.inverse_gamma
-        #loss = self.ragan(d_fake, d_real)[0]
-        loss = self.loss_fn(d_fake, d_real)[0]
-        #loss = loss.mean()
-        d1_grads = torch_grad(outputs=loss, inputs=target, retain_graph=True, create_graph=True, only_inputs=True)
-        #return [_t + _d1/_d1.norm() for _d1, _t in zip(d1_grads, target)]
-        return [_t + _d1 for _d1, _t in zip(d1_grads, target)]

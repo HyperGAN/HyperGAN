89148bae91b052f2e3a6e33f4272c2f313e5afe1
Mikkel Garcia <mikkel@255bits.com>
2021-08-06 01:48:41 -0600

    [feature] adversarial search uses optimizer

--- hypergan/losses/stable_gan_loss.py
+++ hypergan/losses/stable_gan_loss.py
@@ -3,6 +3,8 @@ import torch
 from torch.nn.parameter import Parameter
 from torch.autograd import Variable
 from torch.autograd import grad as torch_grad
+from torch import optim
+import collections
 
 class StableGANLoss:
     """
@@ -13,7 +15,8 @@ class StableGANLoss:
     ```
     """
     def __init__(self, device='cuda:0'):
-        self.one = torch.tensor(1.0, device=device)
+        self.one = torch.tensor(0.01, device=device)
+        self.optimizer = None
 
     def loss_fn(self, d_real, d_fake):
         criterion = torch.nn.BCEWithLogitsLoss()
@@ -38,51 +41,64 @@ class StableGANLoss:
         fake_adv_loss = 1e8
         # https://github.com/iceli1007/DAT-GAN (modified)
         i = 0
-        while(real_adv_loss > self.one or fake_adv_loss > self.one):
-            t=1
+        if self.optimizer is None:
+            self.fake_imgs_adv= [Variable(g.clone().detach(), requires_grad=True) for g in gs]
+            self.real_imgs_adv= [Variable(x.clone().detach(), requires_grad=True) for x in xs]
+            #self.optimizer = optim.SGD(self.fake_imgs_adv + self.real_imgs_adv, lr=1.0)
+            #self.optimizer = optim.Adam(self.fake_imgs_adv + self.real_imgs_adv, lr=0.001)
+            self.optimizer = torch.optim.RMSprop(self.fake_imgs_adv + self.real_imgs_adv, lr=0.01)
+        else:
+            for target, data in zip(self.fake_imgs_adv, gs):
+                target.data = data.clone()
+            for target, data in zip(self.real_imgs_adv, xs):
+                target.data = data.clone()
+        fake_imgs_adv = self.fake_imgs_adv
+        real_imgs_adv = self.real_imgs_adv
+        self.state = collections.defaultdict(dict)
+        while(True):
             real_value = d_real.mean()
             fake_value = d_fake.mean()
-            fake_imgs_adv=[_g.clone() for _g in gs]
-            real_imgs_adv=[_x.clone() for _x in xs]
-            real_imgs_adv=[Variable(a, requires_grad=True) for a in real_imgs_adv]
-            fake_imgs_adv=[Variable(a,requires_grad=True) for a in fake_imgs_adv]
 
             fake_output= discriminator(*fake_imgs_adv)
             fake_output=fake_output.mean()
             fake_adv_loss = torch.abs(fake_output-real_value)
             fake_grad=torch.autograd.grad(fake_adv_loss,fake_imgs_adv)
-            fake_imgs_adv=[(a - _g) for a, _g in zip(fake_imgs_adv,fake_grad)]
-            fake_grad_sum = sum([f.abs().sum() for f in fake_grad])
+            for grad, fake in zip(fake_grad, fake_imgs_adv):
+                fake.grad = grad
+            #fake_grad_sum = sum([f.abs().sum() for f in fake_grad])
             #print('i', i, 'fake',fake_grad_sum, 'loss', fake_adv_loss)
             real_output= discriminator(*real_imgs_adv)
             real_output=real_output.mean()
             real_adv_loss = torch.abs(real_output-fake_value)
             real_grad=torch.autograd.grad(real_adv_loss,real_imgs_adv)
-            real_imgs_adv=[(a - _g) for a, _g in zip(real_imgs_adv,real_grad)]
+            for grad, real in zip(real_grad, real_imgs_adv):
+                real.grad = grad
+
             real_grad_sum = sum([f.abs().sum() for f in real_grad])
             #print('i', i, 'real',real_grad_sum, 'loss', real_adv_loss)
+            self.optimizer.step()
 
-            # if autoencode:
-                #fake_adv_validity= discriminator(gs[0].detach(), fake_imgs_adv[1].detach())
-                #real_adv_validity= discriminator(real_imgs_adv[0].detach(), fake_imgs_adv[0].detach())
-            #else:
-            fake_adv_validity= discriminator(*[a.detach() for a in fake_imgs_adv])
-
-            real_adv_validity = discriminator(*real_imgs_adv)
+            fake_adv_validity= discriminator(*[a.clone().detach() for a in fake_imgs_adv])
+            real_adv_validity = discriminator(*[a.clone().detach() for a in real_imgs_adv])
+            next_adv_loss = torch.abs(real_adv_validity.mean() - fake_adv_validity.mean())
+            if(next_adv_loss > fake_adv_loss):# redundant or fake_adv_loss > self.one):
+                break
 
             d_loss, g_loss = self.loss_fn(real_adv_validity, fake_adv_validity)
-            gs = [x.detach() for x in fake_imgs_adv]
-            xs = [x.detach() for x in real_imgs_adv]
 
-            d_fake = discriminator(*gs)
-            d_real = discriminator(*xs)
+            d_fake = fake_adv_validity
+            d_real = real_adv_validity
+            gs = fake_imgs_adv
+            xs = real_imgs_adv
             i += 1
-            if i > 100:
-                print("Stable gan loss training step", i)
+            if i%10 == 0 and i > 100:
+                print("Stable gan loss training step", i, "fake loss", float(fake_adv_loss.cpu()), "real loss", float(real_adv_loss.cpu()))
+        print('si', i, fake_adv_loss)
         d_losses.append(d_loss)
         d_loss, g_loss = self.loss_fn(d_real_prime, d_real)
         d_losses.append(d_loss)
         d_loss, g_loss = self.loss_fn(d_fake, d_fake_prime)
+        #g_losses.append(-d_loss)
         d_losses.append(d_loss)
 
         return sum(d_losses), sum(g_losses)

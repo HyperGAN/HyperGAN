[fix] iid backprop

hypergan/train_hooks/iid_train_hook.py
@@ -41,7 +41,8 @@ class IIDTrainHook(BaseTrainHook):
         types = ["z-g'(g(z))", "x - g(g'(x))", "mean", "std"]
         if self.config.types:
             types = self.config.types
-        zprime = self.g_to_z(self.gan.g.clone().detach())
+        #zprime = self.g_to_z(self.gan.g.clone().detach())
+        zprime = self.g_to_z(self.gan.g)
         zxprime = self.g_to_z(x)
         reconstruct_x = self.gan.generator(self.g_to_z(x)).clone().detach()
 
@@ -54,7 +55,7 @@ class IIDTrainHook(BaseTrainHook):
                 loss += ((z - zprime)**2).mean()
                 self.gan.add_metric("IID1d", loss)
             elif t == 'zdisc3':
-                z_loss = self.z_stable_loss.stable_loss(self.z_discriminator, [torch.cat([z,z,z-z], axis=1)], [torch.cat([z,zprime,zprime-z], axis=1)])
+                z_loss = self.z_stable_loss.stable_loss(self.z_discriminator, [torch.cat([z,z], axis=1)], [torch.cat([z,zprime], axis=1)])
                 d_l = z_loss[0]
                 g_l = z_loss[1]
                 self.gan.add_metric('zd', d_l)
1be4dcd704c56caa0c51b84e5b1f83189d115f36
Martyn Garcia <martyn@255bits.com>
2022-03-23 20:07:08 -0600

    [feature] neural turing machine layer

---
+++ hypergan/layers/ntm.py
@@ -0,0 +1,361 @@
+import torch.nn as nn
+import hypergan as hg
+import numpy as np
+import torch
+from torch.nn import functional as F
+from hypergan.layer_shape import LayerShape
+
+from hypergan.modules.modulated_conv2d import EqualLinear
+
+class NTMLayer(hg.Layer):
+
+    """
+        ---
+        description: 'layer ntm'
+        ---
+
+        # neural turing machine layer
+
+
+        ## Optional arguments
+
+        * layers - number of inner layers
+        * heads - number of read/write heads
+        * hidden_size - size of inner layer
+        * memory_n - size of 2d memory
+        * memory_m - size of 2d memory
+
+        ## input size
+
+        2d tensor
+
+        ## output size
+
+        2d tensor
+
+        ## syntax
+
+        ```json
+          "ntm 512"
+        ```
+
+        ## examples
+
+        ```json
+        ```
+    """
+    def __init__(self, component, args, options):
+        super(NTMLayer, self).__init__(component, args, options)
+        M = options.memory_m or 10
+        N = options.memory_n or 10
+        dim = component.current_size.dims[0]
+        layers = options.layers or None
+        num_heads = options.heads or 2
+        inner_dim = options.hidden_size or 512
+        heads = []
+
+        self.output_dim = int(args[0])
+        self.size = LayerShape(self.output_dim)
+        memory = FastMemory(N, M).cuda()
+        for i in range(num_heads):
+            heads += [
+                NTMReadHead(memory, inner_dim).cuda(),
+                NTMWriteHead(memory, inner_dim).cuda()
+            ]
+
+        self.ntm = NTM(dim, self.output_dim, memory, heads).cuda()
+        self.batch_size = component.gan.batch_size()
+        self.memory = memory
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def init_sequence(self):
+        self.memory.reset(self.batch_size)
+
+    def forward(self, input, context):
+
+        self.init_sequence()
+        net = self.ntm(input)
+        return net
+
+    def output_size(self):
+        return self.size
+
+class FastMemory(nn.Module):
+    """Memory bank for NTM."""
+    def __init__(self, N, M):
+        """Initialize the NTM Memory matrix.
+
+        The memory's dimensions are (batch_size x N x M).
+        Each batch has it's own memory matrix.
+
+        :param N: Number of rows in the memory.
+        :param M: Number of columns/features in the memory.
+        """
+        super(FastMemory, self).__init__()
+
+        self.N = N
+        self.M = M
+
+        # The memory bias allows the heads to learn how to initially address
+        # memory locations by content
+        self.register_buffer('mem_bias', torch.Tensor(N, M))
+
+        # Initialize memory bias
+        stdev = 1 / (np.sqrt(N + M))
+        nn.init.uniform_(self.mem_bias, -stdev, stdev)
+
+    def reset(self, batch_size):
+        """Initialize memory from bias, for start-of-sequence."""
+        self.batch_size = batch_size
+        self.memory = self.mem_bias.clone().repeat(batch_size, 1, 1)
+
+    def size(self):
+        return self.N, self.M
+
+    def read(self, w):
+        """Read from memory (according to section 3.1)."""
+        return torch.matmul(w.unsqueeze(1), self.memory).squeeze(1)
+
+    def write(self, w, e, a):
+        """write to memory (according to section 3.2)."""
+        self.prev_mem = self.memory
+        self.memory = torch.Tensor(self.batch_size, self.N, self.M)
+        erase = torch.matmul(w.unsqueeze(-1), e.unsqueeze(1))
+        add = torch.matmul(w.unsqueeze(-1), a.unsqueeze(1))
+        self.memory = self.prev_mem * (1 - erase) + add
+
+    def address(self, k, β, g, s, γ):
+        """NTM Addressing (according to section 3.3).
+
+        Returns a softmax weighting over the rows of the memory matrix.
+
+        :param k: The key vector.
+        :param β: The key strength (focus).
+        :param g: Scalar interpolation gate (with previous weighting).
+        :param s: Shift weighting.
+        :param γ: Sharpen weighting scalar.
+        """
+        # Content focus
+        wc = self._similarity(k, β)
+
+        # Location focus
+        wg = wc*g
+        ŵ = self._shift(wg, s)
+        w = self._sharpen(ŵ, γ)
+
+        return w
+
+    def _similarity(self, k, β):
+        k = k.view(self.batch_size, 1, -1)
+        w = F.softmax(β * F.cosine_similarity(self.memory + 1e-16, k + 1e-16, dim=-1), dim=1)
+        return w
+
+    def _shift(self, w, s):
+
+        batch_size = w.size(0)
+        max_shift = int((s.size(1) - 1) / 2)
+        
+        unrolled = torch.cat([w[:, -max_shift:], w, w[:, :max_shift]], 1)
+        return F.conv1d(unrolled.unsqueeze(1), s.unsqueeze(1))[range(batch_size), range(batch_size)]
+
+    def _sharpen(self, ŵ, γ):
+        w = ŵ ** γ
+        w = torch.div(w, torch.sum(w, dim=1).view(-1, 1) + 1e-16)
+        return w
+
+
+
+def FeedForward(dim, expansion_factor = 4, dropout = 0., dense = nn.Linear):
+    inner_dim = int(dim * expansion_factor)
+    return nn.Sequential(
+        nn.Linear(dim, inner_dim),
+        nn.GELU(),
+        nn.Linear(inner_dim, dim)
+    )
+
+class PreNormResidual(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.fn = fn
+        self.norm = nn.LayerNorm(dim)
+
+    def forward(self, x):
+        return 0.5*self.fn(x) + 0.5*x
+
+
+def _split_cols(mat, lengths):
+    """Split a 2D matrix to variable length columns."""
+    assert mat.size()[1] == sum(lengths), "Lengths must be summed to num columns"
+    l = np.cumsum([0] + lengths)
+    results = []
+    for s, e in zip(l[:-1], l[1:]):
+        results += [mat[:, s:e]]
+    return results
+
+
+class NTMHeadBase(nn.Module):
+    """An NTM Read/Write Head."""
+
+    def __init__(self, memory, controller_size):
+        """Initilize the read/write head.
+
+        :param memory: The :class:`NTMMemory` to be addressed by the head.
+        :param controller_size: The size of the internal representation.
+        """
+        super(NTMHeadBase, self).__init__()
+
+        self.memory = memory
+        self.N, self.M = memory.size()
+        self.controller_size = controller_size
+
+    def register_parameters(self):
+        raise NotImplementedError
+
+    def is_read_head(self):
+        return NotImplementedError
+
+    def _address_memory(self, k, β, g, s, γ):
+        # Handle Activations
+        k = k.clone()
+        β = F.softplus(β)
+        g = F.sigmoid(g)
+        s = F.softmax(s, dim=1)
+        γ = 1 + F.softplus(γ)
+
+        w = self.memory.address(k, β, g, s, γ)
+
+        return w
+
+
+class NTMReadHead(NTMHeadBase):
+    def __init__(self, memory, controller_size):
+        super(NTMReadHead, self).__init__(memory, controller_size)
+
+        # Corresponding to k, β, g, s, γ sizes from the paper
+        self.read_lengths = [self.M, 1, 1, 3, 1]
+        self.fc_read = nn.Linear(controller_size, sum(self.read_lengths))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        # Initialize the linear layers
+        nn.init.xavier_uniform_(self.fc_read.weight, gain=1.4)
+        nn.init.normal_(self.fc_read.bias, std=0.01)
+
+    def is_read_head(self):
+        return True
+
+    def forward(self, embeddings):
+        """NTMReadHead forward function.
+
+        :param embeddings: input representation of the controller.
+        """
+        o = self.fc_read(embeddings)
+        k, β, g, s, γ = _split_cols(o, self.read_lengths)
+
+        # Read from memory
+        w = self._address_memory(k, β, g, s, γ)
+        r = self.memory.read(w)
+
+        return r, w
+
+
+class NTMWriteHead(NTMHeadBase):
+    def __init__(self, memory, controller_size):
+        super(NTMWriteHead, self).__init__(memory, controller_size)
+
+        # Corresponding to k, β, g, s, γ, e, a sizes from the paper
+        self.write_lengths = [self.M, 1, 1, 3, 1, self.M, self.M]
+        self.fc_write = nn.Linear(controller_size, sum(self.write_lengths))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        # Initialize the linear layers
+        nn.init.xavier_uniform_(self.fc_write.weight, gain=1.4)
+        nn.init.normal_(self.fc_write.bias, std=0.01)
+
+    def is_read_head(self):
+        return False
+
+    def forward(self, embeddings):
+        """NTMWriteHead forward function.
+
+        :param embeddings: input representation of the controller.
+        """
+        o = self.fc_write(embeddings)
+        k, β, g, s, γ, e, a = _split_cols(o, self.write_lengths)
+
+        # e should be in [0, 1]
+        e = F.sigmoid(e)
+
+        # Write to memory
+        w = self._address_memory(k, β, g, s, γ)
+        self.memory.write(w, e, a)
+
+        return w
+class NTM(nn.Module):
+    """A Neural Turing Machine."""
+    def __init__(self, num_inputs, num_outputs, memory, heads):
+        """Initialize the NTM.
+
+        :param num_inputs: External input size.
+        :param num_outputs: External output size.
+        :param memory: :class:`NTMMemory`
+        :param heads: list of :class:`NTMReadHead` or :class:`NTMWriteHead`
+
+        Note: This design allows the flexibility of using any number of read and
+              write heads independently, also, the order by which the heads are
+              called in controlled by the user (order in list)
+        """
+        super(NTM, self).__init__()
+
+        # Save arguments
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.memory = memory
+        self.heads = heads
+
+        self.N, self.M = memory.size()
+
+        # Initialize the initial previous read values to random biases
+        self.num_read_heads = 0
+        self.init_r = []
+        for head in heads:
+            if head.is_read_head():
+                init_r_bias = torch.randn(1, self.M) * 0.01
+                self.register_buffer("read{}_bias".format(self.num_read_heads), init_r_bias.data)
+                self.init_r += [init_r_bias]
+                self.num_read_heads += 1
+
+        assert self.num_read_heads > 0, "heads list must contain at least a single read head"
+
+        # Initialize a fully connected layer to produce the actual output:
+        #   [controller_output; previous_reads ] -> output
+        self.fc = nn.Linear(num_inputs + self.num_read_heads * self.M, num_outputs)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        # Initialize the linear layer
+        nn.init.xavier_uniform_(self.fc.weight, gain=1)
+        nn.init.normal_(self.fc.bias, std=0.01)
+
+    def forward(self, x):
+        """NTM forward function.
+
+        :param x: input vector (batch_size x num_inputs)
+        """
+        # Read/Write from the list of heads
+        reads = []
+        heads_states = []
+        for head in self.heads:
+            if head.is_read_head():
+                r, head_state = head(x)
+                reads += [r]
+            else:
+                head_state = head(x)
+            heads_states += [head_state]
+
+        # Generate Output
+        inp2 = torch.cat([x] + reads, dim=1)
+        o = self.fc(inp2)
+
+        return o

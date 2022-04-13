0f66b6361a775092e4b8c1e08f5ee9a7b448ca9d
Martyn Garcia <martyn.garcia@gmail.com>
2022-04-12 18:20:50 -0600

    [fix] stable gan loss

--- hypergan/losses/stable_gan_loss.py
+++ hypergan/losses/stable_gan_loss.py
@@ -1,7 +1,9 @@
 import hypergan
 import torch
+import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from torch.autograd import Variable
+from torch.autograd import grad as torch_grad
 from torch.autograd import grad as torch_grad
 
 class StableGANLoss:
@@ -15,136 +17,85 @@ class StableGANLoss:
     def __init__(self, device='cuda:0', gammas=None, offsets=None, gan=None, metric_name=''):
         self.gan = gan
         self.metric_name = metric_name
+        self.target_x = None
+        self.target_g = None
+        self.fake_target_x = None
+        self.fake_target_g = None
+        self.reg_fake_target = None
+        self.reg_target = None
+        self.loss = self.gan.create_component("loss")
         if gammas is None:
-            gammas = [0, 10.0, 10.0, 0, 100.0]
-        if offsets is None:
-            offsets = [10,10,10,10]
-        if(len(gammas) <= 5):
-            gammas += [0,0,1e3,0]
-        self.offsets = []
-        for offset in offsets:
-            self.offsets.append(torch.tensor(offset, device=device))
-        self.gammas = []
-        for gamma in gammas:
-            if gamma is None or gamma == 0:
-                self.gammas.append(None)
-            else:
-                self.gammas.append(torch.tensor(gamma, device=device))
-        self.gamma1 = self.gammas[0]
-        self.g_gamma1 = self.gammas[1]
-        self.gamma2 = self.gammas[2]
-        self.g_gamma2 = self.gammas[3]
-        self.inverse_gamma = torch.tensor(gammas[4], device=device)
-
-        print(self.gammas)
-        self.l_gammas = self.gammas[5:]
-
-        self.l_offsets = offsets
-
-        self.softplus = torch.nn.Softplus(1, 20)
-        self.target1_x = None
-        self.target1_g = None
-        self.target2_x = None
-        self.target2_g = None
-
-    def loss_fn(self, d_real, d_fake):
-        if self.gan is None:
-            #cr = torch.mean(d_real,0)
-            #cf = torch.mean(d_fake,0)
-            #criterion = torch.nn.BCEWithLogitsLoss()
-            #g_loss = criterion(d_fake-cr, torch.ones_like(d_fake))
-            #d_loss = criterion(d_real-cf, torch.ones_like(d_real)) + criterion(d_fake-cr, torch.zeros_like(d_fake))
-            #g_loss = criterion(d_fake, torch.ones_like(d_fake))
-            #d_loss = criterion(d_real, torch.ones_like(d_real)) + criterion(d_fake, torch.zeros_like(d_fake))
-            #g_loss = torch.log(1-torch.sigmoid(d_fake) + 1e-13)
-            #d_loss = torch.log(torch.sigmoid(d_real) + 1e-13) +torch.log(1-torch.sigmoid(d_fake) + 1e-13)
-            #d_loss = (d_real - d_fake).mean()
-            #g_loss = d_fake.mean()
-            d_loss = (self.softplus(-d_real) + self.softplus(d_fake)).mean()
-            g_loss = self.softplus(-d_fake).mean()
-            return d_loss, g_loss
+            d_gammas = [10.0, 10.0, 1e6, 1e6]
         else:
-            return self.gan.trainable_gan.loss.forward(d_real, d_fake)
+            d_gammas = gammas
+        self.d_gammas = [torch.tensor(gamma,device=device) for gamma in d_gammas]
+        self.g_gamma = torch.tensor(10.0,device=device)
+        self.inverse_gamma = torch.tensor(1e3,device=device)
 
-    def stable_loss(self, discriminator, xs, gs, d_fake=None, d_real=None):
-        if d_fake is None:
-            d_fake = discriminator(*gs)
-        if d_real is None:
-            d_real = discriminator(*xs)
+    def stable_loss(self, discriminator, xs, gs, d_real = None, d_fake = None):
         d_losses = []
         g_losses = []
-        d_loss, g_loss = self.loss_fn(d_real, d_fake)
-        #d_losses.append(d_loss)
-        #g_losses.append(g_loss)
-        if self.target1_x == None:
-            self.target1_g = [Parameter(g, requires_grad=True) for g in gs]
-            self.target1_x = [Parameter(x, requires_grad=True) for x in xs]
-            self.target2_g = [Parameter(g, requires_grad=True) for g in gs]
-            self.target2_x = [Parameter(x, requires_grad=True) for x in xs]
-        for target, g in zip(self.target1_g + self.target2_g, gs + gs):
-            target.data = g.clone()
-        for target, x in zip(self.target1_x + self.target2_x, xs + xs):
-            target.data = x.clone()
-
-        g1, neg_inverse_fake = self.inverse(discriminator(*self.target2_g), d_real, self.target2_g)
-        g2, neg_inverse_real = self.inverse(d_fake, discriminator(*self.target2_x), self.target2_x)
-        #if self.gan is not None:
-        #    self.gan.add_metric('nif_diff', (neg_inverse_fake[0] - self.target2_g[0]).abs().sum())
-        #    self.gan.add_metric('nir_diff', (neg_inverse_real[0] - self.target2_x[0]).abs().sum())
-
-        reg_fake, _ = self.loss_fn(d_real, discriminator(*neg_inverse_fake))
-        reg_real, g_ = self.loss_fn(discriminator(*neg_inverse_real), d_fake)
-
-        if self.gamma2 is not None:
-            d_losses.append(self.gamma2*(reg_fake+reg_real))
-        if self.g_gamma2 is not None:
-            g_losses.append(self.g_gamma2 * g_)
-
-        #if self.gan is not None:
-        #    self.gan.add_metric('dl_s2', self.gamma2*(reg_fake+reg_real))
-        #    self.gan.add_metric('gl_s2', self.g_gamma2*(g_))
-
-        g3, inverse_fake = self.inverse(d_real, discriminator(*self.target1_g), self.target1_g)
-        g4, inverse_real = self.inverse(discriminator(*self.target1_x), d_fake, self.target1_x)
-
-        #if self.gan is not None:
-        #    self.gan.add_metric('if_diff', (inverse_fake[0] - self.target1_g[0]).abs().sum())
-        #    self.gan.add_metric('ir_diff', (inverse_real[0] - self.target1_x[0]).abs().sum())
-
-        reg_fake, g_ = self.loss_fn(discriminator(*inverse_fake), d_fake)
-        reg_real = self.loss_fn(d_real, discriminator(*inverse_real))[0]
-
-        if self.gamma1 is not None:
-            d_losses.append(self.gamma1*(reg_fake+reg_real))
-        if self.g_gamma1 is not None:
-            g_losses.append(self.g_gamma1 * g_)
-
-        #if self.gan is not None:
-        #    self.gan.add_metric('dl_s1', self.gamma1*(reg_fake+reg_real))
-        #    self.gan.add_metric('gl_s1', self.g_gamma1*(g_))
-        #g_losses.append(-g4[0].abs().mean()*self.l_gammas[3])
-        #g_losses.append(-g1[0].abs().mean()*self.l_gammas[0])
-
-        def add_l_loss_for(index, name, g):
-            if self.l_gammas[index] is not None:
-                result = torch.relu(g[0].abs().mean()-self.offsets[index])*self.l_gammas[index]
-                d_losses.append(result)
-                if self.gan is not None:
-                    self.gan.add_metric(self.metric_name+name, g[0].abs().mean())
-                    self.gan.add_metric(self.metric_name+name+"-", result)
-            else:
-                return None
-            return result
-        add_l_loss_for(0, 'l0', g1)
-        add_l_loss_for(1, 'l1', g2)
-        add_l_loss_for(2, 'l2', g3)
-        add_l_loss_for(3, 'l3', g4)
+        for form in ["teacher", "teacher_fake", "regularize_fake", "regularize_real"]:
+            adv_d_loss, adv_g_loss = self.adversarial_norm(form, xs[0], gs[0], discriminator, d_real, d_fake) # TODO multiple
+            self.gan.add_metric(form+'d', adv_d_loss.mean())
+            d_losses.append(adv_d_loss)
+            if adv_g_loss is not None:
+                self.gan.add_metric(form+'g', adv_g_loss.mean())
+                g_losses.append(adv_g_loss)
         return sum(d_losses), sum(g_losses)
 
+    def adversarial_norm(self, form, x, g, discriminator, d_real, d_fake):
+        if form == "teacher":
+            if self.target_x is None:
+                self.target_g = Parameter(g, requires_grad=True)
+                self.target_x = Parameter(x, requires_grad=True)
+            self.target_x.data = x.data.clone()
+            self.target_g.data = g.data.clone()
+            teacher_fake = self.inverse(d_real, discriminator(self.target_g), self.target_g)
+            teacher_real = self.inverse(discriminator(self.target_x), d_fake, self.target_x)
+
+            reg_fake, g_ = self.loss.forward(discriminator(teacher_fake), d_fake)
+            reg_real, _ = self.loss.forward(d_real, discriminator(teacher_real))
+
+            return self.d_gammas[0]*(reg_fake+reg_real), self.g_gamma* g_
+
+        if form == "teacher_fake":
+            if self.fake_target_x is None:
+                self.fake_target_g = Parameter(g, requires_grad=True)
+                self.fake_target_x = Parameter(x, requires_grad=True)
+
+            self.fake_target_x.data = x.data.clone()
+            self.fake_target_g.data = g.data.clone()
+            neg_teacher_fake = self.inverse(discriminator(self.fake_target_g), d_real, self.fake_target_g)
+            neg_teacher_real = self.inverse(d_fake, discriminator(self.fake_target_x), self.fake_target_x)
+
+            reg_fake, _ = self.loss.forward(d_real, discriminator(neg_teacher_fake))
+            reg_real, _ = self.loss.forward(discriminator(neg_teacher_real), d_fake)
+
+            return self.d_gammas[1]*(reg_fake+reg_real), None
+
+        if form == "regularize_fake":
+            if self.reg_fake_target is None:
+                self.reg_fake_target = Parameter(g, requires_grad=True)
+            self.reg_fake_target.data = g.data.clone()
+            reg_g = self.regularize_adversarial_norm(d_real, discriminator(self.reg_fake_target), self.reg_fake_target)
+            norm = (reg_g**2).mean()
+            return self.d_gammas[2] * norm.mean(), None
+
+        if form == "regularize_real":
+            if self.reg_target is None:
+                self.reg_target = Parameter(x, requires_grad=True)
+            self.reg_target.data = x.data.clone()
+            reg_x = self.regularize_adversarial_norm(d_fake, discriminator(self.reg_target), self.reg_target)
+            norm = (reg_x**2).mean()
+            return self.d_gammas[3] * norm.mean(), None
+
     def inverse(self, d_real, d_fake, target):
-        #loss = (d_fake - d_real) * self.inverse_gamma
-        loss = self.loss_fn(d_fake, d_real)[0]
-        #loss = (d_real - d_fake)
-        d1_grads = torch_grad(outputs=loss, inputs=target, retain_graph=True, create_graph=True, only_inputs=True)
-        return d1_grads, [_t + _d1/_d1.abs().sum()*self.inverse_gamma for _d1, _t in zip(d1_grads, target)]
-        #return [_t + _d1 for _d1, _t in zip(d1_grads, target)]
+        loss = self.loss.forward(d_fake, d_real)[0] * self.inverse_gamma
+        grads = torch_grad(outputs=loss, inputs=[target], retain_graph=True, create_graph=True, only_inputs=True)
+        return target + grads[0]
+
+    def regularize_adversarial_norm(self, d_real, d_fake, target):
+        loss = ((d_real - d_fake)**2).mean()
+        grads = torch_grad(outputs=loss, inputs=[target], retain_graph=True, create_graph=True, allow_unused=True)
+        return grads[0]

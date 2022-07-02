c907772f935fe157bddb368dc7c05aeb0ee03805
Martyn Garcia <martyn.garcia@gmail.com>
2022-07-02 17:07:48 -0600

    [refactor] attention layer

--- hypergan/layers/__init__.py
+++ hypergan/layers/__init__.py
@@ -14,6 +14,7 @@ from .operation import Operation
 from .add import Add
 from .cat import Cat
 from .mul import Mul
+from .attention import Attention
 from .channel_attention import ChannelAttention
 from .efficient_attention import EfficientAttention
 from .ez_norm import EzNorm

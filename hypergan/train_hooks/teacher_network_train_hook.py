7d26b8907a2a8809092ef25d4caa1c495d936cb8
Martyn Garcia <martyn.garcia@gmail.com>
2022-12-13 21:55:32 -0700

    [feature] load pretrained jepa in train hook

--- hypergan/train_hooks/teacher_network_train_hook.py
+++ hypergan/train_hooks/teacher_network_train_hook.py
@@ -15,19 +15,36 @@ from PIL import Image
 from torch.autograd import Variable
 from torch.autograd import grad as torch_grad
 from hypergan.train_hooks.clip_prompt_train_hook import ClipPromptTrainHook, MakeCutouts
+from data2vec.models.data2vec_vision import Data2VecVisionConfig, Data2VecVisionModel
+from data2vec.models.data2vec_image_classification import Data2VecImageClassificationConfig
+from data2vec.tasks.mae_image_pretraining import *
+from data2vec.models.data2vec2 import *
+from fairseq.dataclass.initialize import hydra_init
+from fairseq import checkpoint_utils, tasks
 
 class TeacherNetworkTrainHook(BaseTrainHook):
-
     def create(self):
         cut_size = 224
         self.make_cutouts = MakeCutouts(cut_size, self.config.cutn or 1, cut_pow=1)
-        model, train_transform, eval_transform = open_clip.create_model_and_transforms(self.config.model or 'ViT-B-32', self.config.model_provider or 'openai')
-        self.perceptor = model.eval().requires_grad_(False).to('cuda:0')
+        state = checkpoint_utils.load_checkpoint_to_cpu("base_imagenet.pt", {})
+        pretrained_args = state.get("cfg", None)
+        pretrained_args.criterion = None
+        pretrained_args.lr_scheduler = None
+        task = tasks.setup_task(pretrained_args.task)
+        pretrained_args.task.data = cfg.data
+        model = task.build_model(pretrained_args.model, from_checkpoint=True)
+        model.remove_pretraining_modules()
+        model.load_state_dict(state["model"], strict=True)
+        self.data2vec = model
+        self.data2vec = self.data2vec.eval().requires_grad_(False).to('cuda:0')
 
     def forward(self, d_loss, g_loss):
-        layer = self.perceptor.encode_image(self.make_cutouts(self.gan.x)).float()
-        target = (layer > 0).float()
+        inpx = self.gan.x
+        layer = self.data2vec(inpx, mask=False, features_only=True)["layer_results"][-1]
         prediction = self.gan.discriminator.context[self.config.layer_name]
-        loss = F.cross_entropy(prediction.view(target.shape[0], -1), target.view(target.shape[0], -1))# * 0.05
+        target = (layer > 0).float()
+        #loss = F.cross_entropy(prediction.view(target.shape[0], -1), target.view(target.shape[0], -1))# * 0.05
+        loss = F.mse_loss(prediction.view(target.shape[0], -1), target.view(target.shape[0], -1))# * 0.05
         self.add_metric('ce', loss)
+        self.add_metric('t', target.mean())
         return loss, loss

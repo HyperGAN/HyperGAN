d3d103bf76e5f5b12af0f8382e7064eff40ccf4f
Martyn Garcia <martyn.garcia@gmail.com>
2023-01-18 16:06:15 -0700

    [fix] use cos loss with clip

--- hypergan/train_hooks/teacher_network_train_hook.py
+++ hypergan/train_hooks/teacher_network_train_hook.py
@@ -23,11 +23,17 @@ class TeacherNetworkTrainHook(BaseTrainHook):
         self.make_cutouts = MakeCutouts(cut_size, self.config.cutn or 1, cut_pow=1)
         model, train_transform, eval_transform = open_clip.create_model_and_transforms(self.config.model or 'ViT-B-32', self.config.model_provider or 'openai')
         self.perceptor = model.eval().requires_grad_(False).to('cuda:0')
+        self.normalize = transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073],
+                                  std=[0.26862954, 0.26130258, 0.27577711])
 
     def forward(self, d_loss, g_loss):
-        layer = self.perceptor.encode_image(self.make_cutouts(self.gan.x)).float()
-        target = (layer > 0).float()
+        layer =  self.perceptor.encode_image(self.make_cutouts(self.normalize(self.gan.x/2+0.5)))
+        #target = (layer > 0).float()
         prediction = self.gan.discriminator.context[self.config.layer_name]
-        loss = F.cross_entropy(prediction.view(target.shape[0], -1), target.view(target.shape[0], -1))# * 0.05
+
+        input_normed = F.normalize(layer.unsqueeze(1), dim=2)
+        embed_normed = F.normalize(prediction.unsqueeze(1), dim=2)
+        #loss = F.cross_entropy(prediction.view(target.shape[0], -1), target.view(target.shape[0], -1))# * 0.05
+        loss = input_normed.sub(embed_normed).norm(dim=2).div(2).arcsin().pow(2).mul(2).mean()
         self.add_metric('ce', loss)
         return loss, loss
